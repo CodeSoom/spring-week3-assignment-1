cac174ec0710a693c7a2f39f6edc5003bda8859c
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-01 20:02:31 +0900

    test: Create 'HelloController' web test

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,17 +1,17 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
-
 class HelloControllerTest {
-  @Test
-  void testSayHello() {
-    HelloController helloController = new HelloController();
-    //AssertJ
-    assertThat(helloController.hello()).isEqualTo("Hello World!");
-    //JUnit5
-    assertEquals(helloController.hello(), "Hello World!");
-  }
+    @Test
+    void testSayHello() {
+        HelloController helloController = new HelloController();
+        //AssertJ
+        assertThat(helloController.hello()).isEqualTo("Hello World!");
+        //JUnit5
+        assertEquals(helloController.hello(), "Hello World!");
+    }
 }
