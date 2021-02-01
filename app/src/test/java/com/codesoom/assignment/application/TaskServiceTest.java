532c6e65c9055acd397d2af60065469f21165b4e
Lee Dong Wook <sh95119@gmail.com>
2021-02-02 00:47:32 +0900

    FEATURE: add unit test of TaskService

---
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -0,0 +1,79 @@
+package com.codesoom.assignment.application;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.models.Task;
+
+class TaskServiceTest {
+
+    private TaskService taskService;
+    private static final String TASK_TITLE = "test";
+    private static final String UPDATE_POSTFIX = "test";
+
+    @BeforeEach
+    void setUp() {
+        // subject
+        taskService = new TaskService();
+
+        // fixture
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+
+        taskService.createTask(task);
+    }
+
+    @Test
+    void getTasks() {
+        assertThat(taskService.getTasks()).hasSize(1);
+
+        Task task = taskService.getTasks().get(0);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    void getTaskWithValidId() {
+        Task found = taskService.getTask(1L);
+        assertThat(found.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    void getTaskWithInvalidId() {
+        assertThatThrownBy(() -> taskService.getTask(100L))
+            .isInstanceOf(TaskNotFoundException.class);
+    }
+
+    @Test
+    void createTask() {
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+
+        taskService.createTask(task);
+        assertThat(taskService.getTasks()).hasSize(2);
+    }
+
+    @Test
+    void deleteTask() {
+        int oldSize = taskService.getTasks().size();
+
+        taskService.deleteTask(1L);
+
+        int newSize = taskService.getTasks().size();
+
+        assertThat(oldSize - newSize).isEqualTo(1);
+    }
+
+    @Test
+    void updateTask() {
+        Task source = new Task();
+        source.setTitle(TASK_TITLE + UPDATE_POSTFIX);
+        taskService.updateTask(1L, source);
+
+        Task task = taskService.getTask(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE + "!!!");
+    }
+
+}
