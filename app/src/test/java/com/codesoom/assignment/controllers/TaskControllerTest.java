610f8b1f23cf35e7ba33717945f6ae0fad50871e
juunini <juuni.ni.i@gmail.com>
2021-02-01 14:36:44 +0900

    Set list method with tasks test

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,6 +1,8 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,6 +11,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,6 +29,9 @@ class TaskControllerTest {
     @MockBean
     private TaskService taskService;
 
+    private final long TASK_ID = 0;
+    private final String TASK_TITLE = "sample";
+
     @Nested
     @DisplayName("list 메소드는")
     class Describe_list {
@@ -30,20 +39,48 @@ class TaskControllerTest {
         @Nested
         @DisplayName("Task 가 하나도 없을 때")
         class Context_without_any_tasks {
+            @BeforeEach
+            void setEmpty() {
+                given(taskService.getTasks())
+                        .willReturn(new ArrayList<>());
+            }
 
             @Test
             @DisplayName("빈 배열을 리턴한다.")
             void It_returns_empty() throws Exception {
+                final String expectContent = "[]";
+
                 mockMvc.perform(get("/tasks"))
                         .andExpect(status().isOk())
-                        .andExpect(content().string("[]"));
+                        .andExpect(content().string(expectContent));
             }
         }
 
         @Nested
         @DisplayName("Task 가 있을 때")
         class Context_with_tasks {
+            @BeforeEach
+            void setTasks() {
+                Task task = new Task();
+                task.setId(TASK_ID);
+                task.setTitle(TASK_TITLE);
 
+                List<Task> tasks = new ArrayList<>();
+                tasks.add(task);
+
+                given(taskService.getTasks())
+                        .willReturn(tasks);
+            }
+
+            @Test
+            @DisplayName("task 가 담겨있는 JSON Array 문자열을 리턴한다.")
+            void It_returns_JSON_array_string() throws Exception {
+                final String expectContent = String.format("[{\"id\":%d,\"title\":\"%s\"}]", TASK_ID, TASK_TITLE);
+
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(expectContent));
+            }
         }
     }
 
