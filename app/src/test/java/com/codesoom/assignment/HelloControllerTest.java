825e8ef0e7b11e06f47c5ff48c55641d1afc4004
Law Woo <law@cupist.com>
2021-02-01 16:40:02 +0900

    Update sayHello test by assertj

--- app/src/test/java/com/codesoom/assignment/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/HelloControllerTest.java
@@ -3,11 +3,12 @@ package com.codesoom.assignment;
 import com.codesoom.assignment.controllers.HelloController;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloControllerTest {
     @Test
     void sayHello() {
-        assertEquals("Hello, world!", new HelloController().sayHello());
+        assertThat(new HelloController().sayHello()).isEqualTo("Hello, world!");
     }
 }
