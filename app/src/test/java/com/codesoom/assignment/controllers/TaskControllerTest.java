3d80720b9f5cfa8d0fba83efc4606375ff4486ea
thkim <thkim2017@gmail.com>
2021-02-03 07:58:32 +0900

    Add a test when a task is added controller.list() should be not empty

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,4 +12,16 @@ class TaskControllerTest {
 
         assertThat(controller.list()).isEmpty();
     }
+
+    @Test
+    void createNewTask() {
+        TaskController controller = new TaskController();
+
+        Task task = new Task();
+        task.setTitle("test");
+        controller.create(new Task());
+
+        assertThat(controller.list()).isNotEmpty();
+    }
+
 }
