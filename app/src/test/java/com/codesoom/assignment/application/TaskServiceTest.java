1e6e90656204a51d3341a034a3d595c94b97c794
newoo <newoo4297@naver.com>
2021-02-02 20:14:20 +0900

    Add test about getTask

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -1,19 +1,29 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("TaskService")
 public class TaskServiceTest {
+    private static final Long TASK_ID = 1L;
+    private static final String TASK_TITLE = "Test";
+
     private TaskService taskService;
+    private Task task;
 
     @BeforeEach
     void setUp() {
         taskService = new TaskService();
+
+        task = new Task();
+        task.setTitle(TASK_TITLE);
     }
 
     @Nested
@@ -29,5 +39,51 @@ public class TaskServiceTest {
             }
         }
 
+        @Nested
+        @DisplayName("with a task")
+        class Context_with_task {
+            @BeforeEach
+            void addNewTask() {
+                taskService.createTask(task);
+            }
+
+            @Test
+            @DisplayName("return list")
+            void it_return_empty() {
+                assertThat(taskService.getTasks()).hasSize(1);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("getTask")
+    class Describe_getTask {
+        @BeforeEach
+        void addNewTask() {
+            taskService.createTask(task);
+        }
+
+        @Nested
+        @DisplayName("with valid id")
+        class Context_with_valid_id {
+            @Test
+            @DisplayName("return task")
+            void it_return_empty() {
+                assertThat(taskService.getTask(1L).getClass()).isEqualTo(Task.class);
+                assertThat(taskService.getTask(1L).getId()).isEqualTo(TASK_ID);
+                assertThat(taskService.getTask(1L).getTitle()).isEqualTo(TASK_TITLE);
+            }
+        }
+
+        @Nested
+        @DisplayName("with invalid id")
+        class Context_with_invalid_id {
+            @Test
+            @DisplayName("return task")
+            void it_return_empty() {
+                assertThatThrownBy(() -> taskService.getTask(100L))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+        }
     }
 }
