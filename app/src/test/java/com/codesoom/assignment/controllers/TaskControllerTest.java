0959d2b57492e3777084213ef2b546f8fc5df179
mikekang47 <gyeong0308@gmail.com>
2021-02-02 14:33:16 +0900

    Make TaskControllerTest.java and make it works Plus add an constructor to approach TaskController.java in TaskControllerTest.java

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,16 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @Test
+    void list() {
+        TaskController controller = new TaskController();
+
+        assertThat(controller.list()).isEmpty();
+    }
+}
