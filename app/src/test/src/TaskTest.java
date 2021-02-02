c0b8b9d069b54381c996649c016ca1d58ed00755
wenodev <logicalstark1105@gmail.com>
2021-02-02 23:14:58 +0900

    Test코드 위치 변경

---
+++ app/src/test/src/TaskTest.java
@@ -0,0 +1,13 @@
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+
+    Task task;
+
+    @Test
+    void init(){
+        task = new Task();
+    }
+
+}
