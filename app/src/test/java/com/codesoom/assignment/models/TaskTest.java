43f5ab7ae192e188a71f45b0c863d5ac66ba624b
seungin yang <rhfpdk92@naver.com>
2021-02-02 22:45:27 +0900

    Set OLD_ID variable to ID and OLD_TITLE to TITLE

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -8,22 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskTest {
     Task task;
-    final Long OLD_ID = 1L;
+    final Long ID = 1L;
     final Long NEW_ID = 2L;
-    final String OLD_TITLE = "old title";
+    final String TITLE = "old title";
     final String NEW_TITLE = "new title";
 
     @BeforeEach
     void setup() {
         task = new Task();
-        task.setId(OLD_ID);
+        task.setId(ID);
         task.setTitle("old title");
     }
 
     @Test
     @DisplayName("getId() 테스트")
     void getIdTest() {
-        assertThat(task.getId()).isEqualTo(OLD_ID);
+        assertThat(task.getId()).isEqualTo(ID);
     }
 
     @Test
@@ -36,7 +36,7 @@ class TaskTest {
     @Test
     @DisplayName("getTitle() 테스트")
     void getTitleTest() {
-        assertThat(task.getTitle()).isEqualTo(OLD_TITLE);
+        assertThat(task.getTitle()).isEqualTo(TITLE);
     }
     @Test
     @DisplayName("setTitle() 테스트")
