d43332e261de4b00c222386c4c6ff12f78b12039
newoo <newoo4297@naver.com>
2021-02-02 19:49:04 +0900

    Create TaskTest about task model

--- app/src/test/java/com/codesoom/assignment/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/HelloControllerTest.java
@@ -6,8 +6,14 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class HelloControllerTest {
+    private static final String HELLO = "Hello, world!";
+
+    /**
+     * Test about "GET /" request.
+     * Check if hello text is expected tesst
+     */
     @Test
     void sayHello() {
-        assertThat(new HelloController().sayHello()).isEqualTo("Hello, world!");
+        assertThat(new HelloController().sayHello()).isEqualTo(HELLO);
     }
 }
