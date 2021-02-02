20802089efbf23b81d2da8baa69c788ea3381295
sunggyupaik <melon9751@naver.com>
2021-02-02 19:41:21 +0900

    Make CRUD testService Test
    Make CREATE, UPDATE, DELETE test methods

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -5,6 +5,8 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,6 +15,7 @@ class TaskServiceTest {
 
     private TaskService taskService;
     private static final String TASK_TITLE = "test";
+    private static final String UPDATE_POSTFIX = "!!!";
 
     @BeforeEach
     void setUp() {
@@ -25,14 +28,58 @@ class TaskServiceTest {
 
     @Test
     void getTasks() {
-        assertThat(taskService.getTasks()).isEmpty();
+        List<Task> tasks = taskService.getTasks();
+        assertThat(tasks).hasSize(1);
+
+        Task task = taskService.getTasks().get(0);
+        assertThat(task.getId()).isEqualTo(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
     }
 
     @Test
-    void getTask() {
-        assertThat(taskService.getTask(1L).getTitle()).isEqualTo(TASK_TITLE);
+    void createTest() {
+        int oldSize = taskService.getTasks().size();
 
-        assertThatThrownBy(() -> taskService.getTask(1L))
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+
+        taskService.createTask(task);
+
+        int newSize = taskService.getTasks().size();
+
+        assertThat(newSize - oldSize).isEqualTo(1);
+    }
+
+    @Test
+    void updateTask() {
+        Task source = new Task();
+        source.setTitle(TASK_TITLE + UPDATE_POSTFIX);
+        taskService.updateTask(1L, source);
+
+        Task task = taskService.getTask(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE + UPDATE_POSTFIX);
+    }
+
+    @Test
+    void deleteTest() {
+        int oldSize = taskService.getTasks().size();
+
+        taskService.deleteTask(1L);
+
+        int newSize = taskService.getTasks().size();
+
+        assertThat(oldSize - newSize).isEqualTo(1);
+    }
+
+    @Test
+    void getTaskWithValidId() {
+        Task task = taskService.getTask(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    void getTaskWithInvalidId() {
+        assertThatThrownBy(() -> taskService.getTask(100L))
                 .isInstanceOf(TaskNotFoundException.class);
     }
 }
