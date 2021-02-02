5f7e69834181c7cf4e91d3678c50678c023daa77
Lee Dong Wook <dwlee@crscube.io>
2021-02-02 20:58:44 +0900

    FIX: modify method of request mapping to get mapping

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -1,12 +1,15 @@
 package com.codesoom.assignment.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
     public String sayHello() {
         return "Hello, World!";
     }
