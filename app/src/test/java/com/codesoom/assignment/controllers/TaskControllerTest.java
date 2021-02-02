833f7745c7e4af51a8fac27046d5881ae04b6841
mikekang47 <gyeong0308@gmail.com>
2021-02-02 15:27:02 +0900

    Add createNewTask method to test create task Plust setUp method

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,10 +9,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TaskControllerTest {
 
+    private TaskController controller;
+
+    @BeforeEach
+    void setUp(){
+       controller = new TaskController();
+    }
+
     @Test
     void list() {
-        TaskController controller = new TaskController();
-
         assertThat(controller.list()).isEmpty();
     }
+
+    @Test
+    void createNewTask(){
+        Task task = new Task();
+        task.setTitle("Test1");
+        controller.create(task);
+
+        task.setTitle("Test2");
+        controller.create(task);
+
+        assertThat(controller.list()).hasSize(2);
+        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
+        assertThat(controller.list().get(0).getTitle()).isEqualTo("Test1");
+        assertThat(controller.list().get(1).getId()).isEqualTo(2L);
+        assertThat(controller.list().get(1).getTitle()).isEqualTo("Test2");
+    }
+
 }
