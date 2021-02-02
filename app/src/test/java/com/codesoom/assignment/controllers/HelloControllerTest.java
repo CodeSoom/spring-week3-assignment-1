3cd3653621763641f75bc43638f1b1fde6629094
Lee Dong Wook <dwlee@crscube.io>
2021-02-02 22:03:32 +0900

    TEST: modify HelloController to simple

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -6,14 +6,13 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+@WebMvcTest(HelloController.class)
 class HelloControllerTest {
-    private HelloController controller;
 
-    @BeforeEach
-    void setUp() {
-        controller = new HelloController();
-    }
+    @Autowired
+    private HelloController controller;
 
     @Test
     void sayHello() {
