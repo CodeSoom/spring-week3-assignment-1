989f739fb822966a9740e08147a4a93436bd3998
mikekang47 <gyeong0308@gmail.com>
2021-02-02 16:14:16 +0900

    Make fixtures to easily approach to taskService Plus make getTask method

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -5,6 +5,8 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
@@ -12,18 +14,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class TaskServiceTest {
 
     private TaskService taskService;
-
+    private static final String TASK_TITLE = "test";
     @BeforeEach
     void setUp() {
         taskService = new TaskService();
+
+        //fixtures
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+
+        taskService.createTask(task);
     }
 
     @Test
-    void getTasks(){
-        Task task = new Task();
-        task.setTitle("Test");
-        taskService.createTask(task);
-        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("Test");
+    void getTasks() {
+        List<Task> tasks = taskService.getTasks();
+
+        assertThat(tasks).hasSize(1);
+
+        Task task = tasks.get(0);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    void getTask(){
+        Task task = taskService.getTask(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
         assertThatThrownBy(() -> taskService.getTask(100L))
                 .isInstanceOf(TaskNotFoundException.class);
     }
