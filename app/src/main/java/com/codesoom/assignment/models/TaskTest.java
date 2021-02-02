dece564a25e1c6c6c2257689f7625baa7d01c449
wenodev <logicalstark1105@gmail.com>
2021-02-02 23:01:23 +0900

    fixture 생성

---
+++ app/src/main/java/com/codesoom/assignment/models/TaskTest.java
@@ -0,0 +1,22 @@
+package com.codesoom.assignment.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/*
+1. fixture 만들기
+ */
+
+class TaskTest {
+
+    Task task;
+
+    @BeforeEach
+    void init(){
+        task = new Task();
+    }
+    
+}
