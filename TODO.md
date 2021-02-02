f3aecb1b94240a58ad39f2d4b363e53d4e29b6fd
wenodev <logicalstark1105@gmail.com>
2021-02-03 07:41:19 +0900

    Test코드 작성 계획

--- TODO.md
+++ TODO.md
@@ -4,6 +4,6 @@
 1. 강의실 자료 학습 및 과제 확인 - 완료
 
 ## TUE
-1. Task 모델 테스트
-
+1. Task 모델 테스트 - 보류
+2. HelloController 테스트
 
--- app/src/test/src/models/TaskTest.java
+++ app/src/test/src/models/TaskTest.java
@@ -1,3 +1,5 @@
+package models;
+
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.Test;
 
