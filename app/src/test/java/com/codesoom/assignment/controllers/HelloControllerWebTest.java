798ee3a15e15736417dd59e0e4343599a0c0a83f
Lee Dong Wook <dwlee@crscube.io>
2021-02-02 22:09:27 +0900

    TEST: add HelloControllerWebTest

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.core.StringContains.*;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -30,6 +31,12 @@ public class HelloControllerWebTest {
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().string(containsString("Hello, World!")));
+    }
+
+    @Test
+    void sayHelloShouldReturnMessage() throws Exception {
+        this.mockMvc.perform(get("/")).andDo(print()).andExpect(status().isOk())
+            .andExpect(content().string(containsString("Hello, World")));
     }
 
     @Test
