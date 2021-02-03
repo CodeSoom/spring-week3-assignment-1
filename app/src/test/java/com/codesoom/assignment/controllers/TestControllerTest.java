3dba0d3c92423aec28731b7f0eef70bb3c7c6ebf
etff <wish2rich@gmail.com>
2021-02-04 01:21:29 +0900

    test: remove unuse import

--- app/src/test/java/com/codesoom/assignment/controllers/TestControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TestControllerTest.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
