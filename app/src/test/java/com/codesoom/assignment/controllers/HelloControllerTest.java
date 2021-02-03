a7f4a664e683acfe4b2a2fbb741e18f819ae54a9
sunggyupaik <melon9751@naver.com>
2021-02-03 21:02:54 +0900

    Add HelloController test

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,15 +1,27 @@
 package com.codesoom.assignment.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureMockMvc
+@SpringBootTest
 class HelloControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void sayHello(){
-        HelloController controller = new HelloController();
-        String result = controller.sayHello();
-        assertThat(result).isEqualTo("Hello, world!");
+    void sayHello() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
     }
 }
