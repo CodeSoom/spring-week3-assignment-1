ac3e8f59b935b59d3ca8fa64789b5980be858653
Las <haho013039@gmail.com>
2021-02-03 23:44:01 +0900

    Add web test case for create task

--- app/src/test/java/com/codesoom/assignment/controllers/TaskWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskWebTest.java
@@ -1,14 +1,15 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -24,5 +25,16 @@ public class TaskWebTest {
     void getAllTasks() throws Exception {
         mockMvc.perform(get("http://localhost/tasks"))
             .andExpect(status().isOk());
+    }
+
+    @Test
+    void createTask() throws Exception {
+        String taskJsonString = "{\"title\": \"play game!\"}";
+
+        mockMvc.perform(
+            post("http://localhost/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(taskJsonString)
+        ).andExpect(status().isCreated());
     }
 }
