fbc5bd4d65846ff8183c9d8feea4f04fe0a30c8b
Las <haho013039@gmail.com>
2021-02-03 21:04:04 +0900

    Add task controller test cases

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,78 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+    private TaskController taskController;
+    @BeforeEach
+    void initTaskController() {
+        TaskService taskService = new TaskService();
+        taskController = new TaskController(taskService);
+    }
+
+    @Test
+    void list() {
+        assertEquals(0, taskController.list().size());
+    }
+
+    @Test
+    void detail() {
+        Task task = new Task();
+        task.setTitle("Play!");
+        Task createdTask = taskController.create(task);
+
+        Task foundTask = taskController.detail(createdTask.getId());
+
+        assertEquals("Listen", foundTask.getTitle());
+    }
+
+    @Test
+    void create() {
+        Task task = new Task();
+        task.setTitle("Play!");
+
+        Task createdTask = taskController.create(task);
+
+        assertEquals("Play!", createdTask.getTitle());
+    }
+
+    @Test
+    void update() {
+        Task task = new Task();
+        task.setTitle("Play!");
+        Task createdTask = taskController.create(task);
+
+        task.setTitle("Listen");
+        Task updatedTask = taskController.update(createdTask.getId(), task);
+
+        assertEquals("Listen", updatedTask.getTitle());
+    }
+
+    @Test
+    void patch() {
+        Task task = new Task();
+        task.setTitle("Play!");
+        Task createdTask = taskController.create(task);
+
+        task.setTitle("Listen");
+        Task updatedTask = taskController.patch(createdTask.getId(), task);
+
+        assertEquals("Listen", updatedTask.getTitle());
+    }
+
+    @Test
+    void delete() {
+        Task task = new Task();
+        task.setTitle("Play!");
+        Task createdTask = taskController.create(task);
+
+        taskController.delete(createdTask.getId());
+
+        assertEquals(0, taskController.list().size());
+    }
+}
