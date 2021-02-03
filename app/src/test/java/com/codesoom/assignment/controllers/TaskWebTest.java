80867e3f329f8e7f4a4cd5d7405c666ecb4aa655
Las <haho013039@gmail.com>
2021-02-03 23:33:27 +0900

    Add web test for get empty tasks

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskWebTest.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.TaskService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskWebTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private TaskService taskService = new TaskService();
+
+    @Test
+    void getAllTasks() throws Exception {
+        mockMvc.perform(get("http://localhost/tasks"))
+            .andExpect(status().isOk());
+    }
+}
