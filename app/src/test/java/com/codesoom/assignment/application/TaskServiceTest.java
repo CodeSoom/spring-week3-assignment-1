466664798f5899d3632678b691afdf583a7d2c51
thkim <thkim2017@gmail.com>
2021-02-04 11:24:26 +0900

    Add a test for TaskService Create a test for getTasks method Apply BeforeEach setUp()

---
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -0,0 +1,21 @@
+package com.codesoom.assignment.application;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService();
+    }
+
+    @Test
+    void getTasks() {
+        assertThat(taskService.getTasks()).isEmpty();
+    }
+}
