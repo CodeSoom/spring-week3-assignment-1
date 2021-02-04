4d84637144d0baf9c11a9b36ac8b4d93c9b55d95
rlawlstjd0077 <rlawlstjd077@gmail.com>
2021-02-04 19:05:21 +0900

    refactor: Remove context

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -6,16 +6,15 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Task 클래스")
 class TaskTest {
 
-    Task task;
     final long TASK_ID = 0L;
     final long UPDATE_ID = 1L;
     final String TASK_TITLE = "Get Sleep";
     final String UPDATE_TITLE = "Do Study";
+    Task task;
 
     @BeforeEach
     void setUp() {
@@ -27,59 +26,42 @@ class TaskTest {
     @Nested
     @DisplayName("getId() 메서드는")
     class Describe_getId {
-        @Nested
-        @DisplayName("task가 존재한다면")
-        class Context_task_exist {
-            @Test
-            @DisplayName("task의 id를 반환한다")
-            void it_return_task_id() {
-                assertThat(task.getId()).isEqualTo(TASK_ID);
-            }
+        @Test
+        @DisplayName("task의 id를 반환한다")
+        void it_return_task_id() {
+            assertThat(task.getId()).isEqualTo(TASK_ID);
         }
     }
 
     @Nested
     @DisplayName("setId() 메서드는")
     class Describe_setId {
-        @Nested
-        @DisplayName("task가 존재한다면")
-        class Context_task_exist {
-            @Test
-            @DisplayName("task의 id를 변경한다")
-            void it_set_task_id() {
-                task.setId(UPDATE_ID);
-                assertThat(task.getId()).isEqualTo(UPDATE_ID);
-            }
+        @Test
+        @DisplayName("task의 id를 변경한다")
+        void it_set_task_id() {
+            task.setId(UPDATE_ID);
+            assertThat(task.getId()).isEqualTo(UPDATE_ID);
         }
     }
 
     @Nested
     @DisplayName("getTitle() 메서드는")
     class Describe_getTitle {
-        @Nested
-        @DisplayName("task가 존재한다면")
-        class Context_task_exist {
-            @Test
-            @DisplayName("task의 title를 반환한다")
-            void it_return_task_id() {
-                assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
-            }
+        @Test
+        @DisplayName("task의 title를 반환한다")
+        void it_return_task_id() {
+            assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
         }
     }
 
     @Nested
     @DisplayName("setTitle() 메서드는")
     class Describe_setTitle {
-        @Nested
-        @DisplayName("task가 존재한다면")
-        class Context_task_exist {
-            @Test
-            @DisplayName("task의 title을 변경한다")
-            void it_set_task_id() {
-                task.setTitle(UPDATE_TITLE);
-                assertThat(task.getTitle()).isEqualTo(UPDATE_TITLE);
-            }
+        @Test
+        @DisplayName("task의 title을 변경한다")
+        void it_set_task_id() {
+            task.setTitle(UPDATE_TITLE);
+            assertThat(task.getTitle()).isEqualTo(UPDATE_TITLE);
         }
     }
-
 }
