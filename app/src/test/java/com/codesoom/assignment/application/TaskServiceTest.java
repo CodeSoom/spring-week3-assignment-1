9067f93e542c3169195973028cb7fb6299129d1a
thkim <thkim2017@gmail.com>
2021-02-04 22:11:29 +0900

    Add tests for getTask With valid Id and inValid Id

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -1,10 +1,12 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TaskServiceTest {
     private TaskService taskService;
@@ -17,5 +19,21 @@ class TaskServiceTest {
     @Test
     void getTasks() {
         assertThat(taskService.getTasks()).isEmpty();
+    }
+
+    @Test
+    void getTaskWithValidId() {
+        Task task = new Task();
+        task.setTitle("test");
+
+        taskService.createTask(task);
+
+        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("test");
+    }
+
+    @Test
+    void getTaskWithInvalidId() {
+        assertThatThrownBy(() -> taskService.getTask(1L))
+                .isInstanceOf(TaskNotFoundException.class);
     }
 }
