02c8455aa5b77023720a0166451f2dfb793f34b5
jennifer <jennifer@novonetworks.com>
2021-02-05 09:21:54 +0900

    Update TaskControllerTest

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -21,7 +21,7 @@ class TaskControllerTest {
     private static final String TASK_TITLE_2 = "title2";
     private Task task1;
     private Task task2;
-    private List<Long> invalidIds;
+    private List<Long> givenInvalidIds;
 
     private TaskController taskController;
 
@@ -36,18 +36,18 @@ class TaskControllerTest {
         task2 = new Task();
         task2.setTitle(TASK_TITLE_2);
 
-        invalidIds = new ArrayList<>();
-        Collections.addAll(invalidIds, -100L, -1L, 0L, 5L, 100L);
+        givenInvalidIds = new ArrayList<>();
+        Collections.addAll(givenInvalidIds, -100L, -1L, 0L, 5L, 100L);
     }
 
     @Test
-    @DisplayName("할 일이 없는 경우 list()를 실행하면 비어있는 리스트가 리턴된다.")
+    @DisplayName("할 일이 없는 경우 할 일 목록을 가져오면 비어있는 리스트가 리턴된다.")
     void listWithEmptyTasks() {
         assertThat(taskController.list()).isEmpty();
     }
 
     @Test
-    @DisplayName("할 일이 추가되는 경우 list()를 실행하면 리턴되는 리스트의 크기가 증가한다.")
+    @DisplayName("할 일이 추가되는 경우 할 일 목록을 가져오면 리턴되는 리스트의 크기가 증가한다.")
     void listWithAddedTasks() {
         taskController.create(task1);
         assertThat(taskController.list()).hasSize(1);
@@ -61,7 +61,7 @@ class TaskControllerTest {
 
 
     @Test
-    @DisplayName("할 일이 하나 이상 있는 경우 detail(N)을 실행하면 N번째 할 일이 리턴된다. (N > 0)")
+    @DisplayName("주어진 id를 가진 할 일이 저장되어 있는 경우 해당 할 일을 가져올 수 있다.")
     void detailWithValidId() {
         taskController.create(task1);
         taskController.create(task2);
@@ -76,7 +76,7 @@ class TaskControllerTest {
         taskController.create(task1);
         taskController.create(task2);
 
-        for (long id : invalidIds) {
+        for (long id : givenInvalidIds) {
             Assertions.assertThrows(
                     TaskNotFoundException.class,
                     () -> taskController.detail(id),
@@ -86,7 +86,7 @@ class TaskControllerTest {
     }
 
     @Test
-    @DisplayName("할 일이 없는 경우 할 일을 create()하면, 비어있지 않은 리스트가 리턴된다.")
+    @DisplayName("할 일이 없는 경우 할 일을 추가하면, 비어있지 않은 리스트가 리턴된다.")
     void createNewTask() {
         taskController.create(task1);
 
@@ -94,7 +94,7 @@ class TaskControllerTest {
     }
 
     @Test
-    @DisplayName("기존 할 일을 찾을 수 있고 변경할 할 일이 주어지면, update()를 했을 때 기존 할 일이 새로운 할 일로 변경된다.")
+    @DisplayName("주어진 id를 가진 할 일을 찾을 수 있고 변경할 수 있다면, 할 일이 변경된다.")
     void updateTaskWithValidId() {
         taskController.create(task1);
 
@@ -105,11 +105,11 @@ class TaskControllerTest {
     }
 
     @Test
-    @DisplayName("기존 할 일을 찾을 수 없으면, NotFound 예외가 발생한다.")
+    @DisplayName("주어진 id를 가진 할 일을 찾을 수 없으면, NotFound 예외가 발생한다.")
     void updateTaskWithInvalidId() {
         taskController.create(task1);
 
-        for (long id : invalidIds) {
+        for (long id : givenInvalidIds) {
             Assertions.assertThrows(
                     TaskNotFoundException.class,
                     () -> taskController.update(id, task2),
@@ -119,14 +119,14 @@ class TaskControllerTest {
     }
 
     @Test
-    @DisplayName("기존 할 일을 찾을 수 있지만 변경할 할 일이 존재하지 않으면, NotFound 예외가 발생한다.")
+    @DisplayName("주어진 id를 가진 할 일이 저장되어 있지만 변경할 내용이 없다면, NPE 가 발생한다.")
     void updateTaskWithInvalidNewTask() {
         taskController.create(task1);
 
         Assertions.assertThrows(
-                TaskNotFoundException.class,
-                () -> taskController.update(task1.getId(), null),
-                "할 일을 찾을 수 없을 경우를 표현하는 예외가 던져져야 한다."
+                NullPointerException.class,
+                () -> taskController.update(1L, null),
+                "NullPointerException 이 던져져야 한다."
         );
     }
 }
