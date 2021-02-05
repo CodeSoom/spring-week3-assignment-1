2f5e72cb980a2a59a88d12994cb9e5dbcef31149
wenodev <logicalstark1105@gmail.com>
2021-02-05 11:56:57 +0900

    비어있는 list 목록 테스트

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -3,6 +3,10 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskControllerTest {
 
@@ -17,6 +21,12 @@ class TaskControllerTest {
         task = new Task();
         task.setId(1L);
         task.setTitle("책읽기");
+    }
+
+    @DisplayName("Task 비어있는 목록")
+    @Test
+    void testListWithoutContent(){
+        assertThat(taskController.list()).isEmpty();
     }
     
 }
