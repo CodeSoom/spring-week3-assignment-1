07a24bf33ce87f39b6dac5de52c1ab8bdd4c5ff2
wenodev <logicalstark1105@gmail.com>
2021-02-05 11:55:06 +0900

    fixture 생성

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,5 +1,22 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
+
 class TaskControllerTest {
+
+    TaskController taskController;
+    TaskService taskService;
+    Task task;
+
+    @BeforeEach
+    void init(){
+        taskService = new TaskService();
+        taskController = new TaskController(taskService);
+        task = new Task();
+        task.setId(1L);
+        task.setTitle("책읽기");
+    }
     
 }
