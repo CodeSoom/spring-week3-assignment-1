2cd875cbd8a3abd87e96277d8a0bb047a9a95427
juunini <juuni.ni.i@gmail.com>
2021-02-05 15:10:45 +0900

    Fix TaskController mvc test

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerMvcTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerMvcTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -14,10 +15,10 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -37,20 +38,31 @@ class TaskControllerMvcTest {
     final String givenTitle = "sample";
     final String givenModifyTitle = "modify sample";
 
+    Task givenTask(long id, String title) {
+        Task task = new Task();
+        task.setId(id);
+        task.setTitle(title);
+        return task;
+    }
+
     @Nested
-    @DisplayName("list 메서드는")
+    @DisplayName("[GET] /tasks 메서드는")
     class Describe_list {
 
         @Nested
         @DisplayName("등록된 Task 가 하나도 없을 때")
         class Context_without_task {
+            @BeforeEach
+            void setup() {
+                List<Task> tasks = new ArrayList<>();
+
+                given(taskService.getTasks())
+                        .willReturn(tasks);
+            }
 
             @Test
             @DisplayName("빈 집합을 리턴한다.")
             void It_returns_void_array() throws Exception {
-                when(taskService.getTasks())
-                        .thenReturn(new ArrayList<>());
-
                 mockMvc.perform(get("/tasks"))
                         .andExpect(content().json("[]"));
             }
@@ -59,16 +71,20 @@ class TaskControllerMvcTest {
         @Nested
         @DisplayName("등록된 Task 가 있을 때")
         class Context_with_task {
+            @BeforeEach
+            void setup() {
+                Task task = givenTask(givenID, givenTitle);
+
+                List<Task> tasks = new ArrayList<>();
+                tasks.add(task);
+
+                given(taskService.getTasks())
+                        .willReturn(tasks);
+            }
 
             @Test
             @DisplayName("Task 의 집합을 리턴한다.")
             void It_returns_exists_array() throws Exception {
-                Task task = new Task();
-                task.setTitle(givenTitle);
-
-                when(taskService.getTasks())
-                        .thenReturn(new ArrayList<>(Collections.singletonList(task)));
-
                 mockMvc.perform(get("/tasks"))
                         .andExpect(content().json(String.format("[{\"title\":\"%s\",\"id\":%d}]", givenTitle, givenID)));
             }
@@ -76,19 +92,21 @@ class TaskControllerMvcTest {
     }
 
     @Nested
-    @DisplayName("detail 메서드는")
+    @DisplayName("[GET] /tasks/{id} 메서드는")
     class Describe_detail {
 
         @Nested
         @DisplayName("찾는 id가 없을 때")
         class Context_without_target_id {
+            @BeforeEach
+            void setup() {
+                given(taskService.getTask(givenID))
+                        .willThrow(new TaskNotFoundException(givenID));
+            }
 
             @Test
             @DisplayName("status not found 를 던진다.")
             void It_throws_task_not_found_exception() throws Exception {
-                when(taskService.getTask(givenID))
-                        .thenThrow(new TaskNotFoundException(givenID));
-
                 mockMvc.perform(get("/tasks/{id}", givenID))
                         .andExpect(status().isNotFound());
             }
@@ -97,17 +115,17 @@ class TaskControllerMvcTest {
         @Nested
         @DisplayName("찾는 id가 있을 때")
         class Context_with_target_id {
+            @BeforeEach
+            void setup() {
+                Task task = givenTask(givenID, givenTitle);
+
+                given(taskService.getTask(givenID))
+                        .willReturn(task);
+            }
 
             @Test
             @DisplayName("Task 를 리턴한다.")
             void It_returns_task() throws Exception {
-                Task task = new Task();
-                task.setTitle(givenTitle);
-                task.setId(givenID);
-
-                when(taskService.getTask(givenID))
-                        .thenReturn(task);
-
                 mockMvc.perform(get("/tasks/{id}", givenID))
                         .andExpect(content().json(String.format("{\"title\":\"%s\",\"id\":%d}", givenTitle, givenID)));
             }
@@ -115,21 +133,21 @@ class TaskControllerMvcTest {
     }
 
     @Nested
-    @DisplayName("create 메서드는")
+    @DisplayName("[POST] /tasks 메서드는")
     class Describe_create {
+        @BeforeEach
+        void setup() {
+            Task task = givenTask(givenID, givenTitle);
+
+            given(taskService.createTask(any(Task.class)))
+                    .willReturn(task);
+        }
 
         @Test
         @DisplayName("생성된 Task 를 리턴한다.")
         void It_returns_created_task() throws Exception {
-            Task task = new Task();
-            task.setTitle(givenTitle);
-            task.setId(givenID);
-
-            when(taskService.createTask(any()))
-                    .thenReturn(task);
-
-            String postContent = String.format("{\"title\":\"%s\"}", task.getTitle());
-            String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", task.getTitle(), task.getId());
+            String postContent = String.format("{\"title\":\"%s\"}", givenTitle);
+            String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", givenTitle, givenID);
 
             mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(postContent))
                     .andExpect(content().json(receiveJSON));
@@ -137,19 +155,21 @@ class TaskControllerMvcTest {
     }
 
     @Nested
-    @DisplayName("update 메서드는")
+    @DisplayName("[PUT] /tasks/{id} 메서드는")
     class Describe_update {
 
         @Nested
         @DisplayName("대상 id가 없을 때")
         class Context_without_target_id {
+            @BeforeEach
+            void setup() {
+                given(taskService.updateTask(any(long.class), any(Task.class)))
+                        .willThrow(new TaskNotFoundException(givenID));
+            }
 
             @Test
             @DisplayName("status not found 를 던진다.")
             void It_throws_task_not_found_exception() throws Exception {
-                when(taskService.updateTask(any(), any()))
-                        .thenThrow(new TaskNotFoundException(givenID));
-
                 String postContent = String.format("{\"title\":\"%s\"}", givenModifyTitle);
 
                 mockMvc.perform(put("/tasks/{id}", givenID).contentType(MediaType.APPLICATION_JSON).content(postContent))
@@ -160,19 +180,19 @@ class TaskControllerMvcTest {
         @Nested
         @DisplayName("대상 id가 있을 때")
         class Context_with_target_id {
+            @BeforeEach
+            void setup() {
+                Task task = givenTask(givenID, givenModifyTitle);
+
+                given(taskService.updateTask(any(long.class), any(Task.class)))
+                        .willReturn(task);
+            }
 
             @Test
             @DisplayName("변경된 Task 를 리턴한다.")
             void It_returns_modified_task() throws Exception {
-                Task task = new Task();
-                task.setTitle(givenModifyTitle);
-                task.setId(givenID);
-
-                when(taskService.updateTask(any(), any()))
-                        .thenReturn(task);
-
-                String postContent = String.format("{\"title\":\"%s\"}", task.getTitle());
-                String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", task.getTitle(), task.getId());
+                String postContent = String.format("{\"title\":\"%s\"}", givenModifyTitle);
+                String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", givenModifyTitle, givenID);
 
                 mockMvc.perform(put("/tasks/{id}", givenID).contentType(MediaType.APPLICATION_JSON).content(postContent))
                         .andExpect(content().json(receiveJSON));
@@ -181,19 +201,21 @@ class TaskControllerMvcTest {
     }
 
     @Nested
-    @DisplayName("patch 메서드는")
+    @DisplayName("[PATCH] /tasks/{id} 메서드는")
     class Describe_patch {
 
         @Nested
         @DisplayName("대상 id가 없을 때")
         class Context_without_target_id {
+            @BeforeEach
+            void setup() {
+                given(taskService.updateTask(any(long.class), any(Task.class)))
+                        .willThrow(new TaskNotFoundException(givenID));
+            }
 
             @Test
             @DisplayName("status not found 를 던진다.")
             void It_throws_task_not_found_exception() throws Exception {
-                when(taskService.updateTask(any(), any()))
-                        .thenThrow(new TaskNotFoundException(givenID));
-
                 String postContent = String.format("{\"title\":\"%s\"}", givenModifyTitle);
 
                 mockMvc.perform(patch("/tasks/{id}", givenID).contentType(MediaType.APPLICATION_JSON).content(postContent))
@@ -204,19 +226,19 @@ class TaskControllerMvcTest {
         @Nested
         @DisplayName("대상 id가 있을 때")
         class Context_with_target_id {
+            @BeforeEach
+            void setup() {
+                Task task = givenTask(givenID, givenModifyTitle);
+
+                given(taskService.updateTask(any(long.class), any(Task.class)))
+                        .willReturn(task);
+            }
 
             @Test
             @DisplayName("변경된 Task 를 리턴한다.")
             void It_returns_modified_task() throws Exception {
-                Task task = new Task();
-                task.setTitle(givenModifyTitle);
-                task.setId(givenID);
-
-                when(taskService.updateTask(any(), any()))
-                        .thenReturn(task);
-
-                String postContent = String.format("{\"title\":\"%s\"}", task.getTitle());
-                String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", task.getTitle(), task.getId());
+                String postContent = String.format("{\"title\":\"%s\"}", givenModifyTitle);
+                String receiveJSON = String.format("{\"title\":\"%s\",\"id\":%d}", givenModifyTitle, givenID);
 
                 mockMvc.perform(patch("/tasks/{id}", givenID).contentType(MediaType.APPLICATION_JSON).content(postContent))
                         .andExpect(content().json(receiveJSON));
@@ -225,18 +247,20 @@ class TaskControllerMvcTest {
     }
 
     @Nested
-    @DisplayName("delete 메서드는")
+    @DisplayName("[DELETE] /tasks/{id} 메서드는")
     class Describe_delete {
         @Nested
         @DisplayName("대상 id가 없을 때")
         class Context_without_target_id {
+            @BeforeEach
+            void setup() {
+                given(taskService.deleteTask(givenID))
+                        .willThrow(new TaskNotFoundException(givenID));
+            }
 
             @Test
             @DisplayName("Task 를 찾을 수 없다는 예외를 던진다.")
             void It_throws_task_not_found_exception() throws Exception {
-                when(taskService.deleteTask(givenID))
-                        .thenThrow(new TaskNotFoundException(givenID));
-
                 mockMvc.perform(delete("/tasks/{id}", givenID))
                         .andExpect(status().isNotFound());
             }
@@ -245,13 +269,15 @@ class TaskControllerMvcTest {
         @Nested
         @DisplayName("대상 id가 있을 때")
         class Context_with_target_id {
+            @BeforeEach
+            void setup() {
+                given(taskService.getTask(givenID))
+                        .willThrow(new TaskNotFoundException(givenID));
+            }
 
             @Test
             @DisplayName("삭제 후 대상 id를 조회하면 status not found 를 던진다.")
             void It_returns_modified_task() throws Exception {
-                when(taskService.getTask(givenID))
-                        .thenThrow(new TaskNotFoundException(givenID));
-
                 mockMvc.perform(delete("/tasks/{id}", givenID));
                 mockMvc.perform(get("/tasks/{id}", givenID))
                         .andExpect(status().isNotFound());
