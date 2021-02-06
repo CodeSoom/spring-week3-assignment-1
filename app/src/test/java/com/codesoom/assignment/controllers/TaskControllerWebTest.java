0c4a73a97640360c9bb68c4f998990728389b0d6
mikekang47 <gyeong0308@gmail.com>
2021-02-06 15:22:40 +0900

    Create TaskControllerWebTest.java and make list method

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -0,0 +1,23 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class TaskControllerWebTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void list() throws Exception {
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk());
+    }
+
+
+}
