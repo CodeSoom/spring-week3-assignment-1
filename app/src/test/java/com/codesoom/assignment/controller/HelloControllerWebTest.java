6b8b627460b23903f72fb77d2cf9e39c4c2cefe2
developerOlive <chloehiho@gmail.com>
2021-02-06 22:18:18 +0900

    Refactor : HelloControllerWebTest 어노테이션 추가

--- app/src/test/java/com/codesoom/assignment/controller/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/HelloControllerWebTest.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.controller;
 
+import com.codesoom.assignment.controllers.HelloController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.hamcrest.Matchers.containsString;
@@ -9,6 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(HelloController.class)
 public class HelloControllerWebTest {
 
     @Autowired
