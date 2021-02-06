83ed16a47b661e9183f2ca1b8a05bc99f8ba27fb
thkim <thkim2017@gmail.com>
2021-02-06 19:04:47 +0900

    Add a test about detail title when getTasks in TaskControllerWebTest Apply given statement

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -10,8 +10,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -25,8 +30,15 @@ public class TaskControllerWebTest {
 
     @Test
     void list() throws Exception {
+        List<Task> tasks = new ArrayList<>();
+        Task task = new Task();
+        task.setTitle("Test Task");
+        tasks.add(task);
+        given(taskService.getTasks()).willReturn(tasks);
+
         mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Task")));
     }
 
     @Test
