d408bb1853737eb6858a30d64699ad3e40aa0ac7
thkim <thkim2017@gmail.com>
2021-02-06 18:51:47 +0900

    Add tests for detail in TaskControllerWebTest Apply MockBean and given statement

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -1,21 +1,49 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(TaskController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TaskControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private TaskService taskService;
 
     @Test
     void list() throws Exception {
         mockMvc.perform(get("/tasks"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void detailWithValidId() throws Exception {
+        Task task = new Task();
+        given(taskService.getTask(1L)).willReturn(task);
+
+        mockMvc.perform(get("/tasks/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void detailWithInvalidId() throws Exception {
+        Task task = new Task();
+        given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
+
+        mockMvc.perform(get("/tasks/100"))
+                .andExpect(status().isNotFound());
     }
 }
