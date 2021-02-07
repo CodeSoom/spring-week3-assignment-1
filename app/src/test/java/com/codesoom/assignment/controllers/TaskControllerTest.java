0eb238f691f492d96b3ee91dc636be3ad6d4c013
thkim <thkim2017@gmail.com>
2021-02-07 11:03:49 +0900

    In setUp() a task is made and is added to tasks Plus add an occasion when taskService.getTasks() is given

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -5,7 +5,11 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
@@ -17,11 +21,27 @@ class TaskControllerTest {
     void setUp() {
         taskService = mock(TaskService.class);
         controller = new TaskController(taskService);
+
+        List<Task> tasks = new ArrayList<>();
+        Task task = new Task();
+        task.setTitle("Task1");
+        tasks.add(task);
+
+        given(taskService.getTasks()).willReturn(tasks);
     }
 
     @Test
     void listWithoutTasks() {
+        given(taskService.getTasks()).willReturn(new ArrayList<>());
+
         assertThat(controller.list()).isEmpty();
+
+        verify(taskService).getTasks();
+    }
+
+    @Test
+    void listWithSomeTasks() {
+        assertThat(controller.list()).isNotEmpty();
 
         verify(taskService).getTasks();
     }
