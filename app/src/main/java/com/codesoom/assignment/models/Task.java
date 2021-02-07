63ce3f6f05d6b201a938551750340e07728147dd
etff <wish2rich@gmail.com>
2021-02-07 14:00:20 +0900

    test: add equals and hashcode test

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.models;
 
+import java.util.Objects;
+
 public class Task {
     private Long id;
 
@@ -19,5 +21,32 @@ public class Task {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    /**
+     * 할 일의 값이 같은지 비교합니다
+     * @param o 비교 대상
+     * @return
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Task)) {
+            return false;
+        }
+        Task task = (Task) o;
+        return Objects.equals(getId(), task.getId()) &&
+                Objects.equals(getTitle(), task.getTitle());
+    }
+
+    /**
+     * 할 일의 해쉬값을 리턴합니다.
+     * @return
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle());
     }
 }
--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.models;
 
+import com.codesoom.assignment.App;
+import com.codesoom.assignment.dto.ErrorResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,8 +11,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("Task 클래스")
 public class TaskTest {
     private static final Long GIVEN_ID = 1L;
-    private static final Long CHANGE_ID = 1L;
+    private static final Long CHANGE_ID = 2L;
     private static final String GIVEN_TITLE = "homework";
+    private static final String CHANGE_TITLE = "work";
 
     private Task task;
 
@@ -51,13 +54,75 @@ public class TaskTest {
     @DisplayName("getId은 Task의 id을 리턴한다")
     @Test
     void getId() {
-        assertThat(task.getId()).isEqualTo(CHANGE_ID);
+        assertThat(task.getId()).isEqualTo(GIVEN_ID);
     }
 
     @DisplayName("getTitle은 Task의 title을 리턴한다")
     @Test
     void getTitle() {
         assertThat(task.getTitle()).isEqualTo(GIVEN_TITLE);
+    }
+
+    @DisplayName("Equals와 해쉬코드 값을 테스트")
+    @Test
+    void equal_hashcode() {
+        Task task1 = new Task();
+        task1.setId(GIVEN_ID);
+        task1.setTitle(GIVEN_TITLE);
+
+        Task task2 = new Task();
+        task2.setId(GIVEN_ID);
+        task2.setTitle(GIVEN_TITLE);
+
+        assertThat(task1.equals(task2)).isTrue();
+        assertThat(task1.hashCode()).isEqualTo(task2.hashCode());
+    }
+
+    @Test
+    void equal_with_different_object() {
+        Task task = new Task();
+        App app = new App();
+
+        assertThat(task.equals(app)).isFalse();
+    }
+
+    @Test
+    void equal_with_different_id() {
+        Task task1 = new Task();
+        task1.setId(GIVEN_ID);
+        task1.setTitle(GIVEN_TITLE);
+
+        Task task2 = new Task();
+        task2.setId(CHANGE_ID);
+        task2.setTitle(GIVEN_TITLE);
+
+        assertThat(task1.equals(task2)).isFalse();
+    }
+
+    @Test
+    void equal_with_different_title() {
+        Task task1 = new Task();
+        task1.setId(GIVEN_ID);
+        task1.setTitle(GIVEN_TITLE);
+
+        Task task2 = new Task();
+        task2.setId(GIVEN_ID);
+        task2.setTitle(CHANGE_TITLE);
+
+        assertThat(task1.equals(task2)).isFalse();
+    }
+
+    @Test
+    void equal_with_different_id_title() {
+        Task task1 = new Task();
+        task1.setId(GIVEN_ID);
+        task1.setTitle(GIVEN_TITLE);
+
+        Task task2 = new Task();
+        task2.setId(CHANGE_ID);
+        task2.setTitle(CHANGE_TITLE);
+
+        assertThat(task1.equals(task2)).isFalse();
     }
 
 }
