02270bf54089d5add444035e829cf7ada1ddfaf6
Changsu Im <imcxsu@gmail.com>
2021-05-18 00:53:39 +0900

    Add unit test for HelloController

---
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -0,0 +1,12 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, world!";
+    }
+}
