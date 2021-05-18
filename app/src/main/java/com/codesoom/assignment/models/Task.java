cf7c69205ca311e0061070b900f5d6b75558742a
humblEgo <injun.woo30000@gmail.com>
2021-05-18 15:33:51 +0900

    equals, hashCode 오버라이딩
    테스트 작성시 반환 값 검증을 위해 Task 객체의 equals, hashCode
    메서드를 오버라이딩했습니다. 결과적으로 프로퍼티가 동일하면 동일한
    Task로 판단하게됩니다.

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -2,7 +2,6 @@ package com.codesoom.assignment.models;
 
 public class Task {
     private Long id;
-
     private String title;
 
     public Long getId() {
@@ -19,5 +18,22 @@ public class Task {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Task)) {
+            return false;
+        }
+        return id.equals(((Task)obj).getId()) &&
+                title.equals(((Task)obj).getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        // id, title이 null를 고려한 hashCode.. 이게.. 맞나..??
+        int result = id != null ? id.hashCode() * 31 : 0;
+        result += title != null ? title.hashCode() * 31 : 0;
+        return result;
     }
 }
