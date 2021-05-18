a3391c4b3fad204457c462bf11dc24b348eff1e6
Flying-Fisherman <ghdud7037@gmail.com>
2021-05-19 00:21:56 +0900

    Modify TaskControllerTest and TaskServiceTest
    TaskControllerTest, TaskServiceTest의 로직을 작성, 수정하였습니다.
    @DisplayName 어노테이션을 사용하여 각 테스트에 대한 설명을 작성하였습니다.
    
    내일은 HelloController 유닛, Task 모델의 Test 로직 및 MockMVC를 통한 테스트 로직을 작성 할 예정입니다.

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,19 +20,93 @@ class TaskServiceTest {
         taskService = new TaskService();
     }
 
+    void createTestTask() {
+        Task task = new Task();
+
+        task.setTitle("Test1");
+        taskService.createTask(task);
+
+        task.setTitle("Test2");
+        taskService.createTask(task);
+    }
+
+    @DisplayName("Test to get Task list to use \"getTasks\" on TaskService class")
     @Test
     void getTasks() {
         assertThat(taskService.getTasks()).isEmpty();
     }
 
+    @DisplayName("Test to get specific Task to use \"getTask\" on TaskService class")
     @Test
     void getTask() {
         Task task = new Task();
         task.setTitle(TASK_TITLE);
-        taskService.createTask(task);
+        createTestTask();
 
-        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("test");
+        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("Test1");
+        assertThat(taskService.getTask(2L).getTitle()).isEqualTo("Test2");
 
         assertThatThrownBy(() -> taskService.getTask(100L)).isInstanceOf(TaskNotFoundException.class);
+    }
+
+    @DisplayName("Test to create Task to use \"createTask\" on TaskService class")
+    @Test
+    void createTask() {
+        Task task = new Task();
+
+        assertThatThrownBy(() -> taskService.getTask(1L)).isInstanceOf(TaskNotFoundException.class);
+        assertThatThrownBy(() -> taskService.getTask(2L)).isInstanceOf(TaskNotFoundException.class);
+
+        task.setTitle("Test1");
+        taskService.createTask(task);
+
+        task.setTitle("Test2");
+        taskService.createTask(task);
+
+        assertThat(taskService.getTasks().get(0).getId()).isEqualTo(1L);
+        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("Test1");
+
+        assertThat(taskService.getTasks().get(1).getId()).isEqualTo(2L);
+        assertThat(taskService.getTask(2L).getTitle()).isEqualTo("Test2");
+    }
+
+    @DisplayName("Test to update Task's title to use \"updateTask\" on TaskService class")
+    @Test
+    void updateTask() {
+        Task task = new Task();
+        task.setTitle("New Test1");
+
+        assertThatThrownBy(() -> taskService.updateTask(1L, task)).isInstanceOf(TaskNotFoundException.class);
+
+        createTestTask();
+
+        taskService.updateTask(1L, task);
+
+        task.setTitle("New Test2");
+        taskService.updateTask(2L, task);
+
+        assertThat(taskService.getTask(1L).getTitle()).isEqualTo("New Test1");
+        assertThat(taskService.getTask(2L).getTitle()).isEqualTo("New Test2");
+    }
+
+    @DisplayName("Test to delete specific Task to use \"deleteTask\" on TaskService class")
+    @Test
+    void deleteTask() {
+        assertThatThrownBy(() -> taskService.getTask(1L)).isInstanceOf(TaskNotFoundException.class);
+        assertThatThrownBy(() -> taskService.getTask(2L)).isInstanceOf(TaskNotFoundException.class);
+
+        createTestTask();
+
+        assertThat(taskService.getTask(1L)).isNotNull();
+        assertThat(taskService.getTask(2L)).isNotNull();
+
+        taskService.deleteTask(1L);
+
+        assertThatThrownBy(() -> taskService.getTask(1L)).isInstanceOf(TaskNotFoundException.class);
+        assertThat(taskService.getTask(2L)).isNotNull();
+
+        taskService.deleteTask(2L);
+
+        assertThatThrownBy(() -> taskService.getTask(2L)).isInstanceOf(TaskNotFoundException.class);
     }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -32,6 +33,7 @@ class TaskControllerTest {
         return task;
     }
 
+    @DisplayName("Test to get Task list to use \"list\" on TaskController class")
     @Test
     void list() {
         assertThat(controller.list()).isEmpty();
@@ -42,6 +44,7 @@ class TaskControllerTest {
         assertThat(controller.list()).hasSize(2);
     }
 
+    @DisplayName("Test to get specific Task to use \"getDetail\" on TaskController class")
     @Test
     void getDetail() {
         assertThatThrownBy(() -> controller.detail(1L)).isInstanceOf(TaskNotFoundException.class);
@@ -53,6 +56,7 @@ class TaskControllerTest {
         assertThat(controller.detail(2L).getTitle()).isEqualTo("test2");
     }
 
+    @DisplayName("Test to create Task to use \"createNewTask\" on TaskController class")
     @Test
     void createNewTask() {
         assertThat(controller.list()).isEmpty();
@@ -70,6 +74,7 @@ class TaskControllerTest {
         assertThat(controller.list()).hasSize(2);
     }
 
+    @DisplayName("Test to update Task's title to use \"updateTask\" on TaskController class")
     @Test
     void updateTask() {
         createTestTask("test");
@@ -84,6 +89,7 @@ class TaskControllerTest {
         assertThatThrownBy(() -> controller.detail(2L)).isInstanceOf(TaskNotFoundException.class);
     }
 
+    @DisplayName("Test to update Task's title to use \"patchTask\" on TaskController class")
     @Test
     void patchTask() {
         createTestTask("test");
@@ -98,6 +104,7 @@ class TaskControllerTest {
         assertThatThrownBy(() -> controller.detail(2L)).isInstanceOf(TaskNotFoundException.class);
     }
 
+    @DisplayName("Test to delete Task to use \"deleteTask\" on TaskController class")
     @Test
     void deleteTask() {
         createTestTask("test");
