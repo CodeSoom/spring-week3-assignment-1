fbb310ef6d7acc2b883bfea33d01f4400bce6938
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-05-18 22:28:43 +0900

    Make a unit test about "TaskController.detail"
    - Setup one sample task.
    = Test task id and title.

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -6,6 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
     @Test
+    void application() {
+        App.main(new String[] {});
+    }
+
+    @Test
     void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -9,18 +9,38 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskControllerTest {
+
+    private final Long ID = 1L;
+    private final String TITLE = "SAMPLE TITLE";
+
     private TaskController controller;
     private TaskService taskService;
+    private Task task;
 
     @BeforeEach
     void setUp() {
         taskService = new TaskService();
         controller = new TaskController(taskService);
+
+        task = new Task();
+        task.setId(ID);
+        task.setTitle(TITLE);
+
+        taskService.createTask(task);
     }
 
     @Test
-    void list() {
+    void tasks() {
         assertThat(controller.list()).isEmpty();
+    }
+
+    @Test
+    void task() {
+        Task task = controller.detail(ID);
+
+        assertThat(task).isNotNull();
+        assertThat(task.getId()).isEqualTo(ID);
+        assertThat(task.getTitle()).isEqualTo(TITLE);
     }
 
     @Test
