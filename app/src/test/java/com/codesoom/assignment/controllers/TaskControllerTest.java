f02586ea53d56f4b08dc6b63167329f4ddd6ed32
DevRunner21 <devrunner21@gmail.com>
2021-05-18 18:40:21 +0900

    프로젝트 소스 수정
    - 아샬님이 작성하신 버전으로 되어 있어서 본인 버전의 소스로 변경
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,28 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @Test
+    void list() {
+    }
+
+    @Test
+    void detail() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
