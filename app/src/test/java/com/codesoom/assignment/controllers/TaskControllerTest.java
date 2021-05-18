bea3053d31d8e1ac4815e986e500b749ef7e48e5
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-05-18 22:45:09 +0900

    Make tests about "update" and "patch"
    - Update test for task alreay created.
    - Patch test for task new created.

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -5,6 +5,8 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +14,7 @@ class TaskControllerTest {
 
     private final Long ID = 1L;
     private final String TITLE = "SAMPLE TITLE";
+    private final String UPDATED = "[UPDATED]";
 
     private TaskController controller;
     private TaskService taskService;
@@ -31,7 +34,9 @@ class TaskControllerTest {
 
     @Test
     void tasks() {
-        assertThat(controller.list()).isEmpty();
+        List<Task> tasks = controller.list();
+        assertThat(tasks).isNotEmpty();
+        assertThat(tasks).hasSize(1);
     }
 
     @Test
@@ -52,12 +57,43 @@ class TaskControllerTest {
         task.setTitle("Test2");
         controller.create(task);
 
-        assertThat(controller.list()).hasSize(2);
+        assertThat(controller.list()).hasSize(3);
 
-        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
-        assertThat(controller.list().get(0).getTitle()).isEqualTo("Test");
+        assertThat(controller.list().get(0).getId()).isEqualTo(ID);
+        assertThat(controller.list().get(0).getTitle()).isEqualTo(TITLE);
 
         assertThat(controller.list().get(1).getId()).isEqualTo(2L);
-        assertThat(controller.list().get(1).getTitle()).isEqualTo("Test2");
+        assertThat(controller.list().get(1).getTitle()).isEqualTo("Test");
+
+        assertThat(controller.list().get(2).getId()).isEqualTo(3L);
+        assertThat(controller.list().get(2).getTitle()).isEqualTo("Test2");
+    }
+
+    @Test
+    void updateTask() {
+        Task task = controller.detail(ID);
+        task.setTitle(TITLE + UPDATED);
+
+        Task updatedTask = controller.update(ID, task);
+
+        assertThat(updatedTask).isNotNull();
+        assertThat(updatedTask.getId()).isEqualTo(ID);
+        assertThat(updatedTask.getTitle()).isEqualTo(TITLE + UPDATED);
+    }
+
+    @Test
+    void patchTask() {
+        Task newTask = new Task();
+        newTask.setTitle(TITLE);
+
+        newTask = controller.create(newTask);
+        Long newTaskId = newTask.getId();
+        newTask.setTitle(TITLE + UPDATED);
+
+        Task patchedTask = controller.update(newTaskId, newTask);
+
+        assertThat(patchedTask).isNotNull();
+        assertThat(patchedTask.getId()).isEqualTo(newTaskId);
+        assertThat(patchedTask.getTitle()).isEqualTo(TITLE + UPDATED);
     }
 }
