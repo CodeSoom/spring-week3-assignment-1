021611064604c1b01e72786252c54c1a8ea576d1
Changsu Im <imcxsu@gmail.com>
2021-05-19 02:08:16 +0900

    Add unit test for application context

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,10 +8,21 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class AppTest {
     @Test
+    @DisplayName("스프링 애플리케이션을 실행한다.")
+    void springApplicationContextTest() {
+        String[] args = new String[]{
+                "--spring.main.banner-mode=off",
+                "--logging.level.root=ERROR"
+        };
+        App.main(args);
+    }
+
+    @Test
+    @DisplayName("기본 인사를 반환한다.")
     void appHasAGreeting() {
         App classUnderTest = new App();
         assertNotNull(classUnderTest.getGreeting(),
                       "app should have a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+        assertEquals("Hello, world!", classUnderTest.getGreeting());
     }
 }
