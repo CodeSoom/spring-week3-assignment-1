52153e4c5d7189d4ad8e877889c1bd5222afe4f8
Joowan Kim <rlawndhks217@gmail.com>
2021-05-20 07:51:57 +0900

    Add tests for TaskController's all methods

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,11 +1,13 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TaskControllerTest {
     private TaskController controller;
@@ -19,7 +21,6 @@ class TaskControllerTest {
 
     @Test
     void list() {
-
         assertThat(controller.list()).isEmpty();
     }
 
@@ -37,4 +38,38 @@ class TaskControllerTest {
 
         assertThat(controller.list().get(0).getId()).isEqualTo(1L);
     }
+
+    @Test
+    void detail() {
+        Task task = new Task();
+        task.setTitle("task test");
+        controller.create(task);
+        assertThat(controller.detail(1L).getTitle()).isEqualTo(task.getTitle());
+        assertThatThrownBy(() -> controller.detail(100L)).isInstanceOf(TaskNotFoundException.class);
+    }
+
+    @Test
+    void update() {
+        Task src = new Task();
+        src.setTitle("task test");
+        controller.create(src);
+        assertThat(controller.detail(1L).getTitle()).isEqualTo(src.getTitle());
+
+        Task task = new Task();
+        task.setTitle("updated task");
+        assertThat(controller.update(1L, task).getTitle()).isEqualTo(task.getTitle());
+    }
+
+    @Test
+    void delete() {
+        Task task = new Task();
+        task.setTitle("task test");
+        controller.create(task);
+        assertThat(controller.detail(1L).getTitle()).isEqualTo(task.getTitle());
+
+        controller.delete(1L);
+        assertThat(controller.list()).isEmpty();
+        assertThatThrownBy(() -> controller.detail(1L)).isInstanceOf(TaskNotFoundException.class);
+    }
+
 }
