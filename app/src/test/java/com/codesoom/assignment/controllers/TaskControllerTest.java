75b7802143d49c513ee38817357fbcf53685e2f3
Joowan Kim <rlawndhks217@gmail.com>
2021-05-19 20:56:33 +0900

    Add TaskControllerTest

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,21 +1,40 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskControllerTest {
+    private TaskController controller;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        this.taskService = new TaskService();
+        this.controller = new TaskController(this.taskService);
+    }
+
     @Test
     void list() {
-//        TaskController controller = new TaskController();
-//        assertThat(controller.list()).isEmpty();
+
+        assertThat(controller.list()).isEmpty();
     }
 
     @Test
     void createNewTask() {
-//        TaskController controller = new TaskController();
-//        Task task = new Task();
-//        task.setTitle("");
+        Task task = new Task();
+        task.setTitle("");
+
+        controller.create(task);
+        assertThat(controller.list()).isNotEmpty();
+        assertThat(controller.list()).hasSize(1);
+
+        controller.create(task);
+        assertThat(controller.list()).hasSize(2);
+
+        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
     }
 }
