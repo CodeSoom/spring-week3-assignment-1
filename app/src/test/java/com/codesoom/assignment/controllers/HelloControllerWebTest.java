f3901f49d18f0cd5a9f732ba0899d46f181910a2
humblEgo <injun.woo30000@gmail.com>
2021-05-19 21:14:32 +0900

    refactor HelloControllerWebTest with BDD pattern
    Describe-Context-It 패턴으로 리팩터링하였습니다.

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,15 +15,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@DisplayName("HelloController 클래스 Web")
 class HelloControllerWebTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void sayHello() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello, world!")));
+    @Nested
+    @DisplayName("'/' URL로")
+    class Describe_of_root_url {
+
+
+        @Nested
+        @DisplayName("만약 GET 요청을 보내면")
+        class Context_of_get {
+
+            @Test
+            @DisplayName("200 OK 상태코드와 'Hello, world!' body를 응답한다")
+            public void it_returns_ok_with_valid_string() throws Exception {
+                mockMvc.perform(get("/"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("Hello, world!")));
+            }
+        }
     }
 }
