60d7fb5f6409adf5d0182f62fa65b5069c449382
imdaebeen <imdaebeen_09@naver.com>
2021-05-19 16:29:27 +0900

    TaskServiceTest TaskControllerTest

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,14 +1,41 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskControllerTest {
+    private TaskController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new TaskController(new TaskService());
+    }
+
     @Test
     void list() {
-        TaskController controller = new TaskController();
+
         assertThat(controller.list()).isEmpty();
+    }
+
+    @Test
+    void createNewTask(){
+        Task task = new Task();
+
+        task.setTitle("TEST1");
+        controller.create(task);
+
+        task.setTitle("TEST2");
+        controller.create(task);
+
+        assertThat(controller.list()).hasSize(2);
+        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
+        assertThat(controller.list().get(0).getTitle()).isEqualTo("TEST1");
+        assertThat(controller.list().get(1).getId()).isEqualTo(2L);
+        assertThat(controller.list().get(1).getTitle()).isEqualTo("TEST2");
     }
 }
