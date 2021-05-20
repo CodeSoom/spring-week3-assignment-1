bf221a2b0ebd3d17a0eae43465000435ffa5919d
DevRunner21 <devrunner21@gmail.com>
2021-05-20 09:50:30 +0900

    기존에 정의된 ConrollerAdvice 클래스명을 변경하라
    - GlobalExceptionHandler -> TaskExceptionHandler
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/common/aop/TaskExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/common/aop/TaskExceptionHandler.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 /**
- * 예외가 던져졌을 떄, 해당 예외에 맞는 Response를 반환합니다.
+ * 할 일 관련 예외가 던져졌을 떄, 해당 예외에 맞는 Response를 반환합니다.
  */
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class TaskExceptionHandler {
 
-    private Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+    private Logger log = LoggerFactory.getLogger(TaskExceptionHandler.class);
 
     // taskId에 맞는 할 일을 못찾은 경우 던져진 예외를 처리합니다.
     @ExceptionHandler(TaskNotFoundException.class)
