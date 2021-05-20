1fd961025eedb46adc066206a8f7f2191ed47048
humblEgo <injun.woo30000@gmail.com>
2021-05-20 20:42:14 +0900

    refactor TaskTest concerning scope
    중복과 가독성을 고려하여,
    It 스코프내에 선언한 메소드, 변수들을 Describe, Context 스코프로 이동
    시켰습니다.

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.models;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("Task 클래스의")
 class TaskTest {
 
+    private Task generateTask(long id, String title) {
+        Task task = new Task();
+        task.setId(id);
+        task.setTitle(title);
+        return task;
+    }
+
     @Nested
     @DisplayName("equals 메소드는")
     class Describe_of_equals {
@@ -17,19 +25,18 @@ class TaskTest {
         @DisplayName("만약 Task Type과 관련없는 객체가 주어진다면")
         class Context_with_different_type_of_object {
 
-            final private Task task1;
+            private Task task1;
+            private Object notTask;
 
-            Context_with_different_type_of_object() {
-                this.task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
+            @BeforeEach
+            void setNonTaskObject() {
+                this.task1 = generateTask(1L, "task1");
+                this.notTask = new Object();
             }
 
             @Test
             @DisplayName("false를 리턴한다")
             void it_returns_false() {
-                Object notTask = new Object();
-
                 assertThat(task1.equals(notTask))
                         .isFalse();
             }
@@ -37,23 +44,21 @@ class TaskTest {
 
         @Nested
         @DisplayName("만약 title이 다른 Task 객체가 주어진다면")
-        class Context_with_differnt_title {
+        class Context_with_different_title {
 
-            final private Task task1;
+            private Task task1;
+            private Task task2;
 
-            Context_with_differnt_title() {
-                this.task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
+            @BeforeEach
+            void setTasksTitleDifferent() {
+                long sameId = 1L;
+                this.task1 = generateTask(sameId, "task1");
+                this.task2 = generateTask(sameId, "differentTitle");
             }
 
             @Test
             @DisplayName("false를 리턴한다")
-            void it_retuns_false() {
-                Task task2 = new Task();
-                task2.setId(1L);
-                task2.setTitle("differentTitle");
-
+            void it_returns_false() {
                 assertThat(task1.equals(task2))
                         .isFalse();
             }
@@ -61,24 +66,24 @@ class TaskTest {
 
         @Nested
         @DisplayName("만약 id가 다른 Task 객체가 주어진다면")
-        class Context_with_differnt_id {
+        class Context_with_different_id {
 
-            final private Task task1;
+            private Task task1;
+            private Task task2;
 
-            Context_with_differnt_id() {
-                this.task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
+            @BeforeEach
+            void setTaskIdDifferent() {
+                long id = 1L;
+                long differentId = id + 1;
+                String sameTitle = "sameTitle";
+
+                this.task1 = generateTask(id, sameTitle);
+                this.task2 = generateTask(differentId, sameTitle);
             }
 
             @Test
             @DisplayName("false를 리턴한다")
             void it_returns_false() {
-                Task task2 = new Task();
-                long differentId = task1.getId() + 1L;
-                task2.setId(differentId);
-                task2.setTitle("task1");
-
                 assertThat(task1.equals(task2))
                         .isFalse();
             }
@@ -88,26 +93,25 @@ class TaskTest {
         @DisplayName("만약 id와 title이 같은 객체가 주어진다면")
         class Context_with_attributes_are_same {
 
-            final private Task task1;
+            private Task task1;
+            private Task task2;
 
-            Context_with_attributes_are_same() {
-                this.task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
+            @BeforeEach
+            void setTasksIdAndTitleSame() {
+                long sameId = 1L;
+                String sameTitle = "sameTitle";
+
+                this.task1 = generateTask(sameId, sameTitle);
+                this.task2 = generateTask(sameId, sameTitle);
             }
 
             @Test
             @DisplayName("true를 리턴한다")
             void it_returns_true() {
-                Task task2 = new Task();
-                task2.setId(task1.getId());
-                task2.setTitle(task1.getTitle());
-
                 assertThat(task1.equals(task2))
                         .isTrue();
             }
         }
-
     }
 
     @Nested
@@ -118,17 +122,19 @@ class TaskTest {
         @DisplayName("만약 title이 다른 Task 두 객체로부터 호출된다면")
         class Context_with_differnt_title {
 
+            private Task task1;
+            private Task task2;
+
+            @BeforeEach
+            void setTasksTitleDifferent() {
+                long sameId = 1L;
+                this.task1 = generateTask(sameId, "task1");
+                this.task2 = generateTask(sameId, "differentTitle");
+            }
+
             @Test
             @DisplayName("각각 다른 hashCode를 반환한다")
             void it_returns_different_hash_code() {
-                Task task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
-
-                Task task2 = new Task();
-                task2.setId(1L);
-                task2.setTitle("task2");
-
                 Integer codeOfTask1 = task1.hashCode();
                 Integer codeOfTask2 = task2.hashCode();
 
@@ -141,17 +147,22 @@ class TaskTest {
         @DisplayName("만약 id가 다른 Task 두 객체로부터 호출된다면")
         class Context_with_differnt_id {
 
+            private Task task1;
+            private Task task2;
+
+            @BeforeEach
+            void setTaskIdDifferent() {
+                long id = 1L;
+                long differentId = id + 1;
+                String sameTitle = "sameTitle";
+
+                this.task1 = generateTask(id, sameTitle);
+                this.task2 = generateTask(differentId, sameTitle);
+            }
+
             @Test
             @DisplayName("각각 다른 hashCode를 반환한다")
             void if_returns_differnet_hash_code() {
-                Task task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
-
-                Task task2 = new Task();
-                task2.setId(2L);
-                task2.setTitle("task1");
-
                 Integer codeOfTask1 = task1.hashCode();
                 Integer codeOfTask2 = task2.hashCode();
 
@@ -164,17 +175,21 @@ class TaskTest {
         @DisplayName("만약 attribute가 동일한 Task 객체들로부터 호출된다면")
         class Context_with_same_attributes {
 
+            private Task task1;
+            private Task task2;
+
+            @BeforeEach
+            void setTasksIdAndTitleSame() {
+                long sameId = 1L;
+                String sameTitle = "sameTitle";
+
+                this.task1 = generateTask(sameId, sameTitle);
+                this.task2 = generateTask(sameId, sameTitle);
+            }
+
             @Test
             @DisplayName("각각 동일한 hashCode를 반환한다")
             void it_returns_same_hash_code() {
-                Task task1 = new Task();
-                task1.setId(1L);
-                task1.setTitle("task1");
-
-                Task task2 = new Task();
-                task2.setId(1L);
-                task2.setTitle("task1");
-
                 Integer codeOfTask1 = task1.hashCode();
                 Integer codeOfTask2 = task2.hashCode();
 
