7e8e7e92cbfaa6091251fabae3c6d372791980c3
DevRunner21 <devrunner21@gmail.com>
2021-05-20 09:57:10 +0900

    잘못 작성된 주석을 수정하라
    - TaskNotFoundException.java에잘못 작성된 주석을 제거함
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/main/java/com/codesoom/assignment/common/exceptions/TaskNotFoundException.java
+++ app/src/main/java/com/codesoom/assignment/common/exceptions/TaskNotFoundException.java
@@ -12,7 +12,6 @@ public class TaskNotFoundException extends RuntimeException{
 
     private String message = "해당하는 Task가 존재하지 않습니다.";
 
-    // 주어진 Id에 해당하는 할 일이 목록에 없을 경우 발생하는 예외입니다.
     public TaskNotFoundException() {
         log.error(message);
     }
