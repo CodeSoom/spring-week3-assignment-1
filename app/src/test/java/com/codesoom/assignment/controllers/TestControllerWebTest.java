cba9a494969d27642509540717c0a50cdcb758fd
DevRunner21 <devrunner21@gmail.com>
2021-05-20 21:54:20 +0900

    TaskControllerWebTest 를 수정하라
    - post,put 통신 시 contentType 추가
    - delete 테스트 케이스 설명 수정
    
    Signed-off-by: DevRunner21 <devrunner21@gmail.com>

--- app/src/test/java/com/codesoom/assignment/controllers/TestControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TestControllerWebTest.java
@@ -25,14 +25,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class TestControllerWebTest {
 
+    @MockBean
+    private TaskService taskService;
+
     @Autowired
     private MockMvc mockMvc;
 
     @Autowired
     ObjectMapper objectMapper;
-
-    @MockBean
-    private TaskService taskService;
 
     private final Long NOT_FOUND_TASK_ID = 100L; // 목록에 없는 할 일 ID
     private final Long NEW_TASK_ID = 1L; // 새로 생성할 할 일 ID
@@ -83,6 +83,8 @@ class TestControllerWebTest {
     @DisplayName("할 일 목록에 없는 할 일을 조회 할 때 에러를 확인합니다.")
     void detailWithInvalidId() throws Exception {
         //given
+        given(taskService.findTaskOne(NOT_FOUND_TASK_ID)).willThrow(TaskNotFoundException.class);
+
         //when
         //then
         mockMvc.perform(get("/tasks/"+NOT_FOUND_TASK_ID))
@@ -103,6 +105,8 @@ class TestControllerWebTest {
         newTask.setTitle(NEW_TASK_TITLE);
         tasks.add(newTask);
 
+        given(taskService.findTaskOne(findTaskId)).willReturn(newTask);
+
         //when
         //then
         mockMvc.perform(get("/tasks/"+findTaskId))
@@ -117,18 +121,16 @@ class TestControllerWebTest {
     void create() throws Exception {
 
         //given
-        List<Task> tasks = new ArrayList<>();
-
         Task newTask = new Task();
         newTask.setId(NEW_TASK_ID);
         newTask.setTitle(NEW_TASK_TITLE);
-        tasks.add(newTask);
 
         given(taskService.saveTask(newTask)).willReturn(newTask);
 
         //when
         //then
         mockMvc.perform(post("/tasks")
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(newTask)))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -141,7 +143,7 @@ class TestControllerWebTest {
     void updateInvallid() throws Exception {
 
         //given
-        given(taskService.updateTask(NOT_FOUND_TASK_ID, UPDATE_TASK_TITLE)).willThrow(TaskNotFoundException.class);
+        given(taskService.updateTask(NOT_FOUND_TASK_ID, UPDATE_TASK_TITLE)).willThrow(new TaskNotFoundException());
 
         Task paramTask = new Task();
         paramTask.setTitle(NEW_TASK_TITLE);
@@ -149,6 +151,7 @@ class TestControllerWebTest {
         //when
         //then
         mockMvc.perform(put("/tasks/"+NOT_FOUND_TASK_ID)
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(paramTask)))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string(TASK_NOT_FOUND_ERROR_MESSAGE));
@@ -179,8 +182,9 @@ class TestControllerWebTest {
         //when
         //then
         mockMvc.perform(put("/tasks/"+updateTaskId)
+                .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(paramTask)))
-                .andExpect(status().isNotFound())
+                .andExpect(status().isOk())
                 .andExpect(content().json(objectMapper.writeValueAsString(paramTask)));
 
     }
@@ -190,7 +194,7 @@ class TestControllerWebTest {
     void deleteInvalid() throws Exception {
 
         //given
-        given(taskService.removeTask(NOT_FOUND_TASK_ID)).willThrow(TaskNotFoundException.class);
+        given(taskService.removeTask(NOT_FOUND_TASK_ID)).willThrow(new TaskNotFoundException());
 
         //when
         //then
@@ -201,7 +205,7 @@ class TestControllerWebTest {
     }
 
     @Test
-    @DisplayName("할 일 목록에 있는 할 일을 삭제합니다.")
+    @DisplayName("주어진 ID에 해당하는 할 일을 할 일 목록에서 삭제합니다. 삭제 완료 시 HTTP 204 코드가 반환됩니다.")
     void deleteValid() throws Exception {
 
         //given
