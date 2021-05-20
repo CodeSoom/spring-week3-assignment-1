cb891afe496fd9a19f6460e16e9c8441aadccf74
Flying-Fisherman <ghdud7037@gmail.com>
2021-05-20 19:22:26 +0900

    TaskControllerTest 테스트 구조 변경
    BDD 방법으로 `@Nested`, `@DisplayName` 을 작성하였습니다.

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -5,11 +5,15 @@ import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.web.bind.annotation.PatchMapping;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+@DisplayName("TaskControllerTest 클래스")
 class TaskControllerTest {
     private TaskController controller;
 
@@ -25,94 +29,179 @@ class TaskControllerTest {
         controller.create(task);
     }
 
-    Task createTestFullTask(String source) {
-        Task task = new Task();
-        task.setTitle(source);
+    @Nested
+    @DisplayName("list 메서드는")
+    class Describe_list {
 
-        controller.create(task);
-        return task;
+        @Nested
+        @DisplayName("Task가 없을 경우")
+        class Context_not_exist_task {
+            @Test
+            @DisplayName("빈 배열을 리턴한다.")
+            void it_return_nothing() {
+                assertThat(controller.list()).isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayName("Task가 존재할 경우")
+        class Context_exist_task {
+            @Test
+            @DisplayName("Task 전체 list를 리턴한다.")
+            void it_return_all() {
+                createTestTask("test");
+                createTestTask("test2");
+
+                assertThat(controller.list()).hasSize(2);
+            }
+        }
     }
 
-    @DisplayName("TaskController의 \"list\"메서드를 사용하여 Task list를 불러옵니다.")
-    @Test
-    void list() {
-        assertThat(controller.list()).isEmpty();
+    @Nested
+    @DisplayName("detail 메서드는")
+    class Describe_detail {
 
-        createTestTask("test");
-        createTestTask("test2");
+        @Nested
+        @DisplayName("올바르지 않은 ID값이 주어지면")
+        class Context_wrong_ID {
+            @Test
+            @DisplayName("TaskNotFound에러를 발생시킨다.")
+            void it_throw_exception() {
+                assertThatThrownBy(() -> controller.detail(1L)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
 
-        assertThat(controller.list()).hasSize(2);
+        @Nested
+        @DisplayName("올바른 ID값이 주어지면")
+        class Context_right_ID {
+            @Test
+            @DisplayName("해당 ID의 Task를 리턴한다.")
+            void it_return_task() {
+                createTestTask("test");
+                createTestTask("test2");
+
+                assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
+                assertThat(controller.detail(2L).getTitle()).isEqualTo("test2");
+            }
+        }
     }
 
-    @DisplayName("TaskController의 \"getDetail\"메서드를 사용하여 특정 Task를 불러옵니다.")
-    @Test
-    void getDetail() {
-        assertThatThrownBy(() -> controller.detail(1L)).isInstanceOf(TaskNotFoundException.class);
+    @Nested
+    @DisplayName("create 메서드는")
+    class Describe_create {
 
-        createTestTask("test");
-        createTestTask("test2");
+        @Nested
+        @DisplayName("title 값이 주어지면")
+        class Context_right_title {
+            @Test
+            @DisplayName("Task를 생성하고 해당 Task를 리턴한다.")
+            void it_create_task_and_return() {
+                Task task = new Task();
+                task.setTitle("test");
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
-        assertThat(controller.detail(2L).getTitle()).isEqualTo("test2");
+                assertThat(controller.create(task).getTitle()).isEqualTo("test");
+            }
+        }
     }
 
-    @DisplayName("TaskController의 \"createNewTask\"메서드를 사용하여 Task를 생성합니다.")
-    @Test
-    void createNewTask() {
-        assertThat(controller.list()).isEmpty();
+    @Nested
+    @DisplayName("update 메서드는")
+    class Describe_update {
 
-        createTestTask("test");
+        @Nested
+        @DisplayName("올바르지 않은 ID값이 주어지면")
+        class Context_wrong_ID {
+            @Test
+            @DisplayName("TaskNotFound 에러를 발생시킨다.")
+            void it_throw_Exception() {
+                createTestTask("test");
 
-        assertThat(controller.list()).isNotEmpty();
-        assertThat(controller.list()).hasSize(1);
-        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
-        assertThatThrownBy(() -> controller.detail(2L)).isInstanceOf(TaskNotFoundException.class);
+                Task task = new Task();
+                task.setTitle("New test");
 
-        createTestTask("test2");
+                assertThatThrownBy(() -> controller.update(2L, task)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
 
-        assertThat(controller.list().get(1).getId()).isEqualTo(2L);
-        assertThat(controller.list()).hasSize(2);
+        @Nested
+        @DisplayName("올바른 ID값과 title값이 주어지면")
+        class Context_right_ID_and_title {
+            @Test
+            @DisplayName("title의 값을 변경하고, 해당 Task를 리턴한다.")
+            void it_update_task() {
+                createTestTask("test");
+
+                Task task = new Task();
+                task.setTitle("New test");
+
+                assertThat(controller.update(1L, task).getTitle()).isEqualTo("New test");
+            }
+        }
     }
 
-    @DisplayName("TaskController의 \"updateTask\"메서드를 사용하여 특정 Task의 title을 변경합니다.")
-    @Test
-    void updateTask() {
-        createTestTask("test");
+    @Nested
+    @DisplayName("patch 메서드는")
+    class Describe_patch {
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
+        @Nested
+        @DisplayName("올바르지 않은 ID값이 주어지면")
+        class Context_wrong_ID {
+            @Test
+            @DisplayName("TaskNotFound 에러를 발생시킨다.")
+            void it_throw_Exception() {
+                createTestTask("test");
 
-        Task task = new Task();
-        task.setTitle("new Test");
-        controller.update(1L, task);
+                Task task = new Task();
+                task.setTitle("New test");
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("new Test");
-        assertThatThrownBy(() -> controller.detail(2L)).isInstanceOf(TaskNotFoundException.class);
+                assertThatThrownBy(() -> controller.update(2L, task)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("올바른 ID값과 title값이 주어지면")
+        class Context_right_ID_and_title {
+            @Test
+            @DisplayName("title의 값을 변경하고, 해당 Task를 리턴한다.")
+            void it_update_task() {
+                createTestTask("test");
+
+                Task task = new Task();
+                task.setTitle("New test");
+
+                assertThat(controller.update(1L, task).getTitle()).isEqualTo("New test");
+            }
+        }
     }
 
-    @DisplayName("TaskController의 \"patchTask\"메서드를 사용하여 특정 Task의 title을 변경합니다.")
-    @Test
-    void patchTask() {
-        createTestTask("test");
+    @Nested
+    @DisplayName("delete 메서드는")
+    class Describe_delete {
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
+        @Nested
+        @DisplayName("올바르지 않은 ID값이 주어지면")
+        class Context_wrong_ID {
+            @Test
+            @DisplayName("TaskNotFound 에러를 발생시킨다.")
+            void it_throw_Exception() {
+                assertThatThrownBy(() -> controller.delete(1L)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
 
-        Task task = new Task();
-        task.setTitle("new Test");
-        controller.update(1L, task);
+        @Nested
+        @DisplayName("올바른 ID값이 주어지면")
+        class Context_right_ID {
+            @Test
+            @DisplayName("해당 Task를 삭제한다.")
+            void it_delete_task() {
+                createTestTask("test");
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("new Test");
-        assertThatThrownBy(() -> controller.detail(2L)).isInstanceOf(TaskNotFoundException.class);
-    }
+                assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
 
-    @DisplayName("TaskController의 \"deleteTask\"메서드를 사용하여 특정 Task를 삭제합니다.")
-    @Test
-    void deleteTask() {
-        createTestTask("test");
+                controller.delete(1L);
 
-        assertThat(controller.detail(1L).getTitle()).isEqualTo("test");
-
-        controller.delete(1L);
-
-        assertThatThrownBy(() -> controller.detail(1L)).isInstanceOf(TaskNotFoundException.class);
+                assertThat(controller.list()).isEmpty();
+            }
+        }
     }
 }
