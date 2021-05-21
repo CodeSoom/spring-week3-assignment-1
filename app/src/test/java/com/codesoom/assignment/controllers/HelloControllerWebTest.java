94f1334a745c638de39469144aa886ea1a150e9c
humblEgo <injun.woo30000@gmail.com>
2021-05-21 21:04:15 +0900

    refactor HelloControllerWebTest concerning nested class
    D-C-I 패턴 맥락에 맞춰서 테스트를 단계적으로 실행하도록 수정했습니다

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -1,5 +1,6 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -25,16 +27,29 @@ class HelloControllerWebTest {
     @DisplayName("'/' URL로")
     class Describe_of_root_url {
 
+        private String givenUrl;
+
+        @BeforeEach
+        void setGivenUrl() {
+            givenUrl = "/";
+        }
+
 
         @Nested
         @DisplayName("만약 GET 요청을 보내면")
         class Context_of_get {
 
+            private ResultActions perform;
+
+            @BeforeEach
+            void requestToUrl() throws Exception {
+                perform = mockMvc.perform(get(givenUrl));
+            }
+
             @Test
             @DisplayName("200 OK 상태코드와 'Hello, world!' body를 응답한다")
             public void it_returns_ok_with_valid_string() throws Exception {
-                mockMvc.perform(get("/"))
-                        .andExpect(status().isOk())
+                perform.andExpect(status().isOk())
                         .andExpect(content().string(containsString("Hello, world!")));
             }
         }
