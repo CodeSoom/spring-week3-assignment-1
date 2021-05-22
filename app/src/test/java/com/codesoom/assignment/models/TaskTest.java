ca5bfaebd8452c745293d34d1afed5f45d045ac0
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2021-05-22 14:14:33 +0900

    Apply the `@Nested` in TaskTest
    - Test model named "Task"
    - Test method "Getter/Setter"

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.models;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,25 +16,37 @@ class TaskTest {
         task = new Task();
     }
 
-    @Test
-    @DisplayName("It contains empty ID and title")
-    void taskWithoutValues() {
-        assertThat(task).isNotNull();
-        assertThat(task.getId()).isNull();
-        assertThat(task.getTitle()).isNull();
-    }
+    @Nested
+    @DisplayName("getter setter method")
+    class DescribeGetterSetter {
+        @Nested
+        @DisplayName("without id and title")
+        class ContextWithoutValues {
+            @Test
+            @DisplayName("It contains empty ID and title")
+            void taskWithoutValues() {
+                assertThat(task).isNotNull();
+                assertThat(task.getId()).isNull();
+                assertThat(task.getTitle()).isNull();
+            }
+        }
 
-    @Test
-    @DisplayName("It contains ID and title")
-    void taskWithValues() {
-        final Long id = 1L;
-        final String title = "TEST TITLE";
+        @Nested
+        @DisplayName("with id and title")
+        class ContextWithValues {
+            @Test
+            @DisplayName("It contains ID and title")
+            void taskWithValues() {
+                final Long id = 1L;
+                final String title = "TEST TITLE";
 
-        task.setId(id);
-        task.setTitle("TEST TITLE");
+                task.setId(id);
+                task.setTitle("TEST TITLE");
 
-        assertThat(task).isNotNull();
-        assertThat(task.getId()).isEqualTo(id);
-        assertThat(task.getTitle()).isEqualTo(title);
+                assertThat(task).isNotNull();
+                assertThat(task.getId()).isEqualTo(id);
+                assertThat(task.getTitle()).isEqualTo(title);
+            }
+        }
     }
 }
