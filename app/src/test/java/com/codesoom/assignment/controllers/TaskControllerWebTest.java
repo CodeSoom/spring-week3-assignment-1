a35e68540579ee8507c6b38e26917618ce56ebd8
Flying-Fisherman <ghdud7037@gmail.com>
2021-05-23 01:14:12 +0900

    TaskControllerWebTest list, detail 메서드 완성 / 잔여 메서드 개선 대기
    DCI패턴 + GWT패턴을 통한 이중 확인으로 안정적이고 효과적으로 테스트를 할 수 있도록 구조를 짜고 있습니다.
    
    내일 중으로 잔여 메서드의 테스트들과 기존 TaskControllerTest, TaskServiceTest의 `@DisplayName`을 개선하고
    Task 모델에 대한 테스트를 작성 할 예정입니다.

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -3,27 +3,27 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
+@DisplayName("TaskControllerWebTest 클래스")
 public class TaskControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
@@ -31,38 +31,114 @@ public class TaskControllerWebTest {
     @MockBean
     private TaskService taskService;
 
-    @BeforeEach
-    void setUp() {
+    @Nested
+    @DisplayName("전체 할 일을 요청할 때")
+    class Describe_listMvc {
         List<Task> tasks = new ArrayList<>();
-
         Task task = new Task();
-        task.setTitle("Test Task");
-        tasks.add(task);
 
-        given(taskService.getTasks()).willReturn(tasks);
+        @Nested
+        @DisplayName("할 일이 존재하지 않을 경우")
+        class Context_not_exist_task {
 
-        given(taskService.getTask(1L)).willReturn(task);
+            @Test
+            @DisplayName("빈 배열을 리턴하고, 200 응답코드를 전달합니다.")
+            void it_return_empty() throws Exception {
+                given(taskService.getTasks()).willReturn(tasks);
 
-        given(taskService.getTask(100L))
-                .willThrow(new TaskNotFoundException(100L));
+                final ResultActions actions = mockMvc.perform(get("/tasks"));
+
+                actions
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("[]")))
+                        .andExpect(jsonPath("$.length()", is(0)));
+            }
+
+        }
+
+        @Nested
+        @DisplayName("할 일이 존재할 경우")
+        class Context_exist_task {
+            @Test
+            @DisplayName("할 일 목록을 리턴하고, 200 응답코드를 전달합니다.")
+            void it_return_all() throws Exception {
+                given(taskService.getTasks()).willReturn(tasks);
+                task.setTitle("Test");
+                tasks.add(task);
+
+                final ResultActions actions = mockMvc.perform(get("/tasks"));
+
+                actions
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.[0].title", is("Test")));
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("특정한 할 일을 요청할 때")
+    class Describe_detailMvc {
+        Task task = new Task();
+
+        @Nested
+        @DisplayName("요청한 ID에 맞는 할 일이 없다면")
+        class Context_not_exist_task {
+            @Test
+            @DisplayName("TaskNotFound 예외를 던지고, 404 응답코드를 전달합니다.")
+            void it_throw_exception() throws Exception {
+                given(taskService.getTask(100L))
+                        .willThrow(TaskNotFoundException.class);
+
+                final ResultActions actions = mockMvc.perform(get("/tasks/100"));
+
+                actions
+                        .andExpect(status().isNotFound());
+            }
+        }
+
+        @Nested
+        @DisplayName("요청한 ID에 맞는 할 일이 있다면")
+        class Context_exist_task {
+            Task task = new Task();
+
+            @Test
+            @DisplayName("요청 ID의 할 일을 리턴하고, 200 응답코드를 전달합니다.")
+            void it_return_specific_task() throws Exception {
+                given(taskService.getTask(1L)).willReturn(task);
+                task.setTitle("Test");
+
+                final ResultActions actions = mockMvc.perform(get("/tasks/1"));
+
+                actions
+                        .andExpect(status().isOk())
+                        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                        .andExpect(jsonPath("$.title", is("Test")));
+            }
+        }
     }
 
     @Test
-    void list() throws Exception {
-        mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Test Task")));
+    void createMvcTest() throws Exception {
+        mockMvc.perform(post("/tasks title:Test"))
+                .andExpect(status().isCreated());
     }
 
     @Test
-    void detailWithValidId() throws Exception {
-        mockMvc.perform(get("/tasks/1"))
+    void updateMvcTest() throws Exception {
+        mockMvc.perform(put("/tasks/1 title:New Test"))
                 .andExpect(status().isOk());
     }
 
     @Test
-    void detailWithInvalidId() throws Exception {
-        mockMvc.perform(get("/tasks/100"))
-                .andExpect(status().isNotFound());
+    void patchMvcTest() throws Exception {
+        mockMvc.perform(patch("/tasks/1 title:New Test"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteMvcTest() throws Exception {
+        mockMvc.perform(delete("/tasks/1"))
+                .andExpect(status().isNoContent());
     }
 }
