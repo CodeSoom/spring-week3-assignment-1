a2b6e3591df8eeea4c5846353f917c05949bfd6f
humblEgo <injun.woo30000@gmail.com>
2021-05-22 15:34:19 +0900

    import * 대신 필요한 메소드만 import 하도록 수정

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -9,7 +9,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 @DisplayName("TaskController 클래스")
 class TaskControllerTest {
