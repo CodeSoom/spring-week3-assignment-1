ae128390e30976d7f86d72d3f638acc68ff645f4
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-08-23 20:54:54 +0900

    TaskController Test 생성

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -13,6 +13,10 @@ import java.util.List;
 public class TaskController {
     private TaskService taskService;
 
+    public TaskController() {
+        this.taskService = new TaskService();
+    }
+
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
