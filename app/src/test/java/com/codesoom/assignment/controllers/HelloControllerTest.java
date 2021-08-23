bb0b33156581a70aecc13d57c28b4bebb1fd8193
Sungeheon <elon4856@gmail.com>
2021-08-23 19:15:27 +0900

    feat: HelloControllerTest에  '@Displayname'을 추가하세요

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -2,13 +2,18 @@ package com.codesoom.assignment.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("HelloController테스트")
 class HelloControllerTest {
 
   @Test
+  @DisplayName("Hello World 출력된느지 테스트")
   void testHelloWorld() {
     HelloController helloController = new HelloController();
     assertThat(helloController.hello()).isEqualTo("Hello World!");
   }
+
+
 }
