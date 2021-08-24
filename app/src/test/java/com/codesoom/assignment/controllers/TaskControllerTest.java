0545e74389ff37e9a3e22e3408d60356acdc4aad
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-08-24 20:42:33 +0900

    fixtures 활용한 test 기능 생성

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,14 +1,39 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskControllerTest {
+    private TaskController controller;
+
+    @BeforeEach
+    void setUp() {
+         controller = new TaskController();
+    }
+
     @Test
     void list() {
-        TaskController controller = new TaskController();
-
         assertThat(controller.list()).isEmpty();
+    }
+
+    @Test
+    void createNewTask() {
+        Task task = new Task();
+        task.setTitle("Test1");
+
+        controller.create(task);
+
+        task.setTitle("Test2");
+
+        controller.create(task);
+
+        assertThat(controller.list()).isNotEmpty();
+        assertThat(controller.list().get(0).getId()).isEqualTo(1L);
+        assertThat(controller.list().get(0).getTitle()).isEqualTo("Test1");
+        assertThat(controller.list().get(1).getId()).isEqualTo(2L);
+        assertThat(controller.list().get(1).getTitle()).isEqualTo("Test2");
     }
 }
