7ced7c294df56cf6c2292a736bcdf06f074003e6
daeungkim <daeungkim93@gmail.com>
2021-08-25 05:24:24 +0900

    HelloController javadoc 수정
    HelloController javadoc 수정
    -> HelloController클래스와 sayHello메서드의 목적이 더 잘 드러나도록 변경

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -4,14 +4,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * "/" http request를 처리한다.
+ * 웹 서버의 작동여부를 확인하는 클라이언트의 요청을 처리한다.
  */
 @RestController
 public class HelloController {
 	/**
-	 * "/" http request 요청시, "Hello, world!" 문자열을 리턴한다.
+	 * 웹 서버의 작동여부를 확인하는 클라이언트의 요청이 들어온 경우,
+	 * 해당 웹 서버가 정상 작동함을 확인시켜주는 메시지를 리턴한다.
 	 *
-	 * @return "Hello, world!" 문자열
+	 * @return 웹 서버가 정상 작동함을 확인시켜주는 메시지
 	 */
 	@RequestMapping("/")
 	public String sayHello() {
