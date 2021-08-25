1ddcecc7c44959911007fb80e7018317510f9b9c
daeungkim <daeungkim93@gmail.com>
2021-08-25 12:03:23 +0900

    AppTest, HelloControllerTest 수정
    AppTest, HelloControllerTest 수정
    -> TaskControllerTest와의 형식을 맞추기 위해

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -3,21 +3,24 @@ package com.codesoom.assignment;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("App")
+@DisplayName("App 클래스")
 public final class AppTest {
     private final App app = new App();
 
-    @Test
-    @DisplayName("getGreeting() should exist")
-    void existTest() throws NoSuchMethodException, SecurityException {
-        assertThat(app.getGreeting()).isNotEmpty();
-    }
-
-    @Test
-    @DisplayName("getGreeting() should return \"Hello, world!\"")
-    void returnTest() {
-        assertThat(app.getGreeting()).isEqualTo("Hello, world!");
+    @Nested
+    @DisplayName("getGreeting 메소드는")
+    class Describe_getGreeting {
+        @Nested
+        @DisplayName("언제나")
+        class Context_as_always {
+            @Test
+            @DisplayName("인사 메시지를 리턴한다.")
+            void it_returns_a_greeting_message() {
+                assertThat(app.getGreeting()).isEqualTo("Hello, world!");
+            }
+        }
     }
 }
--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -3,21 +3,24 @@ package com.codesoom.assignment.controllers;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("HelloController")
+@DisplayName("HelloController 클래스")
 public final class HelloControllerTest {
     private final HelloController helloController = new HelloController();
 
-    @Test
-    @DisplayName("sayHello() should exist")
-    void existTest() {
-        assertThat(helloController.sayHello()).isNotEmpty();
-    }
-
-    @Test
-    @DisplayName("sayHello() should return \"Hello, world!\"")
-    void returnTest() {
-        assertThat(helloController.sayHello()).isEqualTo("Hello, world!");
+    @Nested
+    @DisplayName("sayHello 메소드는")
+    class Describe_sayHello {
+        @Nested
+        @DisplayName("언제나")
+        class Context_as_always {
+            @Test
+            @DisplayName("서버의 정상 작동을 확인시켜주는 메시지를 리턴한다.")
+            void it_returns_a_message_checking_the_server_operation() {
+                assertThat(helloController.sayHello()).isEqualTo("Hello, world!");
+            }
+        }
     }
 }
