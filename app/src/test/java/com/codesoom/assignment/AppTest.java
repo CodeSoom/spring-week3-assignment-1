c3a769a9708c677b8f7f2ce080172f2dde017c01
NohJangHyuk <nohriter@gmail.com>
2021-08-25 20:32:30 +0900

    `App.java` 테스트코드를 생성해라

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,16 +1,35 @@
 package com.codesoom.assignment;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.assertj.core.api.Assertions.assertThat;
 
-
+@DisplayName("App 클래스")
 class AppTest {
-    @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should gave a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+    App app;
+
+    @BeforeEach
+    void setUp() {
+        app = new App();
+    }
+
+    @Nested
+    @DisplayName("getGreeting 메소드는")
+    class Describe_getGreeting {
+
+        @Test
+        @DisplayName("문자열을 리턴합니다")
+        void it_returns_String() {
+            assertThat(app.getGreeting()).isInstanceOf(String.class);
+        }
+    }
+
+    @Nested
+    @DisplayName("main 메소드는")
+    class main {
+
     }
 }
