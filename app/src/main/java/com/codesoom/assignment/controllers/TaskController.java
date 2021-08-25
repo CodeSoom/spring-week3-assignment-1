93285a2d6257bd36b910552bb58ba73ac01f4850
daeungkim <daeungkim93@gmail.com>
2021-08-25 11:48:33 +0900

    TaskController list 메서드 테스트 추가
    TaskController javadoc 추가
    TaskController list 메서드 테스트 추가

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -7,42 +7,100 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Task와 관련된 클라이언트의 요청을 처리한다.
+ */
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin
 public class TaskController {
     private TaskService taskService;
 
+    /**
+     * @param taskService Task 데이터 처리 목적
+     */
     public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
 
+    /**
+     * 클라이언트가 저장되어 있는 모든 Task를 요청하는 경우,
+     * 저장되어있는 모든 Task를 리턴한다.
+     *
+     * @return 저장되어있는 모든 Task
+     */
     @GetMapping
     public List<Task> list() {
         return taskService.getTasks();
     }
 
+    /**
+     * 클라이언트가 저장되어 있는 Task중 특정한 Task를 요청하는 경우,
+     * id에 해당하는 Task를 리턴한다.
+     *
+     * @param id Task의 id
+     * @return id에 해당하는 Task
+     * @throws TaskNotFoundException id에 해당하는 Task가 없는경우
+     */
     @GetMapping("{id}")
     public Task detail(@PathVariable Long id) {
         return taskService.getTask(id);
     }
 
+    /**
+     * 클라이언트가 새로운 Task의 생성을 요청하는 경우,
+     * 새로운 Task를 생성하고,
+     * 생성한 Task를 리턴한다.
+     *
+     * @param task 새로 생성할 Task 내용
+     * @return 새로 생성한 Task
+     */
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Task create(@RequestBody Task task) {
         return taskService.createTask(task);
     }
 
+    /**
+     * 클라이언트가 저장되어 있는 Task의 수정을 요청하는 경우,
+     * id에 해당하는 Task를 업데이트하고,
+     * 업데이트한 Task를 리턴한다.
+     *
+     * @param id 업데이트할 Task의 id
+     * @param task 업데이트할 Task의 데이터
+     * @return 업데이트한 Task
+     * @throws TaskNotFoundException id에 해당하는 Task가 없는경우
+     * @see TaskErrorAdvice#handleNotFound() TaskNotFoundException 예외처리 수행
+     */
     @PutMapping("{id}")
     public Task update(@PathVariable Long id, @RequestBody Task task) {
         return taskService.updateTask(id, task);
     }
 
+    /**
+     * 클라이언트가 저장되어 있는 Task의 수정을 요청하는 경우,
+     * id에 해당하는 Task를 업데이트하고,
+     * 업데이트한 Task를 리턴한다.
+     *
+     * @param id 업데이트할 Task의 id
+     * @param task 업데이트할 Task의 데이터
+     * @return 업데이트한 Task
+     * @throws TaskNotFoundException id에 해당하는 Task가 없는경우
+     * @see TaskErrorAdvice#handleNotFound() TaskNotFoundException 예외처리 수행
+     */
     @PatchMapping("{id}")
     public Task patch(@PathVariable Long id, @RequestBody Task task) {
         return taskService.updateTask(id, task);
     }
 
+    /**
+     * 클라이언트가 저장되어 있는 Task의 삭제를 요청하는 경우,
+     * id에 해당하는 Task를 삭제한다.
+     *
+     * @param id 삭제할 Task의 id
+     * @throws TaskNotFoundException id에 해당하는 Task가 없는경우
+     * @see TaskErrorAdvice#handleNotFound() TaskNotFoundException 예외처리 수행
+     */
     @DeleteMapping("{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,68 @@
+package com.codesoom.assignment.controllers;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("TaskController 클래스")
+public class TaskControllerTest {
+    @Nested
+    @DisplayName("list 메소드는")
+    class Describe_list {
+        private final TaskService taskServiceMock = mock(TaskService.class);
+        private final TaskController taskController = new TaskController(taskServiceMock);
+
+        @Nested
+        @DisplayName("저장된 Task가 없다면")
+        class Context_task_empty {
+            @Test
+            @DisplayName("비어있는 리스트를 리턴한다.")
+            void it_returns_a_empty_list() {
+                when(taskServiceMock.getTasks()).thenReturn(new ArrayList<Task>());
+                assertThat(taskController.list()).isEmpty();
+            }
+        }
+
+        @Nested
+        @DisplayName("저장된 Task가 있다면")
+        class Context_task_exist {
+            Task generateTask(final Long id, final String title) {
+                Task task = new Task();
+                task.setId(id);
+                task.setTitle(title);
+                return task;
+            }
+
+            @Test
+            @DisplayName("저장된 Task가 포함된 리스트를 리턴한다.")
+            void it_returns_a_list_with_tasks() {
+                final List<Task> tasks = new ArrayList<Task>();
+                tasks.add(generateTask(1L, "title"));
+                when(taskServiceMock.getTasks()).thenReturn(tasks);
+                assertThat(taskController.list())
+                    .hasSize(1)
+                    .extracting("id", "title")
+                    .containsExactly(tuple(1L, "title"));
+            }
+        }
+
+        @AfterEach
+        void after() {
+            verify(taskServiceMock, atLeastOnce()).getTasks();
+        }
+    }
+}
