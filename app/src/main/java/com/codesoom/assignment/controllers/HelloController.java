9aecbbb61fb35b388c976c567bd32f4e58fc3928
daeungkim <daeungkim93@gmail.com>
2021-08-26 04:42:27 +0900

    HelloController sayHello javadoc 삭제
    HelloController sayHello javadoc 삭제
    -> 클래스에 있는 유일한 메서드이고, 로직도 간단하므로
    -> 클래스 javadoc으로 커버 가능하다고 판단

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -8,12 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController {
-	/**
-	 * 웹 서버의 작동여부를 확인하는 클라이언트의 요청이 들어온 경우,
-	 * 해당 웹 서버가 정상 작동함을 확인시켜주는 메시지를 리턴한다.
-	 *
-	 * @return 웹 서버가 정상 작동함을 확인시켜주는 메시지
-	 */
 	@RequestMapping("/")
 	public String sayHello() {
 		return "Hello, world!";
