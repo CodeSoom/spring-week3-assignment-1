e06086dad41c66c129d1846629b2c8e907cbe206
wjy5446 <wjy5446@gmail.com>
2021-08-25 21:21:07 +0900

    feat) TaskController 웹 테스트 계층 구조로 변경

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -1,5 +1,7 @@
 package com.codesoom.assignment.controllers;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,16 +13,29 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Nested
+@DisplayName("HelloController 웹테스트")
 @SpringBootTest
 @AutoConfigureMockMvc
 public class HelloControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void greeting() throws Exception {
-        mockMvc.perform(get("/"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello")));
+    @Nested
+    @DisplayName("Get ")
+    class Get {
+
+        @Nested
+        @DisplayName("path \'/\' 호출하면,")
+        class Path_root {
+
+            @Test
+            @DisplayName("response(status: ok, content: hello)를 반환합니다.")
+            void response_ok() throws Exception {
+                mockMvc.perform(get("/"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("Hello")));
+            }
+        }
     }
 }
