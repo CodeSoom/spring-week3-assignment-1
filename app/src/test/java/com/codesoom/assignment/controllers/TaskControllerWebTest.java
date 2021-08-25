1fa98592200c66d39d6f0d1bea09f078b182890a
daeungkim <daeungkim93@gmail.com>
2021-08-26 07:14:19 +0900

    TaskControllerWebTest 테스트 어노테이션 변경
    TaskControllerWebTest 테스트 어노테이션 변경
    @SpringBootTest + @AutoConfigureMockMvc -> @WebMvcTest
    -> @SpringBootTest slicing을 사용하지 않기 때문에 @WebMvcTest보다 느림
    -> 실제로 테스트 시간 측정 시 @SpringBootTest : 18s @WebMvcTest : 14s 였음
    -> TaskController 의존성 설정 방법을 몰라서 @SpringBootTest를 사용
    -> TaskController 의존성 설정을 완료했으므로 @SpringBootTest를 쓸 이유가 없음

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -17,16 +17,12 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-// @WebMvcTest
-@SpringBootTest
-@AutoConfigureMockMvc
-public class TaskControllerWebTest {
+@WebMvcTest
+public final class TaskControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
 
