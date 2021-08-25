010727a74434677afd7f47a2bf89fa0dc56dc2c5
shinsanghooon <shinsanghooon@gmail.com>
2021-08-26 01:52:01 +0900

    TaskControllerWebTest 테스트 코드 작성
    1. update 테스트 코드 작성
    2. delete 테스트 코드 작성

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -3,21 +3,29 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.service.TaskService;
 import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
-import org.aspectj.lang.annotation.After;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.*;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -29,18 +37,22 @@ public class TaskControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private TaskService taskService;
 
     @BeforeEach
-    void setUp() {
+    void setUp(){
+
         ArrayList<Task> tasks = new ArrayList<>();
         Task task = new Task(1L, "title");
         tasks.add(task);
 
         given(taskService.getTaskList()).willReturn(tasks);
         given(taskService.getTask(1L)).willReturn(task);
-        given(taskService.createTask(task)).willReturn(task);
+
     }
 
     @Nested
@@ -51,14 +63,16 @@ public class TaskControllerWebTest {
         void list() throws Exception {
             mockMvc.perform(get("/tasks"))
                     .andExpect(status().isOk())
-                    .andExpect(content().string(containsString("title")));
+                    .andExpect(content().string(containsString("title")))
+                    .andDo(print());
         }
 
         @Test
         @DisplayName("정상 요청이면 OK를 응답한다.")
         void detailWithValidId() throws Exception {
-            mockMvc.perform(get("/tasks/1"))
-                    .andExpect(status().isOk());
+            mockMvc.perform(get("/tasks/{id}", 1L))
+                    .andExpect(status().isOk())
+                    .andDo(print());
         }
 
         @Test
@@ -68,14 +82,16 @@ public class TaskControllerWebTest {
                     .willThrow(new TaskNotFoundException("10000"));
 
             mockMvc.perform(get("/tasks/10000"))
-                    .andExpect(status().isNotFound());
+                    .andExpect(status().isNotFound())
+                    .andDo(print());
         }
 
         @Test
         @DisplayName("비정상적인 PathVariable을 요청하면 Not Found를 응답한다.")
         void detailWithInvalidPathVariable() throws Exception {
             mockMvc.perform(get("/tasks/undefined"))
-                    .andExpect(status().isNotFound());
+                    .andExpect(status().isNotFound())
+                    .andDo(print());
         }
     }
 
@@ -85,12 +101,46 @@ public class TaskControllerWebTest {
         @Test
         @DisplayName("요청이 정상적인 경우 할 일을 등록한다.")
         void createTask() throws Exception {
-            mockMvc.perform(post("/tasks"))
-                    .andExpect(status().isCreated());
+            Task task2 = new Task(2L, "title2");
+            mockMvc.perform(post("/tasks")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(task2)))
+                    .andExpect(status().isCreated())
+                    .andDo(print());
         }
     }
 
+    @Nested
+    @DisplayName("할 일(id)에 대한 수정 요청이 오면")
+    class UpdateTask {
 
+        @Test
+        @DisplayName("할 일(id)을 수정한다.")
+        void updateTask() throws Exception {
 
+            Task newTask = new Task(1L, "New Title");
+            given(taskService.updateTask(1L, newTask)).willReturn(newTask);
+
+            mockMvc.perform(put("/tasks/{id}", 1L)
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(newTask)))
+                    .andExpect(status().isOk())
+                    .andDo(print());
+
+        }
+    }
+
+    @Nested
+    @DisplayName("할 일(id)에 대한 완료 요청이 오면")
+    class CompleteTask {
+
+        @Test
+        @DisplayName("할 일(id)을 리스트에서 제외한다.")
+        void completeTask() throws Exception {
+            mockMvc.perform(delete("/tasks/{id}", 1L))
+                    .andExpect(status().isNoContent())
+                    .andDo(print());
+        }
+    }
 
 }
