4d1c1e8ea127ef96d49bcb53ecc0e82cf42e8f1d
shinsanghooon <shinsanghooon@gmail.com>
2021-08-26 22:44:22 +0900

    GlobalExceptionHandler 이름 변경
    GlobalExceptionHandler 라는 전역의 느낌이 나는 핸들러를 컨트롤러 메서드
    관련 예외를 처리하는 핸들러의 역할을 하도록 이름을 변경

--- app/src/main/java/com/codesoom/assignment/exception/ControllerMethodExceptionHandler.java
+++ app/src/main/java/com/codesoom/assignment/exception/ControllerMethodExceptionHandler.java
@@ -7,13 +7,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 /**
- * 애플리케이션 전역에 공통으로 사용되는 Global Exception Handler 입니다.
+ *  컨트롤러 메서드와 관계된 예외를 처리하는 핸들러입니다.
  */
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class ControllerMethodExceptionHandler {
 
     /**
-     * 메소드 Argument 타입이 일치하지 않는 경우 예외를 던집니다.
+     * 컨트롤러 메소드 Argument 타입이 일치하지 않는 경우 예외를 던집니다.
      * 
      * @param e MethodArgumentTypeMismatchException 예외
      */
