9b8ec0e7d2bd25ad43947432619f0b6b0aefa454
ytkim4558@naver.com <R5vSP6djLOpfxXF>
2021-08-26 21:08:31 +0900

    mocktest 추가, 함수 설명 추가

--- app/src/main/java/com/codesoom/assignment/application/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/application/TaskService.java
@@ -9,13 +9,18 @@ import java.util.List;
 
 @Service
 public class TaskService {
-    private List<Task> tasks = new ArrayList<>();
+    private List<Task> tasks = new ArrayList<>(); // 할일 목록
     private Long newId = 0L;
 
     public List<Task> getTasks() {
         return tasks;
     }
 
+    /**
+     * 사용자의 할 일을 가져오기
+     * @param id 가져올 할 일의 id
+     * @return 할 일
+     */
     public Task getTask(Long id) {
         return tasks.stream()
                 .filter(task -> task.getId().equals(id))
@@ -23,6 +28,11 @@ public class TaskService {
                 .orElseThrow(() -> new TaskNotFoundException(id));
     }
 
+    /**
+     * 할일을 할일 목록에 추가
+     * @param source 사용자가 입력한 할 일
+     * @return 추가된 할 일
+     */
     public Task createTask(Task source) {
         Task task = new Task();
         task.setId(generateId());
@@ -33,6 +43,12 @@ public class TaskService {
         return task;
     }
 
+    /**
+     * 사용자가 선택한 할일 의 내용 수정
+     * @param id 사용자가 선택한 할일의 id
+     * @param source 사용자가 수정한 내용
+     * @return
+     */
     public Task updateTask(Long id, Task source) {
         Task task = getTask(id);
         task.setTitle(source.getTitle());
@@ -40,6 +56,11 @@ public class TaskService {
         return task;
     }
 
+    /**
+     * 완료된 할 일을 리스트에서 삭제
+     * @param id
+     * @return 삭제된 결과 반환
+     */
     public Task deleteTask(Long id) {
         Task task = getTask(id);
         tasks.remove(task);
@@ -47,6 +68,10 @@ public class TaskService {
         return task;
     }
 
+    /**
+     * 할일이 추가될때마다 id를 하나 증가
+     * @return 증가된 할 일
+     */
     private Long generateId() {
         newId += 1;
         return newId;
--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -1,34 +1,60 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class TaskControllerWebTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private TaskService taskService = new TaskService();
+    @MockBean
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        List<Task> tasks = new ArrayList<>();
+
+        Task task = new Task();
+        task.setTitle("Test Task");
+        tasks.add(task);
+        given(taskService.getTasks()).willReturn(tasks);
+
+        given(taskService.getTask(1L)).willReturn(task);
+        given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
+    }
 
     @Test
     void list() throws Exception {
         mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Task")));
     }
 
     @Test
     void detailWithValidId() throws Exception {
-        Task task = new Task();
-        taskService.createTask(task);
-
         mockMvc.perform(get("/tasks/1"))
+                .andExpect(status().isOk())
                 .andExpect(status().isOk());
     }
 
