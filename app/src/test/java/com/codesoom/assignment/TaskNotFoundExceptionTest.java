2ee87aa1712c4df8cb2cb65caad47191afa7e3ef
pangnem <sprnd645@gmail.com>
2021-08-26 15:58:02 +0900

    test: DisplayName 추가

--- app/src/test/java/com/codesoom/assignment/TaskNotFoundExceptionTest.java
+++ app/src/test/java/com/codesoom/assignment/TaskNotFoundExceptionTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TaskNotFoundExceptionTest {
@@ -9,7 +10,8 @@ public class TaskNotFoundExceptionTest {
     private static final long id = 1L;
 
     @Test
-    void exception() {
+    @DisplayName("예외 메시지 테스트")
+    void test_exception_message() {
         assertThat(new TaskNotFoundException(id))
             .hasMessage("Task not found: " + id);
     }
