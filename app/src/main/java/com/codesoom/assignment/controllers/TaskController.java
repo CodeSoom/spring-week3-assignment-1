f634b2c0043cc15a67059e58b289a921351b7e05
Sungeheon <elon4856@gmail.com>
2021-08-26 19:32:49 +0900

    fix: returningTask를 createdTask로 이름을 수정하세요

--- app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskController.java
@@ -2,50 +2,62 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tasks")
 @CrossOrigin
 public class TaskController {
-    private TaskService taskService;
 
-    public TaskController(TaskService taskService) {
-        this.taskService = taskService;
-    }
+  private TaskService taskService;
 
-    @GetMapping
-    public List<Task> list() {
-        return taskService.getTasks();
-    }
+  public TaskController(TaskService taskService) {
 
-    @GetMapping("{id}")
-    public Task detail(@PathVariable Long id) {
-        return taskService.getTask(id);
-    }
+    this.taskService = taskService;
+  }
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Task create(@RequestBody Task task) {
-        return taskService.createTask(task);
-    }
+  @GetMapping
+  public List<Task> list() {
 
-    @PutMapping("{id}")
-    public Task update(@PathVariable Long id, @RequestBody Task task) {
-        return taskService.updateTask(id, task);
-    }
+    return taskService.getTasks();
+  }
 
-    @PatchMapping("{id}")
-    public Task patch(@PathVariable Long id, @RequestBody Task task) {
-        return taskService.updateTask(id, task);
-    }
+  @GetMapping("{id}")
+  public Task detail(@PathVariable Long id) {
+    return taskService.getTask(id);
+  }
 
-    @DeleteMapping("{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void delete(@PathVariable Long id) {
-        taskService.deleteTask(id);
-    }
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Task create(@RequestBody Task task) {
+    return taskService.createTask(task);
+  }
+
+  @PutMapping("{id}")
+  public Task update(@PathVariable Long id, @RequestBody Task task) {
+    return taskService.updateTask(id, task);
+  }
+
+  @PatchMapping("{id}")
+  public Task patch(@PathVariable Long id, @RequestBody Task task) {
+    return taskService.updateTask(id, task);
+  }
+
+  @DeleteMapping("{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable Long id) {
+    taskService.deleteTask(id);
+  }
 }
