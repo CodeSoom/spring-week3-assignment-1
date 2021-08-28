aa7b642c4f4e426d229bba93c84a86cbe4c9cdc3
Sungeheon <elon4856@gmail.com>
2021-08-28 20:26:43 +0900

    feat: get /tasks 테스트시 task없을 때의 context를 추가하세요

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -1,36 +1,26 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.TaskNotFoundException;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Collections;
-import java.util.List;
 import org.springframework.test.web.servlet.RequestBuilder;
-
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,10 +29,31 @@ public class TaskControllerWebTest {
   @Autowired
   private MockMvc mockMvc;
 
+  @Autowired
+  ObjectMapper objectMapper;
+
   @MockBean
   private TaskService taskService;
 
   private RequestBuilder requestBuilder;
+  private List<Task> taskList;
+  private Task task;
+
+
+  private final String TASK_TITLE = "Test";
+  private final String CREATE_TITLE = "createTest";
+  private final String MODIFY_TITLE = "modifyTest";
+
+  private TaskController taskController;
+  private Long NotFoundId = 0L;
+
+  @BeforeEach
+  void setUp() {
+    task = new Task();
+    task.setTitle(TASK_TITLE);
+    taskController = new TaskController(new TaskService());
+  }
+
 
   @Nested
   @DisplayName("GET /tasks 요청할 때,")
@@ -55,19 +66,43 @@ public class TaskControllerWebTest {
 
 
     @Nested
-    @DisplayName("task가 없다면")
+    @DisplayName("task가 없을 때")
     class Context_WithoutTask {
 
+      @Test
+      @DisplayName("200 status 코드와 빈 리스트를 return.")
+      void It_Respond200AndEmptyList() throws Exception {
+        given(taskService.getTasks()).willReturn(new ArrayList<>());
+
+        mockMvc.perform(requestBuilder)
+            .andExpect(status().isOk())
+            .andExpect(content().json("[]"));
+      }
     }
 
-    @Test
-    @DisplayName("200 status 코드와 빈 리스트를 반환한다.")
-    void It_Respond200AndEmptyList() throws Exception {
-      given(taskService.getTasks()).willReturn(new ArrayList<>());
 
-      mockMvc.perform(requestBuilder)
-          .andExpect(status().isOk())
-          .andExpect(content().json("[]"));
+    @Nested
+    @DisplayName("task가 있을 때")
+    class Context_WithTask {
+
+      @BeforeEach
+      void setUp() {
+        taskController.create(task);
+        taskList = taskController.list();
+        given(taskService.getTasks()).willReturn(taskList);
+
+      }
+
+      @Test
+      @DisplayName("200 status 코드와 task를 포함한 리스트를 return.")
+      void It_Respond200AndListWithTask() throws Exception {
+        mockMvc.perform(requestBuilder)
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(content().json(objectMapper.writeValueAsString(taskList)));
+      }
+
+
     }
   }
 
