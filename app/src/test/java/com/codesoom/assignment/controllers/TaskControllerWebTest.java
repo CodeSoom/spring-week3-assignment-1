58ba9f3fa2af1afbabab53702e96fe0bf7f8a72e
shinsanghooon <shinsanghooon@gmail.com>
2021-08-28 19:36:18 +0900

    TaskControllerWebTest 테스트 코드 작성(조회만)
    조회 코드만 작성. 생성, 수정 삭제는 곧 작업할 예정입니다

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.service.TaskService;
 import com.codesoom.assignment.exception.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -14,10 +15,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.ArrayList;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -26,7 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@DisplayName("Task Controller WebTest는")
+@DisplayName("Task Controller WebTest의")
 public class TaskControllerWebTest {
 
     @Autowired
@@ -40,55 +45,98 @@ public class TaskControllerWebTest {
 
     @BeforeEach
     void setUp(){
-
         ArrayList<Task> tasks = new ArrayList<>();
         Task task = new Task(1L, "title");
         tasks.add(task);
 
         given(taskService.getTaskList()).willReturn(tasks);
         given(taskService.getTask(1L)).willReturn(task);
-
     }
 
     @Nested
-    @DisplayName("할 일(id)에 대한 조회 요청을 했을 때")
-    class GetTask {
-        @Test
-        @DisplayName("전체 리스트 요청일 경우 OK를 응답한다.")
-        void list() throws Exception {
-            mockMvc.perform(get("/tasks"))
-                    .andExpect(status().isOk())
-                    .andExpect(content().string(containsString("title")))
-                    .andDo(print());
+    @DisplayName("getTaskList 메소드는")
+    class Describe_getTaskList {
+
+        @Nested
+        @DisplayName("할 일이 없으면")
+        class Context_with_no_tasks {
+
+            @BeforeEach
+            void noTaskSetUp() {
+                given(taskService.getTaskList()).willReturn(new ArrayList<Task>());
+            }
+
+            @Test
+            @DisplayName("빈 리스트를 리턴하고 OK를 응답한다.")
+            void list() throws Exception {
+                MvcResult mvcResult = mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("[]"))
+                        .andDo(print())
+                        .andReturn();
+            }
         }
 
-        @Test
-        @DisplayName("정상 요청이면 OK를 응답한다.")
-        void detailWithValidId() throws Exception {
-            mockMvc.perform(get("/tasks/{id}", 1L))
-                    .andExpect(status().isOk())
-                    .andDo(print());
-        }
-
-        @Test
-        @DisplayName("잘못된 아이디를 요청하면 Not Found를 응답한다.")
-        void detailWithInvalidId() throws Exception {
-            given(taskService.getTask(10000L))
-                    .willThrow(new TaskNotFoundException("10000"));
-
-            mockMvc.perform(get("/tasks/10000"))
-                    .andExpect(status().isNotFound())
-                    .andDo(print());
-        }
-
-        @Test
-        @DisplayName("비정상적인 PathVariable을 요청하면 Not Found를 응답한다.")
-        void detailWithInvalidPathVariable() throws Exception {
-            mockMvc.perform(get("/tasks/undefined"))
-                    .andExpect(status().isNotFound())
-                    .andDo(print());
+        @Nested
+        @DisplayName("할 일이 있으면")
+        class Context_with_tasks {
+            @Test
+            @DisplayName("전체 할 일 리스트를 리턴하고 OK를 응답한다.")
+            void list() throws Exception {
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string(containsString("title")))
+                        .andDo(print());
+            }
         }
     }
+
+    @Nested
+    @DisplayName("getTask 메소드는")
+    class Describe_getTask {
+
+        @Nested
+        @DisplayName("할 일이 있으면")
+        class Context_with_task {
+            @Test
+            @DisplayName("할 일을 리턴하고 OK를 응답한다.")
+            void detailWithValidId() throws Exception {
+                mockMvc.perform(get("/tasks/{id}", 1L))
+                        .andExpect(status().isOk())
+                        .andDo(print());
+            }
+        }
+
+        @Nested
+        @DisplayName("할 일이 없으면 ")
+        class Context_with_no_task {
+            @Test
+            @DisplayName("TaskNotFoundException을 던지고 Not Found를 응답한다.")
+            void detailWithInvalidId() throws Exception {
+                given(taskService.getTask(10000L))
+                        .willThrow(new TaskNotFoundException("10000"));
+
+                mockMvc.perform(get("/tasks/10000"))
+                        .andExpect(status().isNotFound())
+                        .andExpect(result -> assertTrue(result.getResolvedException() instanceof TaskNotFoundException))
+                        .andDo(print());
+            }
+        }
+
+        @Nested
+        @DisplayName("비정상적인 PathVariable을 요청하면")
+        class Context_with_bad_request {
+            @Test
+            @DisplayName("Not Found를 응답한다.")
+            void detailWithInvalidPathVariable() throws Exception {
+                mockMvc.perform(get("/tasks/undefined"))
+                        .andExpect(status().isNotFound())
+                        .andExpect(result -> assertTrue(result.getResolvedException() instanceof MethodArgumentTypeMismatchException))
+                        .andDo(print());
+            }
+        }
+    }
+
 
     @Nested
     @DisplayName("할 일(id)에 대한 등록 요청이 왔을 때")
