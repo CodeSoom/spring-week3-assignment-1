522b8d37206bae40456c02537a04efcc8bdf4c29
daeungkim <daeungkim93@gmail.com>
2021-08-28 18:16:53 +0900

    불필요한 컨텍스트 삭제
    불필요한 컨텍스트 삭제

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -5,25 +5,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @DisplayName("HelloController 클래스")
+@ExtendWith(MockitoExtension.class)
 public final class HelloControllerTest {
-    private HelloController helloController = null;
+    @InjectMocks
+    private HelloController helloController;
 
     @Nested
     @DisplayName("sayHello 메소드는")
     class Describe_sayHello {
-        public Describe_sayHello() {
-            helloController = new HelloController();
-        }
-        @Nested
-        @DisplayName("언제나")
-        class Context_as_always {
-            @Test
-            @DisplayName("서버의 정상 작동을 확인시켜주는 메시지를 리턴한다.")
-            void it_returns_a_message_checking_the_server_operation() {
-                assertThat(helloController.sayHello()).isEqualTo("Hello, world!");
-            }
+        @Test
+        @DisplayName("서버의 정상 작동을 확인시켜주는 메시지를 리턴한다.")
+        void it_returns_a_message_checking_the_server_operation() {
+            assertThat(helloController.sayHello()).isEqualTo("Hello, world!");
         }
     }
 }
