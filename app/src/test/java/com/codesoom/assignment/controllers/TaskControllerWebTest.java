a84f65e6a654c5ade442a7b6a7da1cb840de8fe5
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-14 22:56:23 +0900

    TaskController MockMVC 테스트 작성 - 등록,수정 > 응답 body 빈문자열 이슈

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -0,0 +1,115 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.BDDMockito.given;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutput;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TaskControllerWebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TaskService taskService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private final static String NEW_TITLE = "new task";
+    private final static String TITLE_POSTFIX = " spring";
+
+
+    @BeforeEach
+    void setUp() {
+        List<Task> tasks = new ArrayList<>();
+        Task task = new Task();
+        task.setId(1L);
+        task.setTitle(NEW_TITLE);
+        tasks.add(task);
+
+        given(taskService.getTasks()).willReturn(tasks);
+        given(taskService.getTask(1L)).willReturn(task);
+        given(taskService.getTask(100L)).willThrow(TaskNotFoundException.class);
+
+//        given(taskService.createTask(task)).willReturn(task);
+    }
+
+    @Test
+    void list() throws Exception {
+        mockMvc.perform(get("/tasks"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(NEW_TITLE)));
+    }
+
+    @Test
+    void detail() throws Exception {
+        mockMvc.perform(get("/tasks/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(NEW_TITLE)));
+    }
+
+    @Test
+    void create() throws Exception {
+        Task source = new Task();
+        source.setTitle(NEW_TITLE);
+
+        String content = objectMapper.writeValueAsString(source);
+
+        mockMvc.perform(post("/tasks")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        )
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(NEW_TITLE)));
+    }
+
+    @Test
+    void update() throws Exception {
+        Task source = new Task();
+        source.setTitle(NEW_TITLE + TITLE_POSTFIX);
+
+        String content = objectMapper.writeValueAsString(source);
+
+        mockMvc.perform(patch("/tasks/1")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(NEW_TITLE + TITLE_POSTFIX)));
+    }
+
+    @Test
+    void delete() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/tasks/1"))
+                .andExpect(status().isNoContent());
+    }
+}
