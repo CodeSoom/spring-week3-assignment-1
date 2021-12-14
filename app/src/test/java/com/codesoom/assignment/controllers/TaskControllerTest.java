14747106aa172415e0d673415278418a2119379a
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-14 22:20:41 +0900

    TaskController 유닛 테스트 작성

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,73 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    private TaskController controller;
+    private TaskService taskService;
+
+    private final static String NEW_TITLE = "new task";
+    private final static String TITLE_POSTFIX = " spring";
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService();
+        controller = new TaskController(taskService);
+
+        Task source = new Task();
+        source.setTitle(NEW_TITLE);
+        taskService.createTask(source);
+    }
+
+    @Test
+    void list() {
+        List<Task> tasks = controller.list();
+
+        assertThat(tasks).hasSize(1);
+    }
+
+    @Test
+    void detail() {
+        Task task = controller.detail(1L);
+
+        assertThat(task.getId()).isEqualTo(1L);
+        assertThat(task.getTitle()).isEqualTo(NEW_TITLE);
+    }
+
+    @Test
+    void create() {
+        Task source = new Task();
+        source.setTitle(NEW_TITLE + TITLE_POSTFIX);
+        Task task = controller.create(source);
+
+        assertThat(task.getId()).isEqualTo(2L);
+        assertThat(task.getTitle()).isEqualTo(NEW_TITLE + TITLE_POSTFIX);
+    }
+
+    @Test
+    void update() {
+        Task source = controller.detail(1L);
+        source.setTitle(NEW_TITLE + TITLE_POSTFIX);
+        Task update = controller.update(1L, source);
+
+        assertThat(update.getId()).isEqualTo(1L);
+        assertThat(update.getTitle()).isEqualTo(NEW_TITLE + TITLE_POSTFIX);
+    }
+
+    @Test
+    void delete() {
+        controller.delete(1L);
+        List<Task> tasks = controller.list();
+
+        assertThat(tasks).hasSize(0);
+    }
+}
