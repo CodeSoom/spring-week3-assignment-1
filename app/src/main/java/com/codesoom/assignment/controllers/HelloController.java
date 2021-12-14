ead7586aa455a275bce0fb43e69b0bd7abb2a0ff
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-14 21:39:42 +0900

    HelloController 테스트코드 작성

---
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        String content = "hello";
+
+        return content;
+    }
+}
