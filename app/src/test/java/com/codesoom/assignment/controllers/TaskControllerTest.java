45dc09b520945cc15a6f10b482d6ed9f18dc9a5f
kyuwon53 <lkw2v@naver.com>
2021-12-15 20:44:40 +0900

    [Add] 테스트를 설명하라

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -4,13 +4,15 @@ import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-
+@DisplayName("TaskController 클래스")
 class TaskControllerTest {
+
     private TaskController controller;
     private TaskService taskService;
 
@@ -30,22 +32,26 @@ class TaskControllerTest {
     }
 
     @Test
+    @DisplayName("list 메소드는 Task 목록을 리턴한다.")
     void list() {
         assertThat(controller.list()).isNotEmpty();
     }
 
     @Test
+    @DisplayName("detail 메소드는 id에 해당하는 Task를 리턴한다.")
     void detailWithValidId() {
         assertThat(controller.detail(1L).getId()).isEqualTo(1L);
         assertThat(controller.detail(1L).getTitle()).isEqualTo(TASK_TITLE);
     }
 
     @Test
+    @DisplayName("detail 메소드는 id가 없다면 예외를 던진다.")
     void detailWithInvalidId() {
         assertThatThrownBy(() -> controller.detail(0L)).isInstanceOf(TaskNotFoundException.class);
     }
 
     @Test
+    @DisplayName("create 메소드는 새로운 task를 생성하고 task를 리턴한다.")
     void create() {
         int originSize = controller.list().size();
 
@@ -60,6 +66,7 @@ class TaskControllerTest {
     }
 
     @Test
+    @DisplayName("update 메소드는 기존 id의 task의 title을 수정하고 task를 리턴한다.")
     void updateWithValidId() {
         Task source = new Task();
         source.setTitle(UPDATE_POSTFIX + TASK_TITLE);
@@ -70,6 +77,7 @@ class TaskControllerTest {
     }
 
     @Test
+    @DisplayName("patch 메소드는 기존 id의 task의 title을 수정하고 task를 리턴한다.")
     void patchWithValidId() {
         Task source = new Task();
         source.setTitle(UPDATE_POSTFIX + NEW_TITLE);
@@ -80,6 +88,7 @@ class TaskControllerTest {
     }
 
     @Test
+    @DisplayName("delete 메소드는 해당 id의 task를 삭제한다. ")
     void delete() {
         int originSize = controller.list().size();
 
