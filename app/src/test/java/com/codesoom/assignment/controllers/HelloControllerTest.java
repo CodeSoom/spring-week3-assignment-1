ae9953940b662aaa80daa2e28c054e03fd651d09
kyuwon53 <lkw2v@naver.com>
2021-12-15 20:13:53 +0900

    [Create] HelloController의 테스트코드를 작성하라

---
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -0,0 +1,24 @@
+package com.codesoom.assignment.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class HelloControllerTest {
+
+    static final String HELLO_WORLD = "Hello, world!";
+
+    private HelloController helloController;
+
+    @BeforeEach
+    void setUp() {
+        helloController = new HelloController();
+    }
+
+    @Test
+    void sayHello() {
+        assertThat(helloController.sayHello()).isEqualTo(HELLO_WORLD);
+    }
+}
