936431f6461ef11217d710349751722753af9053
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-16 20:44:42 +0900

    [수정] TaskTest - 불필요한 테스트 코드 제거

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -3,7 +3,6 @@ package com.codesoom.assignment.models;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TaskTest {
 
@@ -15,16 +14,5 @@ class TaskTest {
 
         assertThat(task.getId()).isEqualTo(1L);
         assertThat(task.getTitle()).isEqualTo("test1");
-    }
-
-    @Test
-    void nullPointException() {
-        Task task = null;
-
-        assertThatThrownBy(() -> task.getId())
-                .isInstanceOf(NullPointerException.class);
-
-        assertThatThrownBy(() -> task.setId(1L))
-                .isInstanceOf(NullPointerException.class);
     }
 }
