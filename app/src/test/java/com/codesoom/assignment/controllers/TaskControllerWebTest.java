fb810330fd7044085ebecbb6b84db73508bb831b
Seungchul Jang <jangsc0000@gmail.com>
2021-12-17 00:26:40 +0900

    TaskControllerWebTest에서 Mock 제거

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -1,29 +1,28 @@
 package com.codesoom.assignment.controllers;
 
-import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.core.StringContains.containsString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = TaskController.class)
+@AutoConfigureMockMvc
+@SpringBootTest
 public class TaskControllerWebTest {
 
     private static final String FIRST_TASK_TITLE = "test1";
@@ -36,33 +35,17 @@ public class TaskControllerWebTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @Autowired
     private TaskService taskService;
 
     @BeforeEach
     void setUp() {
-        List<Task> tasks = new ArrayList<>();
+        taskService.reset();
 
-        Task firstTask = new Task();
-        firstTask.setTitle(FIRST_TASK_TITLE);
-        tasks.add(firstTask);
-
-        Task secondTask = new Task();
-        secondTask.setTitle(SECOND_TASK_TITLE);
-        secondTask.setId(2L);
-
-        Task updatedTask = new Task();
-        updatedTask.setTitle(FIRST_TASK_TITLE + UPDATE_POSTFIX);
-        updatedTask.setId(1L);
-
-        given(taskService.getTasks()).willReturn(tasks);
-        given(taskService.createTask(any(Task.class))).willReturn(secondTask);
-        given(taskService.getTask(1L)).willReturn(firstTask);
-        given(taskService.getTask(2L)).willThrow(TaskNotFoundException.class);
-        given(taskService.updateTask(eq(1L), any(Task.class))).willReturn(updatedTask);
-        given(taskService.updateTask(eq(2L), any(Task.class))).willThrow(TaskNotFoundException.class);
-        given(taskService.deleteTask(1L)).willReturn(firstTask);
-        given(taskService.deleteTask(2L)).willThrow(TaskNotFoundException.class);
+        // Fixture
+        Task task = new Task();
+        task.setTitle(FIRST_TASK_TITLE);
+        taskService.createTask(task);
     }
 
     @Test
@@ -149,13 +132,13 @@ public class TaskControllerWebTest {
     }
 
     @Test
-    void deleteWithInvalidId() throws Exception {
+    void deleteWithValidId() throws Exception {
         mockMvc.perform(delete("/tasks/1"))
                 .andExpect(status().isNoContent());
     }
 
     @Test
-    void deleteWithValidId() throws Exception {
+    void deleteWithInvalidId() throws Exception {
         mockMvc.perform(delete("/tasks/2"))
                 .andExpect(status().isNotFound());
     }
