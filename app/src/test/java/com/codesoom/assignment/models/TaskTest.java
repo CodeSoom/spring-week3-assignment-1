e931b10549013492372ffd9a1412a4ff184c76d9
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2021-12-16 20:47:00 +0900

    [수정] TaskTest - 테스트 코드에 목적을 명시

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,13 +1,15 @@
 package com.codesoom.assignment.models;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskTest {
 
+    @DisplayName("setter 메소드로 수정하면 getter 메소드로 조회할 수 있다")
     @Test
-    void setter() {
+    void setter_getter() {
         Task task = new Task();
         task.setId(1L);
         task.setTitle("test1");
