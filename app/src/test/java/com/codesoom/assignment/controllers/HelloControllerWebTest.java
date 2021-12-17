d0ac5edcd9dafb305597116497e450edf15bf434
Hyunjune Lee <aphlox@naver.com>
2021-12-18 03:34:14 +0900

    테스트 수정: HelloController MockMVC DisplayName 문구 변경
    가독성이 더 좋아질거라 생각해서
    Context에는 '만약'을 앞에 붙이고
    '합니다'보다는 '한다'로 통일함

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -1,18 +1,18 @@
 package com.codesoom.assignment.controllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@WebMvcTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 @DisplayName("HelloController 클래스")
 public class HelloControllerWebTest {
     @Autowired
@@ -23,10 +23,10 @@ public class HelloControllerWebTest {
     class Describe_sayHello {
 
         @Nested
-        @DisplayName("path = / 일 때")
-        class Context_with_root_path{
+        @DisplayName("만약 path = / 일 때")
+        class Context_with_root_path {
             @Test
-            @DisplayName("OK(200)을 응답합니다.")
+            @DisplayName("OK(200)을 응답한다.")
             void it_returns_200_OK() throws Exception {
                 mockMvc.perform(get("/"))
                         .andExpect(status().isOk());
