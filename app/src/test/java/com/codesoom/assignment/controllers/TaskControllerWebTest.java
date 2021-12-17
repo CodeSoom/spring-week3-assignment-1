34981773d3f14561ec7a78722d553b29cfd7fe36
hyeongjoGwak <hyeongjo.Gwak@kurlycorp.com>
2021-12-17 11:50:20 +0900

    BeforeEach를 이용해 given을 먼저 처리하도록 한다
    - BeforeEach는 각 테스트가 실행 될 때 마다 먼저 선행되는 동작이다.
    - 여기에 given을 넣어서 가짜 객체가 어떤 동작을 수행하는지 정의한다

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -10,8 +11,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -23,25 +29,34 @@ public class TaskControllerWebTest {
     @MockBean
     private TaskService taskService;
 
+    @BeforeEach
+    void setUp() {
+        List<Task> tasks = new ArrayList<>();
+
+        Task task = new Task();
+        task.setTitle("Test Task");
+        tasks.add(task);
+
+        given(taskService.getTasks()).willReturn(tasks);
+        given(taskService.getTask(1L)).willReturn(task);
+        given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
+    }
+
     @Test
     void list() throws Exception {
         mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Task")));
     }
 
     @Test
     void detailWithValidId() throws Exception {
-        Task task = new Task();
-        given(taskService.getTask(1L)).willReturn(task);
-
         mockMvc.perform(get("/tasks/1"))
                 .andExpect(status().isOk());
     }
 
     @Test
     void detailWithInvalidId() throws Exception {
-        given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
-
         mockMvc.perform(get("/tasks/100"))
                 .andExpect(status().isNotFound());
     }
