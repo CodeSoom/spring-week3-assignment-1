5e88341b668d6328acf66370eb944c332bb90c12
hyuk <pjh08190819@naver.com>
2021-12-19 00:27:57 +0900

    healthcheck 에 대한 테스트 코드 작성

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -3,24 +3,43 @@ package com.codesoom.assignment;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("App 클래스")
 class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Nested
     @DisplayName("getGreeting() 메소드는 ")
     class Describe_getGreeting {
         @Test
         @DisplayName("서버 생존용 문자열을 리턴합니다.")
-        void it_return_hello_world() {
+        void it_return_string() {
             App app = new App();
 
             assertNotNull(app.getGreeting());
         }
     }
 
+    @Nested
+    @DisplayName("GET Mapping - '/healthcheck'는")
+    class Describe_get_healthcheck {
+        @Test
+        @DisplayName("200(Ok)와 서버 생존용 문자열을 리턴합니다.")
+        void it_return_ok_and_string() throws Exception {
+            mockMvc.perform(get("/healthcheck"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$").isNotEmpty())
+                    .andDo(print());
+        }
+    }
 }
