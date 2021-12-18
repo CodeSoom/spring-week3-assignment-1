be9b3ad265d1f5e201954155aacb9985e0f8d2ca
kyuwon <kyuwoon369@gmail.com>
2021-12-18 18:25:53 +0900

    [Fix] getTasks 테스트를 분기하라

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -5,6 +5,8 @@ import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,12 +18,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -39,29 +41,51 @@ class TaskControllerWebTest {
     @MockBean
     private TaskService taskService;
 
-    @BeforeEach
-    void setUp() {
-        List<Task> tasks = new ArrayList<>();
+    @Nested
+    @DisplayName("GET Task 조회 요청은")
+    class Describe_get {
+        @Nested
+        @DisplayName("등록된 Task가 있다면")
+        class context_has_task {
+            final int taskCnt = 3;
 
-        Task task = new Task();
-        task.setTitle(TASK_TITLE);
-        tasks.add(task);
+            @BeforeEach
+            void setUp() {
+                for (int i = 0; i < taskCnt; i++) {
+                    taskService.createTask(getTask());
+                }
+            }
 
-        taskService.createTask(task);
-
-        given(taskService.getTasks()).willReturn(tasks);
-        given(taskService.getTask(1L)).willReturn(task);
-
-        given(taskService.getTask(100L))
-                .willThrow(new TaskNotFoundException(100L));
+            @Test
+            @DisplayName("전체 리스트를 리턴한다.")
+            void it_return_list() throws Exception {
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.*", hasSize(taskCnt)));
+            }
+        }
     }
 
-    @Test
-    void list() throws Exception {
-        mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(containsString(TASK_TITLE)));
+    @Nested
+    @DisplayName("등록된 Task가 없다면")
+    class context_none_task {
+        final int taskCnt = 3;
+
+        @BeforeEach
+        void setUp() {
+            List<Task> tasks = taskService.getTasks();
+            tasks.forEach(task -> taskService.deleteTask(task.getId()));
+        }
+
+        @Test
+        @DisplayName("빈 리스트를 리턴한다.")
+        void it_return_list() throws Exception {
+            mockMvc.perform(get("/tasks"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.*", hasSize(0)));
+        }
     }
+
 
     @Test
     void detailWithValidId() throws Exception {
@@ -72,15 +96,14 @@ class TaskControllerWebTest {
 
     @Test
     void detailWithInvalidId() throws Exception {
-        mockMvc.perform(get("/tasks/100"))
+        mockMvc.perform(get("/tasks/0"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
     void create() throws Exception {
-        Task task = new Task();
-        task.setTitle(NEW_TITLE);
-        String content = objectMapper.writeValueAsString(task);
+
+        String content = objectMapper.writeValueAsString(getTask());
 
         mockMvc.perform(post("/tasks")
                         .accept(MediaType.APPLICATION_JSON)
@@ -88,7 +111,8 @@ class TaskControllerWebTest {
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(print())
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$task.title").value(NEW_TITLE));
         //.andExpect(content().string(containsString(NEW_TITLE)));
         // 동작이 되지 않는 이유?
         // Response content
@@ -157,5 +181,12 @@ class TaskControllerWebTest {
     void deleteWithInvalidId() throws Exception {
         mockMvc.perform(delete("/tasks/0"))
                 .andExpect(status().isNotFound());
+    }
+
+    private Task getTask() {
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+
+        return task;
     }
 }
