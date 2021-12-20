5e14d953f94a91eace11ea17b7168db295245253
hyeongjoGwak <hyeongjo.Gwak@kurlycorp.com>
2021-12-20 23:57:27 +0900

    [풀이] TaskController 에 대한 유닛 테스트 풀이 작성
    TaskController에 대한 유닛 테스트를 작성합니다.

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,11 +1,21 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 class TaskControllerTest {
     private TaskController controller;
@@ -16,58 +26,90 @@ class TaskControllerTest {
 
     @BeforeEach
     void setUp() {
-        taskService = new TaskService();
-        controller = new TaskController(taskService);
+        taskService = mock(TaskService.class);
 
         Task task = new Task();
         task.setTitle(TASK_TITLE);
-        controller.create(task);
+        List<Task> tasks = new ArrayList<>();
+        tasks.add(task);
+
+        given(taskService.getTasks()).willReturn(tasks);
+        given(taskService.getTask(1L)).willReturn(task);
+        given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
+        given(taskService.updateTask(eq(100L), any(Task.class))).willThrow(new TaskNotFoundException(100L));
+        given(taskService.deleteTask(100L)).willThrow(new TaskNotFoundException(100L));
+
+        controller = new TaskController(taskService);
     }
 
     @Test
-    void list() {
+    void listWithSomeTasks() {
         assertThat(controller.list()).isNotEmpty();
+
+        verify(taskService).getTasks();
     }
 
     @Test
-    void detail() {
+    void listWithoutTasks() {
+        given(taskService.getTasks()).willReturn(new ArrayList<>());
+
+        assertThat(controller.list()).isEmpty();
+
+        verify(taskService).getTasks();
+    }
+
+    @Test
+    void detailWithExistId() {
         Task task = controller.detail(1L);
-        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+
+        assertThat(task).isNotNull();
+    }
+
+    @Test
+    void detailWithNotExistId() {
+        assertThatThrownBy(() -> controller.detail(100L))
+                .isInstanceOf(TaskNotFoundException.class);
     }
 
     @Test
     void createNewTask() {
         Task task = new Task();
+        task.setTitle("Test2");
 
-        int oldSize = controller.list().size();
-
-        task.setTitle(TASK_TITLE);
         controller.create(task);
 
-        int newSize = controller.list().size();
-
-        assertThat(newSize - oldSize).isEqualTo(1);
+        verify(taskService).createTask(task);
     }
 
     @Test
-    void deleteTask() {
-        int oldSize = controller.list().size();
+    void deleteTaskWithExistId() {
+       controller.delete(1L);
 
-        controller.delete(1L);
-
-        int newSize = controller.list().size();
-
-        assertThat(oldSize - newSize).isEqualTo(1);
+       verify(taskService).deleteTask(1L);
     }
 
     @Test
-    void updateTask() {
-        Task source = new Task();
-        source.setTitle(UPDATE_PREFIX + TASK_TITLE);
+    void deleteTaskWithNotExistId() {
+        assertThatThrownBy(() -> controller.delete(100L))
+                .isInstanceOf(TaskNotFoundException.class);
+    }
 
-        controller.update(1L, source);
+    @Test
+    void updateTaskWithExistId() {
+        Task task = new Task();
+        task.setTitle(UPDATE_PREFIX + TASK_TITLE);
 
-        assertThat(controller.detail(1L).getTitle())
-                .isEqualTo(UPDATE_PREFIX + TASK_TITLE);
+        controller.update(1L, task);
+
+        verify(taskService).updateTask(1L, task);
+    }
+
+    @Test
+    void updateTaskWithNotExistId() {
+        Task task = new Task();
+        task.setTitle(UPDATE_PREFIX + TASK_TITLE);
+
+        assertThatThrownBy(() -> controller.update(100L, task))
+                .isInstanceOf(TaskNotFoundException.class);
     }
 }
