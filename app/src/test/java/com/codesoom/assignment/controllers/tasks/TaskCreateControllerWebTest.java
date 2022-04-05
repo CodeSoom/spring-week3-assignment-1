66ae9646d9eabe10f8f6f62f7ce2d44d668c8c39
subin606 <gaemilee606@gmail.com>
2022-04-05 10:41:20 +0900

    test: 테스트에 필요한 값 상수값으로 정의

--- app/src/test/java/com/codesoom/assignment/controllers/tasks/TaskCreateControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/tasks/TaskCreateControllerWebTest.java
@@ -1,28 +1,21 @@
 package com.codesoom.assignment.controllers.tasks;
 
-import com.codesoom.assignment.domains.Task;
 import com.codesoom.assignment.domains.TaskDto;
-import com.codesoom.assignment.services.TaskCreateService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,18 +27,29 @@ class TaskCreateControllerWebTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    final String TASK_TITLE = "task";
-    final TaskDto CREATE_TASK = new TaskDto(TASK_TITLE);
+    private static final String TASK_TITLE = "task";
+    private static final TaskDto VALID_TASK_DTO = new TaskDto(TASK_TITLE);
+    private static final TaskDto INVALID_TASK_DTO = new TaskDto("");
 
     @DisplayName("할 일을 성공적으로 추가한다.")
     @Test
     void addTask() throws Exception {
         mockMvc.perform(post("/tasks")
-                .content(objectMapper.writeValueAsString(CREATE_TASK))
+                .content(objectMapper.writeValueAsString(VALID_TASK_DTO))
                 .contentType(MediaType.APPLICATION_JSON))
-        .andExpect(status().isCreated())
-        .andExpect(content().string(containsString(TASK_TITLE)))
-        .andDo(print());
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString(TASK_TITLE)))
+                .andDo(print());
+    }
+
+    @DisplayName("빈 값의 할 일을 추가하면 400 에러를 응답한다.")
+    @Test
+    void validateTask() throws Exception {
+        mockMvc.perform(post("/tasks")
+                .content(objectMapper.writeValueAsString(INVALID_TASK_DTO))
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
     }
 
 }
