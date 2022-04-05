d0a6fa364c5f92a4b67b4d13aa528420fa939c0a
pjkfckr <pjkfckr@gmail.com>
2022-04-05 20:26:48 +0900

    Add unit test for TaskService

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -19,10 +19,6 @@ class TaskServiceTest {
 
     @Test
     void getTasks() {
-        // given
-
-        // when
-
         // then
         assertThat(taskService.getTasks()).isEmpty();
     }
@@ -38,8 +34,8 @@ class TaskServiceTest {
         Task found = taskService.getTask(1L);
 
         // then
+        assertThat(found.getId()).isEqualTo(1L);
         assertThat(found.getTitle()).isEqualTo(TASK_TITLE);
-
     }
 
     @Test
@@ -54,15 +50,26 @@ class TaskServiceTest {
         // given
         int oldSize = taskService.getTasks().size();
 
-        Task task = new Task();
-        task.setTitle(TASK_TITLE);
-        taskService.createTask(task);
+        Task task1 = new Task();
+        task1.setTitle(TASK_TITLE);
+        taskService.createTask(task1);
+
+        Task task2 = new Task();
+        taskService.createTask(task2);
 
         // when
         int newSize = taskService.getTasks().size();
+        Task foundTask1 = taskService.getTask(1L);
+        Task foundTask2 = taskService.getTask(2L);
 
         // then
-        assertThat(newSize - oldSize).isEqualTo(1);
+        assertThat(newSize - oldSize).isEqualTo(2);
+
+        assertThat(foundTask1.getTitle()).isNotNull();
+        assertThat(foundTask1.getId()).isEqualTo(1L);
+
+        assertThat(foundTask2.getTitle()).isNull();
+        assertThat(foundTask2.getId()).isEqualTo(2L);
     }
 
     @Test
@@ -77,6 +84,8 @@ class TaskServiceTest {
         Task updatedTask = taskService.updateTask(1L, task);
 
         // then
+        assertThatThrownBy(() -> taskService.updateTask(2L, task))
+                .isInstanceOf(TaskNotFoundException.class);
         assertThat(updatedTask.getTitle()).isEqualTo("updateTitle");
     }
 
@@ -87,6 +96,24 @@ class TaskServiceTest {
                 .isInstanceOf(TaskNotFoundException.class);
     }
 
+    @Test
+    void deleteTaskWithValidId() throws Exception {
+        // given
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+        taskService.createTask(task);
+        int oldSize = taskService.getTasks().size();
+
+        // when
+        taskService.deleteTask(1L);
+        int newSize = taskService.getTasks().size();
+
+        // then
+        assertThatThrownBy(() -> taskService.deleteTask(1L))
+                .isInstanceOf(TaskNotFoundException.class);
+        assertThat(oldSize).isEqualTo(1);
+        assertThat(newSize).isEqualTo(0);
+    }
 
 
     @Test
