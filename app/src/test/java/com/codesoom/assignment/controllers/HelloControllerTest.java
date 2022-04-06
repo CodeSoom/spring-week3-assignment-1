387dd95015a53e9273fad2445fd58e3035e19679
davidyang2149 <davidyang2149@gmail.com>
2022-04-06 21:11:28 +0900

    HelloControllerTest를 계층 구조를 가진 테스트코드로 개선합니다

--- app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerTest.java
@@ -1,10 +1,13 @@
 package com.codesoom.assignment.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("HelloController 클래스")
 class HelloControllerTest {
     private HelloController controller;
 
@@ -13,10 +16,19 @@ class HelloControllerTest {
         controller = new HelloController();
     }
 
-    @Test
-    void sayHello() {
-        String result = controller.sayHello();
+    @Nested
+    @DisplayName("sayHello 메서드는")
+    class Describe_sayHello {
+        @Nested
+        @DisplayName("path가 '/'로 들어오면")
+        class Context_with_default_path {
+            @Test
+            @DisplayName("환영 메세지를 반환합니다")
+            void sayHello() {
+                String result = controller.sayHello();
 
-        assertThat(result).isEqualTo("Hello, world!");
+                assertThat(result).isEqualTo("Hello, world!");
+            }
+        }
     }
 }
