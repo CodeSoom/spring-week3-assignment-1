19cb12bd8ef8076b2854f5acc218a2534d5d8ca5
davidyang2149 <davidyang2149@gmail.com>
2022-04-06 20:58:09 +0900

    HelloController를 설명하는 JavaDoc를 작성합니다

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -3,6 +3,9 @@ package com.codesoom.assignment.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Todo 서버가 작동하는지 확인할 수 있는 메세지를 반환합니다.
+ */
 @RestController
 public class HelloController {
     @RequestMapping("/")
