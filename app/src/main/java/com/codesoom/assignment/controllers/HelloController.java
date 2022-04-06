185866278e50c22fd5d818943950690c49c49d49
davidyang2149 <davidyang2149@gmail.com>
2022-04-06 22:42:44 +0900

    HelloController의 주석을 더 명확하게 작성합니다

--- app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
+++ app/src/main/java/com/codesoom/assignment/controllers/HelloController.java
@@ -4,10 +4,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Todo 서버가 작동하는지 확인할 수 있는 메세지를 반환합니다.
+ * 서버가 작동하는지 확인하는 역할을 합니다.
  */
 @RestController
 public class HelloController {
+    /**
+     * 서버가 작동하는지 확인하는 메세지를 반환합니다.
+     * @return 인사 메세지
+     */
     @RequestMapping("/")
     public String sayHello() {
         return "Hello, world!";
