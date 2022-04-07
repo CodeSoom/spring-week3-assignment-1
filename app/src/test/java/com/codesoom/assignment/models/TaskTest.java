202a4817df7d399f2dee12f34f804c15dd0ad7af
minhee.son <minhee4735@gmail.com>
2022-04-07 15:45:40 +0900

    test Task title validation 매소드에 대한 테스트 추가

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,10 +1,14 @@
 package com.codesoom.assignment.models;
 
+import com.codesoom.assignment.NotProperTaskFormatException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TaskTest {
 
@@ -31,5 +35,16 @@ class TaskTest {
         task.setTitle(TASK_TITLE);
 
         assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @DisplayName("할일의 제목이 없거나 빈값일 경우 오류발생")
+    void taskValidationTest(String title) {
+        task.setTitle(title);
+
+        assertThatThrownBy(() -> task.validation())
+                .isInstanceOf(NotProperTaskFormatException.class);
     }
 }
