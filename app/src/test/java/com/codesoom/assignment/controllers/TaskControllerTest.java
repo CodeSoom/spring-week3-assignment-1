a4b4b4a9a9a33e2948700588a3ff2c177f1bd162
5Hanui <gkdud644@naver.com>
2022-04-07 17:48:31 +0900

    TaskControllerTest list 메소드 테스트 추가

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,5 +1,27 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.application.TaskService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("TaskController 클래스")
 class TaskControllerTest {
 
+    private TaskController taskController;
+    private TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        this.taskService = new TaskService();
+        this.taskController = new TaskController(taskService);
+    }
+
+    @Test
+    @DisplayName("list는 모든 할 일 목록을 반환한다.")
+    void list() {
+        assertThat(taskService.getTasks()).isEmpty();
+    }
 }
