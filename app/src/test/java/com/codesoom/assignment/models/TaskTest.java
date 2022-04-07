1b3eda47aa6962ab748dd233ee043916f9d29efb
5Hanui <gkdud644@naver.com>
2022-04-07 16:44:37 +0900

    TaskTest 테스트메소드 이름 추가

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,10 +1,12 @@
 package com.codesoom.assignment.models;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DisplayName("Task 클래스")
 class TaskTest {
 
     private final static Long TASK_ID = 1L;
@@ -20,11 +22,13 @@ class TaskTest {
     }
 
     @Test
+    @DisplayName("getId는 할 일의 아이디를 반환한다.")
     void getId() {
         assertThat(task.getId()).isEqualTo(TASK_ID);
     }
 
     @Test
+    @DisplayName("setId는 할 일의 아이디를 지정한다.")
     void setId() {
         Long newId = TASK_ID + 1L;
         task.setId(newId);
@@ -34,11 +38,13 @@ class TaskTest {
     }
 
     @Test
+    @DisplayName("getTitle는 할 일의 제목을 반환한다.")
     void getTitle() {
         assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
     }
 
     @Test
+    @DisplayName("setTitle는 할 일의 제목을 지정한다.")
     void setTitle() {
         String newTitle = "New " + TASK_TITLE;
         task.setTitle(newTitle);
