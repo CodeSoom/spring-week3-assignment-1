6a7bbd51d28dcb50a35675a34563ff50518637ce
Jeon Gyedong <jeongyedong@gmail.com>
2022-04-09 21:59:16 +0900

    TST: TaskController 웹테스트 가독성 개선, Task 타이틀 실제 예로 변경
    - mockMVC에 인코딩 설정 "UTF-8" 추가 (참고: https://tychejin.tistory.com/333)

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -12,6 +12,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,20 +36,28 @@ class TaskControllerWebTest {
     private MockMvc mockMvc;
 
     @Autowired
+    private WebApplicationContext ctx;
+
+    @Autowired
     private ObjectMapper objectMapper;
 
     @MockBean
     private TaskService taskService;
 
-    private static final String TASK_TITLE = "task test";
+    private static final String TASK_TITLE = "책 읽기";
     private static final String UPDATE_POSTFIX = "!";
 
     @BeforeEach
     void setUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
         List<Task> tasks = new ArrayList<>();
         Task task = new Task();
+        task.setId(1L);
         task.setTitle(TASK_TITLE);
         tasks.add(task);
+
         given(taskService.getTasks()).willReturn(tasks);
         given(taskService.getTask(1L)).willReturn(task);
         given(taskService.getTask(100L)).willThrow(new TaskNotFoundException(100L));
@@ -54,7 +65,9 @@ class TaskControllerWebTest {
 
     @Test
     void list() throws Exception {
-        mockMvc.perform(get("/tasks")).andExpect(status().isOk()).andExpect(content().string(containsString(TASK_TITLE)));
+        mockMvc.perform(get("/tasks"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString(TASK_TITLE)));
     }
 
     @Test
@@ -72,6 +85,7 @@ class TaskControllerWebTest {
         Task task = new Task();
         task.setTitle(TASK_TITLE);
         String content = objectMapper.writeValueAsString(task);
+
         mockMvc.perform(post("/tasks")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -83,6 +97,7 @@ class TaskControllerWebTest {
         Task task = new Task();
         task.setTitle(TASK_TITLE + UPDATE_POSTFIX);
         String content = objectMapper.writeValueAsString(task);
+
         mockMvc.perform(put("/tasks/1")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON))
@@ -94,6 +109,7 @@ class TaskControllerWebTest {
         Task task = new Task();
         task.setTitle(TASK_TITLE + UPDATE_POSTFIX);
         String content = objectMapper.writeValueAsString(task);
+
         mockMvc.perform(patch("/tasks/1")
                         .content(content)
                         .contentType(MediaType.APPLICATION_JSON))
