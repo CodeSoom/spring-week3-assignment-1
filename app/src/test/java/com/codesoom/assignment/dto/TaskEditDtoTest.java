1fa55d115409841a57ffbcba366ae6b3da5ee089
shineoov <shineoov88@gmail.com>
2022-04-09 20:32:37 +0900

    style: 줄바꿈 수정

--- app/src/test/java/com/codesoom/assignment/dto/TaskEditDtoTest.java
+++ app/src/test/java/com/codesoom/assignment/dto/TaskEditDtoTest.java
@@ -46,7 +46,6 @@ class TaskEditDtoTest {
                 assertThatThrownBy(
                         taskEditDto::validate
                 ).isInstanceOf(IllegalArgumentException.class);
-
             }
         }
     }
