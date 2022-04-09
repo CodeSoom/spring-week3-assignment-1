9c72a968ba46784d90e3d1ecf9530daf2cdaed4b
shineoov <shineoov88@gmail.com>
2022-04-09 14:53:10 +0900

    test: TaskSaveDto toEntity 실패 테스트 케이스 추가

---
+++ TODO.md
@@ -0,0 +1,3 @@
+## TODO
+
+- [X] TaskSaveDto.toEntity 실패하는 테스트 케이스 추가
--- app/src/test/java/com/codesoom/assignment/dto/TaskSaveDtoTest.java
+++ app/src/test/java/com/codesoom/assignment/dto/TaskSaveDtoTest.java
@@ -4,6 +4,9 @@ import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -18,7 +21,7 @@ class TaskSaveDtoTest {
     class Describe_toEntity {
 
         @Nested
-        @DisplayName("할 일 등록에 필요한 데이터로")
+        @DisplayName("할 일 내용이 정상적으로 있다면")
         class Context_validData {
 
             final TaskSaveDto taskSaveDto = new TaskSaveDto(TASK_TITLE);
@@ -35,15 +38,21 @@ class TaskSaveDtoTest {
         }
 
         @Nested
-        @DisplayName("할 일 등록에 필요한 데이터가 없다면")
-        class context_inValidData {
+        @DisplayName("할 일 내용이")
+        class Context_titleValue {
 
-            final TaskSaveDto taskSaveDto = new TaskSaveDto("");
+            TaskSaveDto taskSaveDto;
 
-            @Test
-            @DisplayName("예외를 던진다.")
-            void it_throw_exception() {
-                assertThatThrownBy(taskSaveDto::toEntity)
+            void subject(String title) {
+                taskSaveDto = new TaskSaveDto(title);
+                taskSaveDto.toEntity();
+            }
+
+            @ParameterizedTest(name = "\"{0}\" 이라면 예외를 던진다.")
+            @NullAndEmptySource
+            @ValueSource(strings = {" "})
+            void it_throw_exception(String given) {
+                assertThatThrownBy(() -> subject(given))
                         .isInstanceOf(IllegalArgumentException.class);
             }
         }
