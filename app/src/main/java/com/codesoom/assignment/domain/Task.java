86b5be5d28464e99413c5f7105ca86a98c24249a
hy43 <hyde0401@gmail.com>
2022-05-17 06:33:30 +0900

    feat : title 유효성 검사를 위한 hasValidTitle 메소드 추가(초안)

---
+++ app/src/main/java/com/codesoom/assignment/domain/Task.java
@@ -0,0 +1,11 @@
+package com.codesoom.assignment.domain;
+
+public class Task {
+
+    private Long id;
+    private String title;
+
+    public boolean hasValidTitle() {
+        return false;
+    }
+}
