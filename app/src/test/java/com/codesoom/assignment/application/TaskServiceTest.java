5fbfccd175385c455a47e9faf4a0da94d2595f60
MJbae <bmj5208@gmail.com>
2022-05-17 20:17:58 +0900

    TaskService 내 테스트 설명부분을 읽기 쉽게 수정

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -32,10 +32,10 @@ class TaskServiceTest {
     @DisplayName("getTasks 메소드는")
     class Describe_getTasks {
         @Nested
-        @DisplayName("만약 기본 생성된 Task 1개만 존재한다면")
+        @DisplayName("만약 기본 생성된 Task가 1개만 존재한다면")
         class Context_with_default_task {
             @Test
-            @DisplayName("반환값의 사이즈는 1이다")
+            @DisplayName("사이즈가 1인 콜렉션을 반환한다")
             void it_returns_1_size_of_Collection() {
                 final int actual = service.getTasks().size();
                 assertThat(actual).isEqualTo(TASKS_SIZE);
@@ -50,15 +50,15 @@ class TaskServiceTest {
         @DisplayName("만약 기본 생성된 Task를 조회한다면")
         class Context_with_default_task {
             @Test
-            @DisplayName("기본 생성된 Task의 id값을 반환한다")
-            void it_returns_default_task_id() {
+            @DisplayName("Task를 반환하는데, 반환한 Task의 id와 기본 생성된 Task의 id는 동일하다")
+            void it_returns_task_having_id_equal_to_default_task_id() {
                 final Long actual = service.getTask(TASK_ID).getId();
                 assertThat(actual).isEqualTo(TASK_ID);
             }
 
             @Test
-            @DisplayName("기본 생성된 Task의 title값을 반환한다")
-            void it_returns_default_task_title() {
+            @DisplayName("Task를 반환하는데, 반환한 Task의 title과 기본 생성된 Task의 title은 동일하다")
+            void it_returns_task_having_title_equal_to_default_task_title() {
                 final String actual = service.getTask(TASK_ID).getTitle();
                 assertThat(actual).isEqualTo(TASK_TITLE);
             }
@@ -68,7 +68,7 @@ class TaskServiceTest {
         @DisplayName("만약 존재하지 않는 Task를 조회한다면")
         class Context_with_not_existing_task {
             @Test
-            @DisplayName("TaskNotFoundException이 발생한다")
+            @DisplayName("TaskNotFoundException을 발생시킨다")
             void it_throws_exception() {
                 assertThatThrownBy(() -> service.getTask(TASK_ID_NOT_EXISTING))
                         .isInstanceOf(TaskNotFoundException.class);
