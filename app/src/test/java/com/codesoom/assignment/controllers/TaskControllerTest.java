c28ae6f90d9b5bac1dfd9f933ec3d4d67a4b3500
MJbae <bmj5208@gmail.com>
2022-05-17 15:13:11 +0900

    Controller에 대한 테스트의 메소드 이름을 간소화히기 위해 DisplayName 어노테이션 활용

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.dto.ErrorResponse;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,7 +30,8 @@ class TaskControllerTest {
     }
 
     @Test
-    void Given_Task가_하나만_존재_When_목록_조회_요청_Then_반환값_내_Task_개수는_Service_반환값_개수와_같다() {
+    @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환값 내 Task의 개수는 1개여야 한다")
+    void whenList_returnEqualSize() {
         List<Task> expected = service.getTasks();
         List<Task> actual = controller.list();
 
@@ -37,28 +39,32 @@ class TaskControllerTest {
     }
 
     @Test
-    void Given_Task가_하나만_존재_When_목록_조회_요청_Then_반환된_Task_id_는_Service_내_Task_id와_같아야한다() {
+    @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환된 Task id는 기본 생성된 Task의 id와 같아야한다")
+    void whenList_returnEqualId() {
         Long actual = controller.list().get(FIRST).getId();
 
         assertThat(actual).isEqualTo(TASK_ID);
     }
 
     @Test
-    void Given_Task가_하나만_존재_When_목록_조회_요청_Then_반환된_Task_title_는_Service_내_Task_title와_같아야한다() {
+    @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환된 Task title은 기본 생성된 Task의 title과 같아야한다")
+    void whenList_returnEqualTitle() {
         String actual = controller.list().get(FIRST).getTitle();
 
         assertThat(actual).isEqualTo(TASK_TITLE);
     }
 
     @Test
-    void Given_Task가_하나만_존재_When_상세_조회_요청_Then_반환된_Task_id_는_Service_내_Task_id와_같아야한다() {
+    @DisplayName("기본 생성된 하나만 존재하고, Task Id로 정상적으로 상세조회 요청 시 반환된 Task id는 기본 생성된 Task의 id와 같아야한다")
+    void whenDetail_returnEqualId() {
         Long actual = controller.detail(TASK_ID).getId();
 
         assertThat(actual).isEqualTo(TASK_ID);
     }
 
     @Test
-    void Given_Task가_하나만_존재_When_상세_조회_요청_Then_반환된_Task_title_는_Service_내_Task_title와_같아야한다() {
+    @DisplayName("기본 생성된 하나만 존재하고, Task Id로 정상적으로 상세조회 요청 시 반환된 Task title은 기본 생성된 Task의 title과 같아야한다")
+    void whenDetail_returnEqualTitle() {
         String actual = controller.detail(TASK_ID).getTitle();
 
         assertThat(actual).isEqualTo(TASK_TITLE);
