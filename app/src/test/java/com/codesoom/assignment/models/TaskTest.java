0c7fd3aaef3482d926172aaba81860f8d16ff8d9
HanJiyoung <jiyounghaan@gmail.com>
2022-05-18 19:41:47 +0900

    Task 모델의 getId테스트에서 테스트 대상을 실행하는 코드를 캡슐화하라
    테스트 대상에 대한 코드를 task()로 캡슐화하여, 테스트 대상에 변경이 발생하더라도 이에 딸린 테스트 코드들을 모두 수정할 필요가 없도록 했습니다.

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -12,18 +12,16 @@ class TaskTest {
         @Nested
         @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
         class 만약_id가_1인_Task_객체가_주어진다면 {
-            private Task task;
-
-            @BeforeEach
-            void setUp() {
-                task = new Task("과제하기");
+            Task task() {
+                Task task = new Task("과제하기");
                 task.setId(1L);
+                return task;
             }
 
             @Test
             @DisplayName("id의 값으로 1을 반환한다")
             void id의_값으로_1을_반환한다() {
-                assertThat(task.getId()).isEqualTo(1L);
+                assertThat(task().getId()).isEqualTo(1L);
             }
         }
     }
