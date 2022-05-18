531571e5c61202a130d647c7665759cac09c57e3
10M <endlesslhk@gmail.com>
2022-05-18 16:23:28 +0900

    feat(TaskControllerTest): `id로 할일 가져오기` 테스트 추가

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -1,7 +1,9 @@
 package com.codesoom.assignment.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
@@ -32,5 +34,27 @@ class TaskControllerTest {
         taskService.createTask(new Task());
 
         assertThat(taskController.list()).hasSize(2);
+    }
+
+    @DisplayName("유효한 `id`로 할일 가져오기 테스트")
+    @Test
+    void taskWithValidId() {
+        Task task = new Task();
+        task.setTitle("Task 1");
+
+        taskService.createTask(task);
+
+        assertThat(taskController.detail(1L).getTitle()).isEqualTo("Task 1");
+    }
+
+    @DisplayName("유효하지 않은 `id`로 할일 가져오기 테스트")
+    @Test
+    void taskWithInValidId() {
+        Task task = new Task();
+        task.setTitle("Task 1");
+
+        taskService.createTask(task);
+
+        assertThatThrownBy(() -> taskController.detail(100L).getTitle()).isInstanceOf(TaskNotFoundException.class);
     }
 }
