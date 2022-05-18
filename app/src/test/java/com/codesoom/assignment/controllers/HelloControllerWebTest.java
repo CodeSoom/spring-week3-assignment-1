0e49bf3a10e97b924ba992c14742bd50bf0061db
10M <endlesslhk@gmail.com>
2022-05-18 15:54:12 +0900

    feat(HelloControllerWebTest): MockMvc를 이용한 `HelloController` 테스트

---
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerWebTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void sayHelloTest() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, world!!!")));
+    }
+}
