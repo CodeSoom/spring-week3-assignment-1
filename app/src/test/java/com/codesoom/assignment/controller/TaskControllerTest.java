86476a7aa00a281892e67631eda963575108d294
hy43 <hyde0401@gmail.com>
2022-05-19 08:00:41 +0900

    test : MockMvc에 대한 @Autowired 추가.

--- app/src/test/java/com/codesoom/assignment/controller/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controller/TaskControllerTest.java
@@ -3,6 +3,7 @@ package com.codesoom.assignment.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Nested;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -13,11 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @DisplayName("TaskController Web Test")
 class TaskControllerTest {
 
+    @Autowired
     private MockMvc mockMvc;
-
-    public TaskControllerTest(MockMvc mockMvc) {
-        this.mockMvc = mockMvc;
-    }
 
     @Nested
     @DisplayName("GET /tasks")
