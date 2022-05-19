253bef0f0e0df8f82ca191becfeb5bc06a72612d
10M <endlesslhk@gmail.com>
2022-05-19 13:39:12 +0900

    feat(TaskControllerTest): `TaskController` 테스트 케이스 추가

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -21,13 +21,13 @@ class TaskControllerTest {
         taskController = new TaskController(taskService);
     }
 
-    @DisplayName("할일 목록이 비었을 때 테스트")
+    @DisplayName("할일 목록이 비었을 때 빈 리스트 반환 테스트")
     @Test
     void emptyListTest() {
         assertThat(taskController.list()).hasSize(0);
     }
 
-    @DisplayName("할일 목록이 비어있지 않을 때 테스트")
+    @DisplayName("할일 목록이 비어있지 않을 때 리스트 반환 테스트")
     @Test
     void notEmptyListTest() {
         taskService.createTask(new Task());
@@ -56,5 +56,65 @@ class TaskControllerTest {
         taskService.createTask(task);
 
         assertThatThrownBy(() -> taskController.detail(100L).getTitle()).isInstanceOf(TaskNotFoundException.class);
+    }
+
+    @DisplayName("할 일 만든 후 생성된 결과 리턴")
+    @Test
+    void createTaskReturnResult() {
+        String title = "Task 1";
+        Task task = new Task();
+        task.setTitle(title);
+
+        Task resultTask = taskController.create(task);
+
+        assertThat(resultTask.getId()).isEqualTo(1L);
+        assertThat(resultTask.getTitle()).isEqualTo(title);
+    }
+
+    @DisplayName("기존에 생성된 할 일의 제목 수정 후 수정된 결과 리턴 : Update 이용")
+    @Test
+    void updateTaskReturnResult() {
+        String title = "Task 1";
+        String newTitle = "New " + title;
+        Task task = new Task();
+        task.setTitle(title);
+        Task resultTask = taskService.createTask(task);
+        ;
+        resultTask.setTitle(newTitle);
+
+        Task updateResultTask = taskController.update(resultTask.getId(), resultTask);
+
+        assertThat(updateResultTask.getTitle()).isEqualTo(newTitle);
+    }
+
+    @DisplayName("기존에 생성된 할 일의 제목 수정 후 수정된 결과 리턴 : Patch 이용")
+    @Test
+    void patchTaskReturnResult() {
+        String title = "Task 1";
+        String newTitle = "New " + title;
+        Task task = new Task();
+        task.setTitle(title);
+        Task resultTask = taskService.createTask(task);
+        ;
+        resultTask.setTitle(newTitle);
+
+        Task updateResultTask = taskController.patch(resultTask.getId(), resultTask);
+
+        assertThat(updateResultTask.getTitle()).isEqualTo(newTitle);
+    }
+
+    @DisplayName("할일 목록에서 `id`로 삭제")
+    @Test
+    void deleteTask() {
+        String title = "Task 1";
+        Task task = new Task();
+        task.setTitle(title);
+        Task resultTask = taskService.createTask(task);
+
+        int originalSize = taskService.getTasks().size();
+        taskController.delete(resultTask.getId());
+        int newSize = taskService.getTasks().size();
+
+        assertThat(originalSize - newSize).isEqualTo(1);
     }
 }
