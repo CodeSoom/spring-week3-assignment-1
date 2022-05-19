612a1b79d2dddaf43d63e47896fb01da90a9f10d
yongjunleeme <chesidyong@gmail.com>
2022-05-19 17:41:39 +0900

    test: TaskServiceTest 추가

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -1,11 +1,13 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DisplayName("TaskService 클래스")
@@ -29,7 +31,7 @@ public class TaskServiceTest {
 
     @Nested
     @DisplayName("getTasks 호출은")
-    class Describe_getTasks {
+    class Describe_get_tasks {
 
         @Nested
         @DisplayName("task가 저장돼 있다면")
@@ -60,4 +62,140 @@ public class TaskServiceTest {
         }
     }
 
+    @Nested
+    @DisplayName("getTask 호출은")
+    class Describe_get_task {
+
+        @Nested
+        @DisplayName("저장된 task id를 호출한다면")
+        class Context_with_task_id {
+
+            @BeforeEach
+            void setUp() {
+                taskService.createTask(task);
+            }
+
+            @Test
+            @DisplayName("task를 리턴한다")
+            void it_returns_task() {
+                Task task = taskService.getTask(EXISTING_ID);
+                assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+            }
+        }
+
+        @Nested
+        @DisplayName("저장되지 않은 task id를 호출한다면")
+        class Context_with_not_stored_task_id {
+
+            @Test
+            @DisplayName("TaskNotFoundException이 발생한다")
+            void it_throws_task_not_found_exception() {
+                assertThatThrownBy(() -> taskService.getTask(NOT_EXISTING_ID))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("createTask 호출은")
+    class Describe_create_task {
+
+        @Nested
+        @DisplayName("task가 주어지면")
+        class Context_with_task {
+            int oldSize;
+
+            @BeforeEach
+            void setUp() {
+                oldSize = taskService.getTasks().size();
+            }
+
+            Task subject() {
+                Task newTask = taskService.createTask(task);
+                newTask.setTitle(TASK_TITLE);
+                return newTask;
+            }
+
+            @Test
+            @DisplayName("task를 리턴한다")
+            void it_returns_task() {
+                assertThat(subject().getTitle()).isEqualTo(TASK_TITLE);
+                int newSize = taskService.getTasks().size();
+                assertThat(newSize - oldSize).isEqualTo(1);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("updateTask 호출은")
+    class Describe_update_task {
+
+        Task subject() {
+            task.setId(EXISTING_ID);
+            task.setTitle(TASK_TITLE + UPDATE_POSTFIX);
+            return taskService.createTask(task);
+        }
+
+        @Nested
+        @DisplayName("저장된 task id가 주어지면")
+        class Context_with_stored_task_id {
+
+            @Test
+            @DisplayName("업데이트한 task를 리턴한다")
+            void it_returns_updated_task() {
+                Task task = taskService.updateTask(EXISTING_ID, subject());
+                assertThat(task.getTitle()).isEqualTo(TASK_TITLE + UPDATE_POSTFIX);
+            }
+        }
+
+        @Nested
+        @DisplayName("저장되지 않은 task id가 주어지면")
+        class Context_with_not_stored_task_id {
+
+            @Test
+            @DisplayName("TaskNotFoundException이 발생한다")
+            void it_throws_task_not_found_exception() {
+                assertThatThrownBy(() -> taskService.updateTask(NOT_EXISTING_ID, subject()))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("deleteTask 호출은")
+    class Describe_delete_task {
+
+        @Nested
+        @DisplayName("저장된 task id가 주어지면")
+        class Context_with_stored_task_id {
+
+            int oldSize;
+
+            @BeforeEach
+            void setUp() {
+                taskService.createTask(task);
+                oldSize = taskService.getTasks().size();
+            }
+
+            @Test
+            @DisplayName("task를 삭제한다")
+            void it_deletes_task() {
+                taskService.deleteTask(EXISTING_ID);
+                int newSize = taskService.getTasks().size();
+                assertThat(oldSize - newSize).isEqualTo(1);
+            }
+        }
+
+        @Nested
+        @DisplayName("저장되지 않은 task id가 주어진다면")
+        class Context_with_not_stored_task_id {
+
+            @Test
+            @DisplayName("TaskNotFoundException이 발생한다")
+            void it_throws_task_not_found_exception() {
+                assertThatThrownBy(() -> taskService.deleteTask(NOT_EXISTING_ID))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+    }
 }
