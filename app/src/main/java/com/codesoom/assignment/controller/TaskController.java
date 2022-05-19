a88fe3d31ad3a3e273158d2f30cd36e060e4789e
hy43 <hyde0401@gmail.com>
2022-05-20 08:15:02 +0900

    feat : TaskController 기능 구현(list, detail, create)

--- app/src/main/java/com/codesoom/assignment/controller/TaskController.java
+++ app/src/main/java/com/codesoom/assignment/controller/TaskController.java
@@ -1,7 +1,41 @@
 package com.codesoom.assignment.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.domain.Task;
+import com.codesoom.assignment.dto.TaskDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping("/tasks")
+@CrossOrigin
 public class TaskController {
+
+    private TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<TaskDto> list() {
+        return taskService.list().stream().map(Task::toTaskDto).collect(Collectors.toList());
+    }
+
+    @GetMapping("{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public TaskDto detail(@PathVariable Long id) {
+        return taskService.detail(id).toTaskDto();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TaskDto create(@RequestBody TaskDto taskDto) {
+        return taskService.create(taskDto.toTask()).toTaskDto();
+    }
 }
