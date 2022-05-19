e3340f58ec2522855fdae4d15578437295232502
hy43 <hyde0401@gmail.com>
2022-05-20 07:14:15 +0900

    style : 성공 케이스를 실패 케이스보다 먼저 오도록 재배치

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -8,8 +8,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import javax.persistence.NamedEntityGraph;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -32,17 +30,6 @@ class TaskServiceTest {
     class list {
 
         @Nested
-        @DisplayName("할일이 존재하지 않으면")
-        class when_list_is_empty {
-
-            @Test
-            @DisplayName("빈 목록을 반환합니다.")
-            void list() {
-                assertThat(taskService.list()).isEmpty();
-            }
-        }
-
-        @Nested
         @DisplayName("할일이 존재하면")
         class when_list_is_not_empty {
 
@@ -58,22 +45,23 @@ class TaskServiceTest {
                 assertThat(taskService.list().size()).isEqualTo(1);
             }
         }
+
+        @Nested
+        @DisplayName("할일이 존재하지 않으면")
+        class when_list_is_empty {
+
+            @Test
+            @DisplayName("빈 목록을 반환합니다.")
+            void list() {
+                assertThat(taskService.list()).isEmpty();
+            }
+        }
+
     }
 
     @Nested
     @DisplayName("detail 메소드")
     class detail {
-
-        @Nested
-        @DisplayName("id를 찾을 수 없다면")
-        class when_not_found_task_with_id {
-
-            @Test
-            @DisplayName("TaskNotFoundException 예외를 발생시킵니다.")
-            void detail() {
-                assertThatThrownBy(() -> taskService.detail(NOT_EXIST_ID)).isInstanceOf(TaskNotFoundException.class);
-            }
-        }
 
         @Nested
         @DisplayName("id를 찾았다면")
@@ -94,22 +82,23 @@ class TaskServiceTest {
                 assertThat(taskService.detail(ID).equals(task)).isTrue();
             }
         }
+
+        @Nested
+        @DisplayName("id를 찾을 수 없다면")
+        class when_not_found_task_with_id {
+
+            @Test
+            @DisplayName("TaskNotFoundException 예외를 발생시킵니다.")
+            void detail() {
+                assertThatThrownBy(() -> taskService.detail(NOT_EXIST_ID)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+
     }
 
     @Nested
     @DisplayName("create 메소드")
     class create {
-
-        @Nested
-        @DisplayName("title이 유효하지 않으면")
-        class when_has_invalid_title {
-            @Test
-            @DisplayName("TaskHasInvalidTaskException 예외가 발생합니다.")
-            void create() {
-                Task task = new Task(ID, null);
-                assertThatThrownBy(() -> taskService.create(task)).isInstanceOf(TaskHasInvalidTitleException.class);
-            }
-        }
 
         @Nested
         @DisplayName("id와 title이 유효하면")
@@ -123,6 +112,18 @@ class TaskServiceTest {
                 assertThat(taskService.list().size()).isEqualTo(1);
             }
         }
+
+        @Nested
+        @DisplayName("title이 유효하지 않으면")
+        class when_has_invalid_title {
+            @Test
+            @DisplayName("TaskHasInvalidTaskException 예외가 발생합니다.")
+            void create() {
+                Task task = new Task(ID, null);
+                assertThatThrownBy(() -> taskService.create(task)).isInstanceOf(TaskHasInvalidTitleException.class);
+            }
+        }
+
     }
 
     @Nested
@@ -137,6 +138,17 @@ class TaskServiceTest {
             taskService.create(task);
 
             change = new Task(null, TITLE_TEST_VALUE + MODIFY_POSTFIX);
+        }
+
+        @Nested
+        @DisplayName("id와 title이 유효하면")
+        class when_has_valid_input {
+
+            @Test
+            @DisplayName("변경된 할일 목록을 반환합니다.")
+            void update() {
+                assertThat(taskService.update(ID, change).currentTitle()).isEqualTo(change.currentTitle());
+            }
         }
 
         @Nested
@@ -162,16 +174,6 @@ class TaskServiceTest {
             }
         }
 
-        @Nested
-        @DisplayName("id와 title이 유효하면")
-        class when_has_valid_input {
-
-            @Test
-            @DisplayName("변경된 할일 목록을 반환합니다.")
-            void update() {
-                assertThat(taskService.update(ID, change).currentTitle()).isEqualTo(change.currentTitle());
-            }
-        }
     }
 
     @Nested
@@ -182,17 +184,6 @@ class TaskServiceTest {
         void setUp() {
             Task task = new Task(ID, TITLE_TEST_VALUE);
             taskService.create(task);
-        }
-
-        @Nested
-        @DisplayName("존재하지 않는 id라면")
-        class when_has_invalid_id {
-
-            @Test
-            @DisplayName("TaskNotFoundException 예외를 발생 시킵니다.")
-            void delete() {
-                assertThatThrownBy(() -> taskService.delete(NOT_EXIST_ID)).isInstanceOf(TaskNotFoundException.class);
-            }
         }
 
         @Nested
@@ -213,6 +204,17 @@ class TaskServiceTest {
                 taskService.delete(ID);
                 assertThat(taskService.list().size()).isEqualTo(0);
                 assertThatThrownBy(() -> taskService.delete(ID)).isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("존재하지 않는 id라면")
+        class when_has_invalid_id {
+
+            @Test
+            @DisplayName("TaskNotFoundException 예외를 발생 시킵니다.")
+            void delete() {
+                assertThatThrownBy(() -> taskService.delete(NOT_EXIST_ID)).isInstanceOf(TaskNotFoundException.class);
             }
         }
 
