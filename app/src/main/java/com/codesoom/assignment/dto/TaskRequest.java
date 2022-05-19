6f01bb1abc28f5488e7956bef792c3930825476d
HanJiyoung <jiyounghaan@gmail.com>
2022-05-19 19:23:54 +0900

    TaskRequest 클래스에 title 매개변수를 가지는 생성자를 추가하라
    TaskService 테스트에서 미리 Task를 추가하는 작업을 할 때 특정 title을 가지는 TaskRequest 객체를 생성해 줘야 합니다.
    객체의 일관성을 유지하기 힘든 setter보다 생성자를 통해서 title을 설정해 주는 것이 더 좋다고 생각하여 해당 생성자를 추가하였습니다.

--- app/src/main/java/com/codesoom/assignment/dto/TaskRequest.java
+++ app/src/main/java/com/codesoom/assignment/dto/TaskRequest.java
@@ -3,6 +3,12 @@ package com.codesoom.assignment.dto;
 public class TaskRequest {
     private String title;
 
+    public TaskRequest() {}
+
+    public TaskRequest(String title) {
+        this.title = title;
+    }
+
     public String getTitle() {
         return title;
     }
