8171a0c02bc3c56355a85303562ac54b25dc4115
MJbae <bmj5208@gmail.com>
2022-05-19 10:43:37 +0900

    TaskService 클래스의 updateTask 메소드에 대한 단위 테스트 추가

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -15,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class TaskServiceTest {
     private TaskService service;
     private final String TASK_TITLE = "Test Task";
+    private final String TASK_TITLE_UPDATED = "Updated Task";
     private final Long TASK_ID = 1L;
     private final Long TASK_ID_NOT_EXISTING = 0L;
     private final int TASKS_SIZE = 1;
@@ -76,5 +77,49 @@ class TaskServiceTest {
 
         }
     }
+
+    @Nested
+    @DisplayName("updateTask 메소드는")
+    class Describe_updateTask {
+        @Nested
+        @DisplayName("만약 기본 생성된 Task를 수정한다면")
+        class Context_with_default_task {
+            @Test
+            @DisplayName("Task를 반환하는데, 반환한 Task의 id와 기본 생성된 Task의 id는 동일하다")
+            void it_returns_task_having_id_equal_to_default_task_id() {
+                final Task task = new Task();
+                task.setTitle(TASK_TITLE_UPDATED);
+
+                final Long actual = service.updateTask(TASK_ID, task).getId();
+                assertThat(actual).isEqualTo(TASK_ID);
+            }
+
+            @Test
+            @DisplayName("Task를 반환하는데, 반환한 Task의 title과 요청 시 전달한 Task의 title과 동일한다")
+            void it_returns_task_having_title_equal_to_task_title_delivered() {
+                final Task task = new Task();
+                task.setTitle(TASK_TITLE_UPDATED);
+
+                final String actual = service.updateTask(TASK_ID, task).getTitle();
+                assertThat(actual).isEqualTo(TASK_TITLE_UPDATED);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 존재하지 않는 Task를 조회한다면")
+        class Context_with_not_existing_task {
+            @Test
+            @DisplayName("TaskNotFoundException을 발생시킨다")
+            void it_throws_exception() {
+                final Task task = new Task();
+                task.setTitle(TASK_TITLE_UPDATED);
+
+                assertThatThrownBy(() -> service.updateTask(TASK_ID_NOT_EXISTING, task))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+
+        }
+    }
+    
 
 }
