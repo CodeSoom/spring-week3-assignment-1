b8863a3cc7fa0f733333750ee506b147adfa39be
caoyu-dev <choyumin01@gmail.com>
2022-05-20 22:30:03 +0900

    refactor(TaskTest): @Nested 를 이용한 계층형으로 수정

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,13 +1,11 @@
 package com.codesoom.assignment.models;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@DisplayName("TaskTest 메서드는")
-class TaskTest {
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class TaskTest_클래스의 {
     private static final Long TASK_ID_ONE = 1L;
     private static final Long TASK_ID_TWO = 2L;
     private static final String TASK_TITLE_ONE = "test_One";
@@ -22,30 +20,49 @@ class TaskTest {
         task.setTitle(TASK_TITLE_ONE);
     }
 
-    @Test
-    @DisplayName("id 를 가져올 수 있다.")
-    void getId() {
-        assertThat(task.getId()).isEqualTo(TASK_ID_ONE);
+    @Nested
+    class getId_메서드는 {
+        @Nested
+        class 만약_Task에_값이_있다면 {
+            @Test
+            void id를_가져올_수_있다() {
+                assertThat(task.getId()).isEqualTo(TASK_ID_ONE);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("id 를 저장할 수 있다.")
-    void setId() {
-        task.setId(TASK_ID_TWO);
-        assertThat(task.getId()).isEqualTo(TASK_ID_TWO);
+    @Nested
+    class setId_메서드는 {
+        @Nested
+        class 만약_Task에_값이_없다면 {
+            @Test
+            void id를_저장할_수_있다() {
+                task.setId(TASK_ID_TWO);
+                assertThat(task.getId()).isEqualTo(TASK_ID_TWO);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("title 을 가져올 수 있다.")
-    void getTitle() {
-        assertThat(task.getTitle()).isEqualTo(TASK_TITLE_ONE);
+    @Nested
+    class getTitle_메서드는 {
+        @Nested
+        class 만약_Task에_값이_있다면 {
+            @Test
+            void Title을_가져올_수_있다() {
+                assertThat(task.getTitle()).isEqualTo(TASK_TITLE_ONE);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("title 을 저장할 수 있다.")
-    void setTitle() {
-        task.setTitle(TASK_TITLE_TWO);
-        assertThat(task.getTitle()).isEqualTo(TASK_TITLE_TWO);
+    @Nested
+    class setTitle_메서드는 {
+        @Nested
+        class 만약_Task에_값이_없다면 {
+            @Test
+            void Title을_저장할_수_있다() {
+                task.setTitle(TASK_TITLE_TWO);
+                assertThat(task.getTitle()).isEqualTo(TASK_TITLE_TWO);
+            }
+        }
     }
-
 }
