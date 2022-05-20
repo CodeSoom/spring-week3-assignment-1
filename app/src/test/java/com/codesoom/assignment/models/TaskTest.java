44349d56e6e6edfff85ea146cadac6ac2a480e65
yongjunleeme <chesidyong@gmail.com>
2022-05-20 17:00:01 +0900

    test: Task Model 테스트 추가

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -6,17 +6,37 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Task 모델")
-class TaskTest {
-    static final Long TASK_ID = 1L;
-    static final String TASK_TITLE = "test";
+class TaskTest extends TaskModel {
+    static final Long MODIFIED_TASK_ID = 2L;
+    static final String MODIFIED_TASK_TITLE = "test2";
 
     @Test
-    void create() {
-        Task task = new Task();
-        task.setId(TASK_ID);
-        task.setTitle(TASK_TITLE);
-
+    @DisplayName("getId 메서드는 id를 리턴한다")
+    void getId() {
+        Task task = subject();
         assertThat(task.getId()).isEqualTo(TASK_ID);
+    }
+
+    @Test
+    @DisplayName("getTitle 메서드는 title를 리턴한다")
+    void getTitle() {
+        Task task = subject();
         assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    @DisplayName("setId 메서드는 주어진 id를 저장한다")
+    void setId() {
+        Task task = subject();
+        task.setId(MODIFIED_TASK_ID);
+        assertThat(task.getId()).isEqualTo(MODIFIED_TASK_ID);
+    }
+
+    @Test
+    @DisplayName("setTitle 메서드는 주어진 title를 저장한다")
+    void setTitle() {
+        Task task = subject();
+        task.setTitle(MODIFIED_TASK_TITLE);
+        assertThat(task.getTitle()).isEqualTo(MODIFIED_TASK_TITLE);
     }
 }
