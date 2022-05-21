97b9fd92bc8728709c02b40b986c828eb1604878
MJbae <bmj5208@gmail.com>
2022-05-21 09:59:01 +0900

    TaskController 내 Given When Then 절차에 따른 주석 추가

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -21,86 +21,91 @@ class TaskControllerTest {
 
     @BeforeEach
     void setUp() {
-        Task task = new Task();
-        task.setTitle(TASK_TITLE);
-
+        // GIVEN
         service = new TaskService();
-        service.createTask(task);
+        service.createTask(new Task(TASK_TITLE));
         controller = new TaskController(service);
     }
 
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환값 내 Task의 개수는 1개여야 한다")
     void whenList_returnEqualSize() {
-        final List<Task> expected = service.getTasks();
+        // WHEN
         final List<Task> actual = controller.list();
 
+        // THEN
+        final List<Task> expected = service.getTasks();
         assertThat(actual).hasSameSizeAs(expected);
     }
 
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환된 Task id는 기본 생성된 Task의 id와 같아야 한다")
     void whenList_returnEqualId() {
+        // WHEN
         final List<Task> actual = controller.list();
 
+        // THEN
         assertThat(actual.get(FIRST).getId()).isEqualTo(TASK_ID);
     }
 
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 목록조회 요청 시 반환된 Task title은 기본 생성된 Task의 title과 같아야 한다")
     void whenList_returnEqualTitle() {
+        // WHEN
         final List<Task> actual = controller.list();
 
+        // THEN
         assertThat(actual.get(FIRST).getTitle()).isEqualTo(TASK_TITLE);
     }
 
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 해당 Task 상세조회 요청 시 반환된 Task의 id는 기본 생성된 Task의 id와 같아야 한다")
     void whenDetail_returnEqualId() {
+        // WHEN
         final Task actual = controller.detail(TASK_ID);
 
+        // THEN
         assertThat(actual.getId()).isEqualTo(TASK_ID);
     }
 
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 해당 Task 상세조회 요청 시 반환된 Task의 title은 기본 생성된 Task의 title과 같아야 한다")
     void whenDetail_returnEqualTitle() {
+        // WHEN
         final Task actual = controller.detail(TASK_ID);
 
+        // THEN
         assertThat(actual.getTitle()).isEqualTo(TASK_TITLE);
     }
     @Test
     @DisplayName("기본 생성된 Task가 하나만 존재하면, 해당 Task 생성 요청 시 Task 목록 요청에 따른 반환값의 사이즈는 1이 증가해야 한다")
     void whenCreate_shouldSizeUp() {
-        final int expected = TASKS_SIZE + 1;
-        final Task task = new Task();
-        task.setTitle(INPUT_TITLE);
+        // WHEN
+        controller.create(new Task(INPUT_TITLE));
 
-        controller.create(task);
+        // THEN
         final List<Task> actual = controller.list();
-
-        assertThat(actual.size()).isEqualTo(expected);
+        assertThat(actual.size()).isEqualTo(TASKS_SIZE + 1);
     }
 
 
     @Test
     @DisplayName("Task 생성 요청 시 반환된 Task의 title은 요청 시 전달한 title과 같아야 한다")
     void whenCreate_returnTaskWithFields() {
-        final Task task = new Task();
-        task.setTitle(INPUT_TITLE);
+        // WHEN
+        final Task actual = controller.create(new Task(INPUT_TITLE));
 
-        final Task actual = controller.create(task);
-
+        // THEN
         assertThat(actual.getTitle()).isEqualTo(INPUT_TITLE);
     }
 
     @Test
     @DisplayName("Task 생성 요청 시 반환된 Task의 id는 null일 수 없다")
     void whenCreate_returnTaskWithNotNullId() {
-        final Task task = new Task();
-        task.setTitle(INPUT_TITLE);
+        // WHEN
+        final Task actual = controller.create(new Task(INPUT_TITLE));
 
-        final Task actual = controller.create(task);
+        // THEN
         assertThat(actual.getId()).isNotNull();
     }
 }
