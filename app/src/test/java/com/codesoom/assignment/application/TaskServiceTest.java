a98a6128c50b2e5c4cc06eff543ac5f8d67db9e0
akaster99 <idavid29@naver.com>
2022-06-21 03:43:38 +0900

    Define createTask,updateTask Test at TaskServiceTest

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -15,7 +15,12 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("TaskService 클래스")
 class TaskServiceTest {
+    static final String TASK_TITLE = "Test Title";
+    static final Long TASK_ID = 1L;
+    static final Long INVALID_ID = 12384L;
+    static final String UPDATED_TITLE= "Test Title Updated";
     private TaskService service;
+
     @BeforeEach
     void setUp() {
         service = new TaskService();
@@ -27,29 +32,32 @@ class TaskServiceTest {
     class Describe_getTasks {
         @Nested
         @DisplayName("만약 task가 없다면")
-        class No_task{
+        class No_task {
             @Test
             @DisplayName("빈 리스트를 리턴한다.")
-            void it_return_empty_List(){
+            void it_return_empty_List() {
                 assertThat(service.getTasks()).isEqualTo(new ArrayList());
             }
 
         }
+
         @Nested
         @DisplayName("만약 task가 3개 있으면")
-        class Have_task{
+        class Have_task {
             final static int TASK_SIZE = 3;
+
             @BeforeEach
-            void setup(){
+            void setup() {
                 Task task = new Task();
-                for(int i = 0; i < TASK_SIZE; i++){
-                    task.setTitle("test title-"+ i);
+                for (int i = 0; i < TASK_SIZE; i++) {
+                    task.setTitle("test title-" + i);
                     service.createTask(task);
                 }
             }
+
             @Test
             @DisplayName("크기 3의 리스트를 반환한다.")
-            void it_retruns_tasks(){
+            void it_retruns_tasks() {
                 assertThat(service.getTasks()).hasSize(TASK_SIZE);
             }
         }
@@ -60,8 +68,8 @@ class TaskServiceTest {
     class Describe_getTask {
         @Nested
         @DisplayName("만약 요청한 Id에 해당하는 task를 찾지 못하면")
-        class No_task{
-            final Long INVALID_ID = 32340L;
+        class No_task {
+
             @Test
             @DisplayName("TasksNotFoundException을 발생시킨다.")
             void it_throws_TaskNotFoundException() {
@@ -69,13 +77,12 @@ class TaskServiceTest {
                         .isInstanceOf(TaskNotFoundException.class);
             }
         }
+
         @Nested
         @DisplayName("만약 요청한 Id에 해당하는 task를 찾으면")
-        class has_task{
-            final Long VALID_ID = 1L;
-            static final String TASK_TITLE = "Test Title";
+        class has_task {
             @BeforeEach
-            void setup(){
+            void setup() {
                 Task task = new Task();
                 task.setTitle(TASK_TITLE);
                 service.createTask(task);
@@ -84,20 +91,81 @@ class TaskServiceTest {
             @Test
             @DisplayName("찾은 task를 반환한다.")
             void return_found_task() {
-                assertThat(service.getTask(VALID_ID).getId()).isEqualTo(VALID_ID);
-                assertThat(service.getTask(VALID_ID).getTitle()).isEqualTo(TASK_TITLE);
+                assertThat(service.getTask(TASK_ID).getId()).isEqualTo(TASK_ID);
+                assertThat(service.getTask(TASK_ID).getTitle()).isEqualTo(TASK_TITLE);
             }
         }
 
     }
 
-    @Test
-    void createTask() {
+    @Nested
+    @DisplayName("createTask 메소드는")
+    class Describe_createTask {
+        @Nested
+        @DisplayName("만약 task가 등록되었을 때")
+        class create_Task {
+            Task task;
 
+            @BeforeEach
+            void setup() {
+                task = new Task();
+                task.setTitle(TASK_TITLE);
+            }
+
+            @Test
+            @DisplayName("등록한 Task를 리턴한다.")
+            void it_return_created_task() {
+                assertThat(service.createTask(task).getTitle()).isEqualTo(TASK_TITLE);
+            }
+
+            @Test
+            @DisplayName("getTask를 통해 찾을 수 있다. ")
+            void it_can_be_founded_by_getTask() {
+                assertThatThrownBy(() -> service.getTask(TASK_ID))
+                        .isInstanceOf(TaskNotFoundException.class);
+                service.createTask(task);
+                assertThat(service.getTask(TASK_ID).getTitle()).isEqualTo(TASK_TITLE);
+            }
+        }
     }
 
-    @Test
-    void updateTask() {
+    @Nested
+    @DisplayName("updateTask 메소드는")
+    class Describe_updateTask {
+        Task source;
+        @BeforeEach
+        void setup() {
+            Task savedtask = new Task();
+            savedtask.setTitle(TASK_TITLE);
+            savedtask.setId(1L);
+            service.createTask(savedtask);
+            source = new Task();
+            source.setTitle(UPDATED_TITLE);
+        }
+
+        @Nested
+        @DisplayName("만약 요청한 Id에 해당하는 task를 찾지 못하면")
+        class No_task {
+
+            @Test
+            @DisplayName("TasksNotFoundException을 발생시킨다.")
+            void it_throws_TaskNotFoundException() {
+                assertThatThrownBy(() -> service.updateTask(INVALID_ID,source))
+                        .isInstanceOf(TaskNotFoundException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("만약 요청한 Id에 해당하는 task를 찾으면")
+        class has_task {
+            @Test
+            @DisplayName("task를 업데이트 한다.")
+            void update_task() {
+                assertThat(service.updateTask(TASK_ID,source).getId()).isEqualTo(TASK_ID);
+                assertThat(service.updateTask(TASK_ID,source).getTitle()).isEqualTo(UPDATED_TITLE);
+            }
+        }
+
     }
 
     @Test
