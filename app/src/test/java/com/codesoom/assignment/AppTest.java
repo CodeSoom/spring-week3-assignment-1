0a01511737d85826e9d5d681943935de2cf7cc7a
akaster99 <idavid29@naver.com>
2022-06-21 04:34:34 +0900

    Define AppTest

---
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -0,0 +1,14 @@
+package com.codesoom.assignment;
+
+import com.codesoom.assignment.App;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    @Test
+    void appHasAGreeting() {
+        App classUnderTest = new App();
+        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+        assertEquals(classUnderTest.getGreeting(), "Hello World!");
+    }
+}
