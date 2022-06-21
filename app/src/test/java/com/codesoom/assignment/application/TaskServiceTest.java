cae8696bc0f478def24627eb75fd7d369ef17468
unajun <bjyoon513@gmail.com>
2022-06-22 00:54:31 +0900

    test: implement unit tests for service layer

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -1,28 +1,105 @@
 package com.codesoom.assignment.application;
 
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskServiceTest {
+    private TaskService taskService;
 
-    @Test
-    void getTasks() {
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService();
     }
 
     @Test
-    void getTask() {
+    void getTasks_NoTask_EmptyList() {
+        assertThat(taskService.getTasks()).isEmpty();
     }
 
     @Test
-    void createTask() {
+    void getTasks_Task_TaskInList() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task generatedTask = taskService.createTask(task);
+
+        assertThat(taskService.getTasks()).anySatisfy(t -> {
+            assertThat(t.getTitle()).isEqualTo("test");
+            assertThat(t.getId()).isEqualTo(generatedTask.getId());
+        });
     }
 
     @Test
-    void updateTask() {
+    void getTask_NonExistingId_ThrowTaskNotFoundException() {
+        assertThatThrownBy(() -> taskService.getTask(1L)).hasMessageStartingWith("Task not found:");
     }
 
     @Test
-    void deleteTask() {
+    void getTask_ExistingId_ReturnTask() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task generatedTask = taskService.createTask(task);
+        Long generatedId = generatedTask.getId();
+
+        assertThat(taskService.getTask(generatedId).getId()).isEqualTo(generatedId);
+        assertThat(taskService.getTask(generatedId).getTitle()).isEqualTo(generatedTask.getTitle());
+    }
+
+    @Test
+    void createTask_Will_ReturnCreatedTask() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task generatedTask = taskService.createTask(task);
+
+
+        assertThat(generatedTask.getId()).isEqualTo(taskService.getTask(generatedTask.getId()).getId());
+        assertThat(generatedTask.getTitle()).isEqualTo("test");
+    }
+
+    @Test
+    void createTask_Will_AddTaskInList() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task generatedTask = taskService.createTask(task);
+        Long generatedId = generatedTask.getId();
+
+        assertThat(taskService.getTask(generatedId)).isEqualTo(generatedTask);
+    }
+
+    @Test
+    void updateTask_ExisingId_ChangeTaskTitle() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task firstTask = taskService.createTask(task);
+
+        Task newTask = new Task();
+        newTask.setTitle("new");
+        Task updatedTask = taskService.updateTask(firstTask.getId(), newTask);
+
+        assertThat(updatedTask.getTitle()).isEqualTo("new");
+        assertThat(updatedTask.getId()).isEqualTo(firstTask.getId());
+    }
+
+    @Test
+    void deleteTask_ExisingId_ReturnTargetTask() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task firstTask = taskService.createTask(task);
+
+        assertThat(taskService.deleteTask(firstTask.getId())).isEqualTo(firstTask);
+    }
+
+    @Test
+    void deleteTask_ExisingId_DeleteTaskFromList() {
+        Task task = new Task();
+        task.setTitle("test");
+        Task firstTask = taskService.createTask(task);
+
+        taskService.deleteTask(firstTask.getId());
+        assertThat(taskService.getTasks()).doesNotContain(firstTask);
     }
 }
