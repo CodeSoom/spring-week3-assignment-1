de0ac0fdd0c23ef1829d25a5781bfd1894337749
Jinu Noh <yes.jinu@gmail.com>
2022-06-21 21:07:14 +0900

    test: add MvcMock test to TaskController

--- app/src/test/java/com/codesoom/assignment/ControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/ControllerTest.java
@@ -1,21 +1,48 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.application.TaskService;
-import com.codesoom.assignment.controllers.TaskController;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("TaskController 클래스")
 public class ControllerTest { // FIXME: 이름을 TaskControllerTest로 지으면 java 파일을 인식하지 못한다?!
+    @Autowired
+    private MockMvc mockMvc;
 
-    private TaskController taskController;
+    @MockBean
+    private TaskService taskService;
 
-    @BeforeEach
-    private void setUp() {
-        TaskService taskService = new TaskService();
-        this.taskController = new TaskController(taskService);
+    @Nested
+    @DisplayName("GET /tasks API는")
+    class Describe_GET_tasks {
+
+        @Nested
+        @DisplayName("등록된 task가 없다면")
+        class Context_no_tasks {
+
+            @Test
+            @DisplayName("빈 리스트를 리턴한다")
+            void it_returns_empty_array() throws Exception {
+                given(taskService.getTasks())
+                        .willThrow(new TaskNotFoundException(Long.MAX_VALUE))
+                        .willReturn(new ArrayList<>());
+
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isNotFound());
+            }
+        }
     }
-
-    // FIXME:
-    //  TaskController에서는 TaskService를 호출하기만 한다.
-    //  TaskService의 로직은 TaskServiceTest에서 테스트했다.
-    //  TaskController에서는 무엇을 테스트하면 좋을까?
 }
