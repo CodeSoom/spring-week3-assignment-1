789481f5b632f2db77c586e7f580b665115d9e1b
unajun <bjyoon513@gmail.com>
2022-06-21 23:55:22 +0900

    test: add unit test for Task class

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -11,21 +11,30 @@ class TaskTest {
 
     @BeforeEach
     void setUp() {
+        task = new Task();
     }
 
     @Test
     void getId() {
+        assertThat(task.getId()).isNull();
     }
 
     @Test
     void setId() {
+        Long expected = 3L;
+        task.setId(expected);
+        assertThat(task.getId()).isEqualTo(expected);
     }
 
     @Test
     void getTitle() {
+        assertThat(task.getTitle()).isNull();
     }
 
     @Test
     void setTitle() {
+        String expected = "test title";
+        task.setTitle(expected);
+        assertThat(task.getTitle()).isEqualTo(expected);
     }
 }
