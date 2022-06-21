68551f61e978240a53810ec30218ab5d14ce1b02
unajun <bjyoon513@gmail.com>
2022-06-22 00:55:06 +0900

    test: add test for ErrorResponse

--- app/src/test/java/com/codesoom/assignment/dto/ErrorResponseTest.java
+++ app/src/test/java/com/codesoom/assignment/dto/ErrorResponseTest.java
@@ -2,11 +2,17 @@ package com.codesoom.assignment.dto;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ErrorResponseTest {
 
     @Test
-    void getMessage() {
+    void testGetMessage() {
+        String expected = "test message";
+
+        ErrorResponse errorResponse = new ErrorResponse(expected);
+
+        assertThat(errorResponse.getMessage()).isEqualTo(expected);
     }
 }
