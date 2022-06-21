78886429d1090699603615400a5ceb15ce60aedb
unajun <bjyoon513@gmail.com>
2022-06-21 23:54:40 +0900

    test: create skeleton for unit testing

---
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -0,0 +1,19 @@
+package com.codesoom.assignment;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+    private App app;
+
+    @BeforeEach
+    void setUp(){
+    }
+
+    @Test
+    void getGreeting() {
+    }
+}
---
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -0,0 +1,31 @@
+package com.codesoom.assignment.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskTest {
+    private Task task;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void setTitle() {
+    }
+}
