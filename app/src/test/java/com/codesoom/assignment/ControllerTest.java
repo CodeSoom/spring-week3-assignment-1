5214bd7bc10c85df395cee399d5fb413662b479b
Jinu Noh <yes.jinu@gmail.com>
2022-06-22 21:12:46 +0900

    refactor: extract redundant mockMvc.perform into method `performGet`, `performPost`, `performDelete`

--- app/src/test/java/com/codesoom/assignment/ControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/ControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.List;
 
@@ -35,6 +36,20 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
     private final Task dummyTask2 = dummyTask(2L, "2");
     private final Task dummyTask3 = dummyTask(3L, "3");
 
+    private ResultActions performPost(String url, Object object) throws Exception {
+        return mockMvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(object)));
+    }
+
+    private ResultActions performGet(String url) throws Exception {
+        return mockMvc.perform(get(url));
+    }
+
+    private ResultActions performDelete(String url) throws Exception {
+        return mockMvc.perform(delete(url));
+    }
+
     @BeforeEach
     void setUp() throws Exception {
         /**
@@ -44,11 +59,11 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
          * GET /tasks를 테스트하기 위한 사전 동작으로 GET /tasks를 이용해도 괜찮을까..?
          */
 
-        MvcResult result = mockMvc.perform(get("/tasks")).andReturn();
+        MvcResult result = performGet("/tasks").andReturn();
         String content = result.getResponse().getContentAsString();
         List<Task> tasks = mapper.readValue(content, mapper.getTypeFactory().constructCollectionType(List.class, Task.class));
         for (Task task : tasks) {
-            mockMvc.perform(delete("/tasks/" + task.getId()));
+            performDelete("/tasks/" + task.getId());
         }
     }
 
@@ -63,7 +78,7 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @Test
             @DisplayName("200 OK와 빈 리스트를 리턴한다")
             void it_returns_empty_array() throws Exception {
-                mockMvc.perform(get("/tasks"))
+                performGet("/tasks")
                         .andExpect(status().isOk())
                         .andExpect(content().string(containsString("[]")));
             }
@@ -75,15 +90,15 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
 
             @BeforeEach
             void setUp() throws Exception {
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask1)));
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask2)));
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask3)));
+                performPost("/tasks", dummyTask1);
+                performPost("/tasks", dummyTask2);
+                performPost("/tasks", dummyTask3);
             }
 
             @Test
             @DisplayName("200 OK와 task가 포함된 리스트를 반환한다")
             void it_returns_array() throws Exception {
-                mockMvc.perform(get("/tasks"))
+                performGet("/tasks")
                         .andExpect(status().isOk())
                         .andExpect(content().string(containsString(dummyTask1.getTitle())))
                         .andExpect(content().string(containsString(dummyTask2.getTitle())))
@@ -103,7 +118,7 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @Test
             @DisplayName("404 NOT FOUND 예외를 던진다")
             void it_throws_404_exception() throws Exception {
-                mockMvc.perform(get("/task/" + Long.MAX_VALUE))
+                performGet("/tasks/1")
                         .andExpect(status().isNotFound());
             }
         }
@@ -115,11 +130,11 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
 
             @BeforeEach
             void setUp() throws Exception {
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask1)));
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask2)));
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask3)));
+                performPost("/tasks", dummyTask1);
+                performPost("/tasks", dummyTask2);
+                performPost("/tasks", dummyTask3);
 
-                MvcResult result = mockMvc.perform(get("/tasks")).andReturn();
+                MvcResult result = performGet("/tasks").andReturn();
                 String content = result.getResponse().getContentAsString();
                 tasks = mapper.readValue(content, mapper.getTypeFactory().constructCollectionType(List.class, Task.class));
             }
@@ -128,7 +143,7 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @DisplayName("200 OK와 task를 반환한다")
             void it_returns_task() throws Exception {
                 for (Task task: tasks) {
-                    mockMvc.perform(get("/tasks/" + task.getId()))
+                    performGet("/tasks/" + task.getId())
                             .andExpect(status().isOk())
                             .andExpect(content().string(containsString(task.getTitle())));
                 }
@@ -142,7 +157,7 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @Test
             @DisplayName("400 BAD REQUEST 예외를 던진다")
             void it_returns_400_exception() throws Exception {
-                mockMvc.perform(get("/tasks/invalid"))
+                performGet("/tasks/invalid")
                         .andExpect(status().isBadRequest());
             }
         }
@@ -159,7 +174,7 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @Test
             @DisplayName("201 CREATED와 생성된 Task를 반환한다")
             void it_returns_created_task() throws Exception {
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(mapper.writeValueAsString(dummyTask1)))
+                performPost("/tasks", dummyTask1)
                         .andExpect(status().isCreated())
                         .andExpect(content().string(containsString(dummyTask1.getTitle())));
             }
@@ -184,10 +199,10 @@ public class ControllerTest extends TestHelper { // FIXME: 이름을 TaskControl
             @Test
             @DisplayName("400 BAD REQUEST 예외를 던진다")
             void it_returns_400_exception() throws Exception {
-                mockMvc.perform(post("/tasks").contentType(MediaType.APPLICATION_JSON).content(""))
+                performPost("/tasks", "")
                         .andExpect(status().isBadRequest());
             }
         }
-        
+
     }
 }
