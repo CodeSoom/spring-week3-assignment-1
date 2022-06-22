3823d2f7043e9b0176a33514aaf85ff32d064014
akaster99 <idavid29@naver.com>
2022-06-22 09:17:06 +0900

    delete getGreeting

--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,14 +1,15 @@
 package com.codesoom.assignment;
 
 import com.codesoom.assignment.App;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
+    App app = new App();
     @Test
+    @DisplayName("GettGreeting메소드")
     void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-        assertEquals(classUnderTest.getGreeting(), "Hello, world!");
+
     }
 }
