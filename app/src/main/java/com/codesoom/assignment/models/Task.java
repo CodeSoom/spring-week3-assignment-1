a6f81d305b282e044924f7c29e70253297f81c38
박범진 <qjawlsqjacks@bjp-Macmini.local>
2022-07-18 21:49:16 +0900

    Task 테스트를 위한 생성자와 Equals 정의

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,9 +1,21 @@
 package com.codesoom.assignment.models;
 
+import java.util.Objects;
+
 public class Task {
     private Long id;
-
     private String title;
+
+    public Task() {
+    }
+
+    public Task(String title) {
+        this.title = title;
+    }
+
+    public Task(Long id) {
+        this.id = id;
+    }
 
     public Long getId() {
         return id;
@@ -19,5 +31,22 @@ public class Task {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        return Objects.equals(id, task.id) && Objects.equals(title, task.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title);
     }
 }
