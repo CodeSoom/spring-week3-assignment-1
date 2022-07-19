3bdebd38f6510d0ad64e2594467b2c6f9e500596
박범진 <qjawlsqjacks@fasoo.com>
2022-07-19 19:38:04 +0900

    TaskTest DCI 패턴 BDD 적용

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,49 +1,79 @@
 package com.codesoom.assignment.models;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@DisplayName("Task 클래스")
 class TaskTest {
-    private Task task;
-    private Task titleNullTask;
-    private Task idNullTask;
-    @BeforeEach
-    void setUp() {
-        task = new Task(1L, "BJP");
-        titleNullTask =  new Task(1L, null);
-        idNullTask = new Task(null, "BJP");
+    final Long givenId = 1L;
+    final String givenTitle = "BJP";
+
+    @Nested
+    @DisplayName("setId 메소드는")
+    class Describe_set_id {
+        @Nested
+        @DisplayName("식별자가 주어진다면")
+        class Context_with_id {
+            @Test
+            @DisplayName("작업의 식별자를 주어진 식별자로 할당한다.")
+            void it_assignment_id_to_task() {
+                Task task = new Task();
+                task.setId(givenId);
+
+                assertThat(task.getId()).isEqualTo(givenId);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("작업에 식별자를 주고 작업이 주어진 식별자를 가지고 있는지 확인한다.")
-    void giveTaskIdAndCheckHasGivenId() {
-        Task task = new Task();
-        task.setId(1L);
-
-        assertThat(task).isEqualTo(titleNullTask);
+    @Nested
+    @DisplayName("getId 메소드는")
+    class Describe_get_id {
+        Task task = new Task(givenId, givenTitle);
+        @Nested
+        @DisplayName("작업이 식별자를 가지고 있을 때")
+        class Context_when_Task_has_id {
+            @Test
+            @DisplayName("작업의 식별자를 리턴한다")
+            void it_returns_id() {
+                assertThat(task.getId()).isEqualTo(givenId);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("작업에 제목을 주었을 때, 해당 제목을 가지고 있어야 한다.")
-    void mustHaveTitleWhenGivenTaskWithTitle() {
-        Task task = new Task();
-        task.setTitle("BJP");
+    @Nested
+    @DisplayName("setTitle 메소드는")
+    class Describe_set_title {
+        @Nested
+        @DisplayName("제목이 주어질 때")
+        class Context_with_title {
+            @Test
+            @DisplayName("작업에 제목을 할당한다")
+            void it_assignment_title_to_task() {
+                Task task = new Task();
+                task.setTitle(givenTitle);
 
-        assertThat(task).isEqualTo(idNullTask);
+                assertThat(task.getTitle()).isEqualTo(givenTitle);
+            }
+        }
     }
 
-    @Test
-    @DisplayName("식별자를 가진 작업이 주어지고 식별자를 가져올 때, 주어진 식별자를 가지고 있는지 확인한다.")
-    void checkHasIdWhenGivenTaskWithId() {
-        assertThat(task.getId()).isEqualTo(1L);
-    }
+    @Nested
+    @DisplayName("getTitle 메소드는")
+    class Describe_get_title {
+        @Nested
+        @DisplayName("작업에 제목이 주어졌을 때")
+        class Context_with_title {
+            @Test
+            @DisplayName("작업의 제목을 리턴한다.")
+            void it_returns_title() {
+                Task task = new Task(givenId, givenTitle);
 
-    @Test
-    @DisplayName("제목을 가진 작업이 주어지고 제목을 가져올 때, 주어진 제목을 가지고 있는지 확인한다.")
-    void checkHasTitleWhenGivenTaskWithTitle() {
-        assertThat(task.getTitle()).isEqualTo("BJP");
+                assertThat(task.getTitle()).isEqualTo(givenTitle);
+            }
+        }
     }
 }
