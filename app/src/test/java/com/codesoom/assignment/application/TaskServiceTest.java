7349a1eac1f4632060c00e7c91ac8ea6ee41b330
박범진 <qjawlsqjacks@fasoo.com>
2022-07-19 21:00:34 +0900

    getTasks 테스트 코드 작성

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -2,6 +2,7 @@ package com.codesoom.assignment.application;
 
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,13 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("TaskService 클래스의")
 class TaskServiceTest {
+    TaskService taskService;
+
+    @BeforeEach
+    void setUp() {
+        taskService = new TaskService();
+    }
+
     @Nested
     @DisplayName("getTask 메소드는")
     class Describe_get_task {
@@ -20,7 +28,6 @@ class TaskServiceTest {
             @Test
             @DisplayName("작업을 리턴한다")
             void it_returns_task_with_id() {
-                TaskService taskService = new TaskService();
                 Task task = new Task(1L, "BJP");
                 taskService.createTask(task);
 
@@ -34,10 +41,35 @@ class TaskServiceTest {
             @Test
             @DisplayName("예외를 던진다")
             void it_throws_exception() {
-                TaskService taskService = new TaskService();
-
                 assertThatThrownBy(() -> taskService.getTask(1L))
                         .isExactlyInstanceOf(TaskNotFoundException.class);
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("getTasks 메소드는")
+    class Describe_get_tasks {
+        @Nested
+        @DisplayName("작업을 가진 목록이 주어지면")
+        class Context_with_tasks {
+            @Test
+            @DisplayName("작업 목록을 리턴한다")
+            void It_returns_tasks() {
+                taskService.createTask(new Task(null, "BJP"));
+                taskService.createTask(new Task(null, "BJP"));
+
+                assertThat(taskService.getTasks().size()).isEqualTo(2);
+            }
+        }
+
+        @Nested
+        @DisplayName("빈 작업 목록이 주어지면")
+        class Context_with_empty_tasks {
+            @Test
+            @DisplayName("빈 배열을 리턴한다")
+            void It_returns_empty_array() {
+                assertThat(taskService.getTasks()).isEmpty();
             }
         }
     }
