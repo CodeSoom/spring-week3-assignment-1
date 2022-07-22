da515be8f23e0f48f1412b02ae982882b12ca785
eunice0035 <eunice7357@gmail.com>
2022-07-22 18:15:31 +0900

    TaskServiceTest valid, invalid ID case 추가

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -1,4 +1,54 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.codesoom.assignment.application;
+
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 class TaskServiceTest {
-  
+
+    // TODO
+    // 1. list -> getTasks
+    // 2. detail -> getTasks (with ID)
+    // 3. create -> createTasks (with source)
+    // 4. update -> update Task (with ID, source)
+    // 5. delete -> delete Task (with ID)
+    private TaskService taskService;
+    final String TASK_TITLE = "test";
+
+    @BeforeEach
+    void setUp(){
+        taskService = new TaskService();
+        Task task = new Task();
+        task.setTitle(TASK_TITLE);
+        taskService.createTask(task);
+
+    }
+
+    @Test
+    void getTasks(){
+        List<Task> tasks = taskService.getTasks();
+        assertThat(tasks).hasSize(1);
+
+        Task task = tasks.get(0);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+    }
+
+    @Test
+    void getTaskWithValidId(){
+        Task task = taskService.getTask(1L);
+        assertThat(task.getTitle()).isEqualTo(TASK_TITLE);
+
+    }
+    @Test
+    void getTaskWithInvalidId(){
+        assertThatThrownBy(() -> taskService.getTask(100L))
+                .isInstanceOf(TaskNotFoundException.class);
+    }
+
 }
