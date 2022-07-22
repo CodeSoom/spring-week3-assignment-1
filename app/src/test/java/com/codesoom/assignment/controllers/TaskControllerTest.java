f2a8250b7c99e8475b4b6791ab9481055293701b
eunice0035 <eunice7357@gmail.com>
2022-07-22 18:14:27 +0900

    task, controller 기능 부분 문단 구분

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -2,25 +2,33 @@ package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class TaskControllerTest {
+    private TaskController controller;
+
+    /**
+     * 각각의 Test가 실행되기전에 실행되어 Controller를 생성
+     *
+     */
+    @BeforeEach
+    void setUp(){
+        controller = new TaskController(new TaskService());
+    }
     @Test
     void list(){
-        TaskController controller = new TaskController(new TaskService());
-
         assertThat(controller.list()).isEmpty();
     }
 
     @Test
     void createNewTask(){
-        TaskController controller = new TaskController(new TaskService());
 
         Task task = new Task();
-
         task.setTitle("Test1");
+
         controller.create(task);
 
         assertThat(controller.list()).hasSize(1);
@@ -28,6 +36,7 @@ class TaskControllerTest {
         assertThat(controller.list().get(0).getTitle()).isEqualTo("Test1");
 
         task.setTitle("Test2");
+
         controller.create(task);
 
         assertThat(controller.list()).hasSize(2);
