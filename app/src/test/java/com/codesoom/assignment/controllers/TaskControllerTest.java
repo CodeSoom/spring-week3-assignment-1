f2ec1c5c5c65834afa6dcbd3ed91d0ec7102b593
Eunha Jeong <eunhaay@gmail.com>
2022-09-27 17:35:16 +0900

    test: TaskController 유닛 테스트 추가

---
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerTest.java
@@ -0,0 +1,72 @@
+package com.codesoom.assignment.controllers;
+
+import com.codesoom.assignment.TaskNotFoundException;
+import com.codesoom.assignment.application.TaskService;
+import com.codesoom.assignment.dto.TaskRequestDto;
+import com.codesoom.assignment.models.Task;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@DisplayName("할 일 Controller 유닛 테스트")
+class TaskControllerTest {
+
+    private static final String TITLE = "test";
+    private static final String UPDATE_TITLE = "update test";
+    private static final int TASKS_SIZE = 5;
+
+    private TaskController taskController;
+
+    @BeforeEach
+    void setUp() {
+        TaskService taskService = new TaskService();
+        taskController = new TaskController(taskService);
+
+        for (int id = 1; id <= TASKS_SIZE; id++) {
+            taskController.create(new TaskRequestDto(TITLE + id));
+        }
+    }
+
+    @Test
+    @DisplayName("모든 할 일을 조회할 수 있다")
+    void createTask() {
+        assertThat(taskController.list()).hasSize(TASKS_SIZE);
+    }
+
+    @Test
+    @DisplayName("id에 해당하는 할 일을 상세 조회할 수 있다")
+    void detailTask() {
+        Task task1 = taskController.detail(3L);
+        Task task2 = taskController.detail(4L);
+
+        assertThat(task1.getId()).isEqualTo(3L);
+        assertThat(task1.getTitle()).isEqualTo(TITLE + 3L);
+        assertThat(task2.getId()).isEqualTo(4L);
+        assertThat(task2.getTitle()).isEqualTo(TITLE + 4L);
+    }
+
+    @Test
+    @DisplayName("id에 해당하는 할 일이 없으면 예외가 발생한다")
+    void detailTaskWithInvalidId() {
+        assertThatThrownBy(() -> taskController.detail(1000L))
+                .isInstanceOf(TaskNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("id에 해당하는 할 일을 찾아 수정할 수 있다")
+    void updateTask() {
+        Task updatedTask = taskController.update(1L, new TaskRequestDto(UPDATE_TITLE));
+
+        assertThat(updatedTask.getTitle()).isEqualTo(UPDATE_TITLE);
+    }
+
+    @Test
+    @DisplayName("id에 해당하는 할 일을 삭제할 수 있다")
+    void deleteTask() {
+        taskController.delete(3L);
+
+        assertThat(taskController.list()).hasSize(TASKS_SIZE - 1);
+    }
+}
