108619aa43ec556f25f4b59483a4285846dda583
hautblis <hautblis@gmail.com>
2022-09-28 21:09:41 +0900

    feat : TaskControllerWebTest 테스트 작성

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,9 +1,14 @@
 package com.codesoom.assignment.models;
 
+import java.util.Objects;
+
 public class Task {
     private Long id;
 
     private String title;
+
+    public Task() {
+    }
 
     public Task(Long id, String title) {
         this.id = id;
@@ -24,5 +29,18 @@ public class Task {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(getId(), task.getId()) && Objects.equals(getTitle(), task.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getTitle());
     }
 }
