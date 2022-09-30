e571f0f8b1ed9e4522f820558227c5e2447442d4
parkcw <luckycw@parkcw.com>
2022-09-30 18:20:44 +0900

    Refactor: Refine DisplayName annotation comment of method in TaskServiceTest

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -32,10 +32,10 @@ class TaskServiceTest {
         }
 
         @Nested
-        @DisplayName("DB에 데이터가 존재하지 않는다면")
+        @DisplayName("데이터가 존재하지 않는다면")
         class Context_with_empty_db {
             @Test
-            @DisplayName("빈 ArrayList를 리턴한다")
+            @DisplayName("빈 데이터를 리턴한다")
             void it_returns_empty_arraylist() {
                 List<Task> findTasks = subject();
 
@@ -45,31 +45,29 @@ class TaskServiceTest {
         }
 
         @Nested
-        @DisplayName("DB에 데이터가 존재한다면")
+        @DisplayName("데이터가 존재한다면")
         class Context_with_non_empty_db extends NewTask {
-            private Task task1, task2;
-            private int expectedSize;
+            private final List<Task> givenTasks = new ArrayList<>();
 
             @BeforeEach
             void prepareTests() {
-                task1 = withTitle("할 일 추가1");
-                task2 = withTitle("할 일 추가2");
+                givenTasks.add(withTitle("할 일 추가1"));
+                givenTasks.add(withTitle("할 일 추가2"));
 
-                taskService.createTask(task1);
-                taskService.createTask(task2);
+                taskService.createTask(givenTasks.get(0));
+                taskService.createTask(givenTasks.get(1));
 
-                expectedSize = taskService.getTasks().size();
             }
 
             @Test
-            @DisplayName("존재하는 모든 할 일을 리턴한다")
+            @DisplayName("모든 할 일을 리턴한다")
             void it_returns_all_tasks() {
                 List<Task> findTasks = subject();
                 final int actualSize = findTasks.size();
 
-                assertThat(actualSize).isEqualTo(expectedSize);
-                assertThat(findTasks.get(0).getTitle()).isEqualTo(task1.getTitle());
-                assertThat(findTasks.get(1).getTitle()).isEqualTo(task2.getTitle());
+                assertThat(actualSize).isEqualTo(givenTasks.size());
+                assertThat(findTasks.get(0).getTitle()).isEqualTo(givenTasks.get(0).getTitle());
+                assertThat(findTasks.get(1).getTitle()).isEqualTo(givenTasks.get(1).getTitle());
             }
         }
     }
@@ -83,16 +81,12 @@ class TaskServiceTest {
             private final Long TASK_ID = 1L;
             private final String TITLE = "할 일 추가";
             private final Task givenTask = withTitle(TITLE);
-            private int expectedSize;
-
-            @BeforeEach
-            void prepareTest() {
-                expectedSize = taskService.getTasks().size() + 1;
-            }
 
             @Test
             @DisplayName("DB에 등록하고 등록된 할 일을 리턴한다")
             void it_returns_registered_task() {
+                final int expectedSize = taskService.getTasks().size() + 1;
+
                 Task actualTask = taskService.createTask(givenTask);
 
                 final int actualSize = taskService.getTasks().size();
@@ -109,7 +103,7 @@ class TaskServiceTest {
     @DisplayName("getTask 메소드는")
     class Describe_getTask {
         @Nested
-        @DisplayName("검색한 할 일 ID가 존재하면")
+        @DisplayName("ID가 주어지면")
         class Context_with_existing_task_id extends NewTask {
             private final Long TASK_ID = 1L;
             private final String TITLE = "할 일 검색";
@@ -122,7 +116,7 @@ class TaskServiceTest {
             }
 
             @Test
-            @DisplayName("해당 ID의 할 일을 리턴한다")
+            @DisplayName("할 일을 찾아 리턴한다")
             void it_returns_found_task() {
                 Task actualTask = taskService.getTask(TASK_ID);
 
@@ -132,10 +126,10 @@ class TaskServiceTest {
         }
 
         @Nested
-        @DisplayName("검색한 할 일 ID가 존재하지 않는다면")
+        @DisplayName("유효하지 않은 ID가 주어지면")
         class Context_with_non_existing_task_id {
             @Test
-            @DisplayName("TaskNotFoundException 예외를 던진다")
+            @DisplayName("예외를 던진다")
             void it_throws_task_not_found_exception() {
                 assertThatThrownBy(() -> taskService.getTask(100L)).isInstanceOf(TaskNotFoundException.class);
             }
@@ -147,7 +141,7 @@ class TaskServiceTest {
     @DisplayName("updateTask 메소드는")
     class Describe_updateTask {
         @Nested
-        @DisplayName("수정요청한 할 일 ID가 존재한다면")
+        @DisplayName("ID가 주어지면")
         class Context_with_existing_task_id extends NewTask {
             private  final Long TASK_ID = 1L;
             private final String TITLE = "할 일 수정";
@@ -162,7 +156,7 @@ class TaskServiceTest {
             }
 
             @Test
-            @DisplayName("해당 할 일을 수정하고 수정된 할 일을 리턴한다")
+            @DisplayName("할 일을 찾아 수정하고 리턴한다")
             void it_returns_modified_task() {
                 Task updatedTask = taskService.updateTask(TASK_ID, givenTask);
 
@@ -171,12 +165,12 @@ class TaskServiceTest {
         }
 
         @Nested
-        @DisplayName("수정요청한 할 일 ID가 존재하지 않는다면")
+        @DisplayName("유효하지 않은 ID가 주어지면")
         class Context_with_non_existing_task_id extends NewTask {
             private final Task givenTask = withTitle("할 일 수정예외");
 
             @Test
-            @DisplayName("TaskNotFoundException 예외를 던진다")
+            @DisplayName("예외를 던진다")
             void it_throws_task_not_found_exception() {
                 assertThatThrownBy(() -> taskService.updateTask(100L, givenTask)).isInstanceOf(TaskNotFoundException.class);
             }
@@ -189,7 +183,7 @@ class TaskServiceTest {
         final Long TASK_ID = 1L;
 
         @Nested
-        @DisplayName("삭제요청한 할 일 ID가 존재한다면")
+        @DisplayName("ID가 주어지면")
         class Context_with_existing_task_id extends NewTask {
             private int beforeTotalCount;
             private Task expectedTask;
@@ -202,7 +196,7 @@ class TaskServiceTest {
             }
 
             @Test
-            @DisplayName("해당 할 일을 삭제하고 삭제된 할 일을 리턴한다")
+            @DisplayName("할 일을 찾아 삭제하고 리턴한다")
             void it_returns_modified_task() {
                 final Task deletedTask = taskService.deleteTask(TASK_ID);
                 final int afterTotalCount = taskService.getTasks().size();
@@ -214,10 +208,10 @@ class TaskServiceTest {
         }
 
         @Nested
-        @DisplayName("삭제요청한 할 일 ID가 존재하지 않는다면")
+        @DisplayName("유효하지 않은 ID가 주어지면")
         class Context_with_non_existing_task_id {
             @Test
-            @DisplayName("TaskNotFoundException 예외를 던진다")
+            @DisplayName("예외를 던진다")
             void it_throws_task_not_found_exception() {
                 assertThatThrownBy(() -> taskService.deleteTask(100L)).isInstanceOf(TaskNotFoundException.class);
             }
