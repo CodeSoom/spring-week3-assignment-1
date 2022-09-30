60cf976b309b3512911b649090f5483bc4129cfb
parkcw <luckycw@parkcw.com>
2022-09-30 18:19:15 +0900

    Fix: Add server health check method in AppTest

---
+++ app/src/main/java/com/codesoom/assignment/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.codesoom.assignment.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Server is OK";
+    }
+}
--- app/src/test/java/com/codesoom/assignment/AppTest.java
+++ app/src/test/java/com/codesoom/assignment/AppTest.java
@@ -1,26 +1,33 @@
 package com.codesoom.assignment;
 
+import com.codesoom.assignment.controllers.HomeController;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(HomeController.class)
 @DisplayName("APP 클래스")
 class AppTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
     @Nested
-    @DisplayName("getGreeting 메소드는")
-    class Describe_getGreeting {
-        private final String expected = "Hello, world!";
-
+    @DisplayName("home(HomeController) 메소드는")
+    class Describe_home {
         @Test
-        @DisplayName("해당 메소드를 호출하면 주어진 문자열을 리턴한다")
-        void it_returns_string() {
-            // when
-            App classUnderTest = new App();
-            String actual = classUnderTest.getGreeting();
-
-            assertThat(actual).isEqualTo(expected);
+        @DisplayName("호출하면 서버 상태를 리턴한다")
+        void it_returns_server_status() throws Exception {
+            mockMvc.perform(get("/"))
+                    .andExpect(status().isOk())
+                    .andExpect(content().string("Server is OK"));
         }
 
 
