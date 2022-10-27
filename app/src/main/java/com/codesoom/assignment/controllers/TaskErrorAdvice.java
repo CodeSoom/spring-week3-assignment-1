e17a0d67e0c59b5c1fda70562efcff83c131eb36
steve7867 <parkjunsu3@gmail.com>
2022-10-28 00:03:59 +0900

    TaskErrorAdvice: @RestControllerAdvice로 변경, handleIllegalArgument 메소드 추가
    handleIllegalArgument: NegativeIdException, NullTaskException, InvalidTaskTitleException에 대한 예외 처리

--- app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
@@ -1,19 +1,28 @@
 package com.codesoom.assignment.controllers;
 
+import com.codesoom.assignment.exceptions.InvalidTaskTitleException;
+import com.codesoom.assignment.exceptions.NegativeIdException;
+import com.codesoom.assignment.exceptions.NullTaskException;
 import com.codesoom.assignment.exceptions.TaskNotFoundException;
 import com.codesoom.assignment.dto.ErrorResponse;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class TaskErrorAdvice {
-    @ResponseBody
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(TaskNotFoundException.class)
-    public ErrorResponse handleNotFound() {
-        return new ErrorResponse("Task not found");
+    public ErrorResponse handleNotFound(TaskNotFoundException e) {
+        return new ErrorResponse(e.getMessage());
     }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = {NegativeIdException.class, NullTaskException.class, InvalidTaskTitleException.class})
+    public ErrorResponse handleIllegalArgument(IllegalArgumentException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
 }
