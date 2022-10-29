45030395b2f75b735cf486722fa230e3b06c6f32
steve7867 <parkjunsu3@gmail.com>
2022-10-30 03:24:56 +0900

    TaskDto: 기본 생성자 제거, NullTaskDtoException 제거

--- app/src/main/java/com/codesoom/assignment/exceptions/NullTaskDtoException.java
+++
@@ -1,7 +0,0 @@
-package com.codesoom.assignment.exceptions;
-
-public class NullTaskDtoException extends IllegalArgumentException {
-    public NullTaskDtoException() {
-        super("null인 객체는 허용되지 않습니다.");
-    }
-}
