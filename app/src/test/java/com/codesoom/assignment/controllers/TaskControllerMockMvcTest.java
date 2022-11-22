e9ccce0f2a45c37315d86c7807f19697a4bc81e1
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-22 22:34:07 +0900

    MockBean을 주입받고, given 메서드와 verify 메서드를 활용하여 구체적인 테스트를 작성해라
    MockBean 애노테이션을 통해 TaskService에 대한 가짜 객체를 생성합니다.
    BeforeEach에서 given 메서드를 통해 가짜 객체의 동작에 대해 정의합니다.
    verfiy 메서드를 통해 가짜 객체의 메소드가 실행되었는지 체크합니다.
    
    Json을 사용해 Task 객체를 생성합니다.

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerMockMvcTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerMockMvcTest.java
@@ -3,38 +3,50 @@ package com.codesoom.assignment.controllers;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Slf4j
 class TaskControllerMockMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     private TaskService service;
 
     @BeforeEach
     void before() {
-        Task task = new Task();
-        task.setTitle("test");
-        service.createTask(task);
+        given(service.getTask(1L))
+                .willReturn(Task.builder()
+                                .id(1L)
+                                .title("test")
+                                .build());
+
+        given(service.getTask(100L))
+                .willThrow(TaskNotFoundException.class);
     }
 
     @Test
@@ -42,18 +54,19 @@ class TaskControllerMockMvcTest {
     void getTasks() throws Exception {
         mockMvc.perform(get("/tasks"))
                 .andExpect(status().isOk());
+
+        verify(service).getTasks();
     }
 
     @Test
     @DisplayName("해당 Id에 해당하는 Task 얻기")
     void getTask() throws Exception {
-        Task task = new Task();
-        task.setTitle("test");
-        Task resultTask = service.createTask(task);
-        Long id = resultTask.getId();
+        mockMvc.perform(get("/tasks/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title").exists())
+                .andDo(print());
 
-        mockMvc.perform(get("/tasks/{id}", id))
-                .andExpect(status().isOk());
+        verify(service).getTask(1L);
     }
 
     @Test
@@ -71,9 +84,13 @@ class TaskControllerMockMvcTest {
     @Test
     @DisplayName("Task 객체 생성하기")
     void createTask() throws Exception {
+        Task task = Task.builder().id(2L).title("test2").build();
+        String content = new ObjectMapper().writeValueAsString(task);
+        log.info("content={}", content);
+
         //perform() 안에 조건 집어넣기
         mockMvc.perform(post("/tasks")
-                        .content("{\"title\":\"test\"}")
+                        .content(content)
                         .contentType(APPLICATION_JSON)
                         .accept(APPLICATION_JSON))
                 .andExpect(status().isCreated())
@@ -83,11 +100,15 @@ class TaskControllerMockMvcTest {
     @Test
     @DisplayName("title test -> test2 수정하기")
     void updateTask() throws Exception {
+        //TODO  - content 어떻게 집어넣을지?
+        //      - update 후 수정된 title이 test2로 되어있는지 확인하는 방법 찾기
+
         mockMvc.perform(put("/tasks/1")
                 .content("{\"title\":\"test2\"}")
                 .contentType(APPLICATION_JSON)
                 .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
+//                .andExpect(jsonPath("$.title").value("test2"))
                 .andDo(print());
     }
 
@@ -96,5 +117,7 @@ class TaskControllerMockMvcTest {
     void deleteTask() throws Exception {
         mockMvc.perform(delete("/tasks/1"))
                 .andExpect(status().isNoContent());
+
+        verify(service).deleteTask(1L);
     }
 }
