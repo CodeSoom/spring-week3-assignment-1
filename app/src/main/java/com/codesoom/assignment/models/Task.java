f08893a2b85b3ba4590a3f29ddb6990e65bec2fb
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-22 22:09:40 +0900

    Lombok을 사용하여 Task 클래스를 수정해라
    get/set 코드를 삭제하고 Getter/Setter 애노테이션을 사용하도록 했습니다.
    Builder 애노테이션을 사용하기 위해 NoArgsConstructor와 AllArgsConstructor를 추가하였습니다.

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,23 +1,18 @@
 package com.codesoom.assignment.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Task {
     private Long id;
 
     private String title;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
