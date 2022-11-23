d9e0477ea22d7110e25f55aed32d3b1842024c16
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-23 20:43:04 +0900

    Lombok의 NoArgsConstructor, AllArgsConstructor를 사용하지 마라
    Lombok의 애노테이션을 사용해서 생성자를 생성할 때 파라미터 순서를 필드 선언 순서에 맞춰서 생성한다. 따라서 코드의 리팩터링없이 개발자가 인지하지 못한 채로 파라미터 순서를 바꿔줬을 때 어떤 오류도 발생하지 않아 추후 버그가 생길 수도 있다.
    대신 직접 생성자를 만들고 Builder 애노테이션을 생성자에 선언해주었다.

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,18 +1,22 @@
 package com.codesoom.assignment.models;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
 public class Task {
     private Long id;
 
     private String title;
+
+    public Task() {
+    }
+
+    @Builder
+    public Task(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 }
