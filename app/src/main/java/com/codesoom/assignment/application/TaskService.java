dd3e63733d1e3dc32acebaddd0ce06b8f7adba83
EuijeongHwang_Mac <euijeong.hwang97@gmail.com>
2022-11-25 17:45:14 +0900

    과제 2주차 때 생성한 클래스들로 변경 및 보완
    과제 2주차 때 작성했던 코드로 대체 및 보완함

--- app/src/main/java/com/codesoom/assignment/application/TaskService.java
+++ app/src/main/java/com/codesoom/assignment/application/TaskService.java
@@ -1,54 +1,19 @@
 package com.codesoom.assignment.application;
 
-import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
-import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class TaskService {
-    private List<Task> tasks = new ArrayList<>();
-    private Long newId = 0L;
+public interface TaskService {
 
-    public List<Task> getTasks() {
-        return tasks;
-    }
+    List<Task> getTasks();
 
-    public Task getTask(Long id) {
-        return tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new TaskNotFoundException(id));
-    }
+    Task getTask(Long id);
 
-    public Task createTask(Task source) {
-        Task task = new Task();
-        task.setId(generateId());
-        task.setTitle(source.getTitle());
+    Task createTask(Task task);
 
-        tasks.add(task);
+    Task updateTask(Long id, Task task);
 
-        return task;
-    }
+    void deleteTask(Long id);
 
-    public Task updateTask(Long id, Task source) {
-        Task task = getTask(id);
-        task.setTitle(source.getTitle());
-
-        return task;
-    }
-
-    public Task deleteTask(Long id) {
-        Task task = getTask(id);
-        tasks.remove(task);
-
-        return task;
-    }
-
-    private Long generateId() {
-        newId += 1;
-        return newId;
-    }
 }
--- app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
+++ app/src/main/java/com/codesoom/assignment/controllers/TaskErrorAdvice.java
@@ -1,19 +1,60 @@
 package com.codesoom.assignment.controllers;
 
 import com.codesoom.assignment.TaskNotFoundException;
-import com.codesoom.assignment.dto.ErrorResponse;
+import com.codesoom.assignment.TitleNotFoundException;
+import com.codesoom.assignment.dto.ErrorHandler;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-@ControllerAdvice
+@RestControllerAdvice("com.codesoom.assignment")
 public class TaskErrorAdvice {
-    @ResponseBody
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NullPointerException.class)
+    public ErrorHandler NPEHandler(NullPointerException ne) {
+        return new ErrorHandler("Not Found", ne.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(HttpClientErrorException.BadRequest.class)
+    public ErrorHandler BadRequestHandler(HttpClientErrorException.BadRequest badRequest) {
+        return new ErrorHandler("Bad Request", "다시 한 번 확인해주세요.");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ErrorHandler notReadableExHandler(HttpMessageNotReadableException he) {
+        return new ErrorHandler("Not Readable", "RequestBody가 존재하지 않습니다. 다시 한 번 확인해주세요.");
+    }
+
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ErrorHandler methodNotSupportedExHandler(HttpRequestMethodNotSupportedException he) {
+        return new ErrorHandler("Method Not Supported", "Id를 입력해주세요.");
+    }
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(TaskNotFoundException.class)
-    public ErrorResponse handleNotFound() {
-        return new ErrorResponse("Task not found");
+    public ErrorHandler taskNotFoundExHandler(TaskNotFoundException te) {
+        return new ErrorHandler("Task Not Found", "Task가 존재하지 않습니다.");
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(TitleNotFoundException.class)
+    public ErrorHandler titleNotFoundExHandler(TitleNotFoundException te) {
+        return new ErrorHandler("Title Not Found", "Title은 한 글자 이상 입력이 필수입니다.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ErrorHandler typeMismatchExHandler(MethodArgumentTypeMismatchException me) {
+        return new ErrorHandler("Type Mismatch", "Id는 숫자만 입력 가능합니다.");
     }
 }
