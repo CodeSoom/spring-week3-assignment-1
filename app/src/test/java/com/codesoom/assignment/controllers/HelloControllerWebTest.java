bc1260335bef28615210358022d1bc110af18019
Giwan Kim <giwankim@gmail.com>
2023-01-26 06:45:47 +0900

    feat: add HelloController and tests

---
+++ app/src/test/java/com/codesoom/assignment/controllers/HelloControllerWebTest.java
@@ -0,0 +1,27 @@
+package com.codesoom.assignment.controllers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloController.class)
+class HelloControllerWebTest {
+
+  @Autowired
+  MockMvc mockMvc;
+
+  @Test
+  void sayHello() throws Exception {
+    mockMvc.perform(get("/"))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString("Hello, World!")));
+  }
+}
