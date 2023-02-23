3a32169fc34e24d873470ecf0c0775ec1d1ed829
tlatmsrud <tlatmsrud@naver.com>
2023-02-24 01:33:31 +0900

    @Nested를 사용하여 테스트를 계층적으로 분리하라
    1. @Nested 어노테이션을 사용하여 계층적으로 분리합니다. 이로써
       mockBean에 대한 given절을 기능별로 분리하게 되었습니다.
    2. @DisplayName 어노테이션을 사용하여 테스트 내용을 명확히합니다.

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -3,7 +3,10 @@ package com.codesoom.assignment.application;
 import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.StatusAssertions;
@@ -24,85 +27,153 @@ class TaskServiceTest {
     private static final Long CREATE_ID = 2L;
 
     private static final Long INVALID_ID = 100L;
-    @BeforeEach
-    void setUp(){
-        Task task = new Task();
-        task.setTitle(TITLE);
-        taskService = new TaskService();
-        taskService.createTask(task);
+
+    @Nested
+    @DisplayName("Task 리스트 테스트")
+    class TasksTest{
+        @BeforeEach
+        void setUp(){
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService = new TaskService();
+            taskService.createTask(task);
+        }
+
+        @Test
+        @DisplayName("Task 리스트 조회")
+        void getTasks() {
+
+            assertThat(taskService.getTasks()).hasSize(1);
+        }
     }
 
-    @Test
-    void getTasks() {
-        assertThat(taskService.getTasks()).hasSize(1);
-    }
+    @Nested
+    @DisplayName("Task 상세 테스트")
+    class TaskDetailTest{
+        @BeforeEach
+        void setUp(){
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService = new TaskService();
+            taskService.createTask(task);
+        }
 
-    @Test
-    void getTaskWithValidId() {
-        assertThat(taskService.getTask(DEFAULT_ID).getId()).isEqualTo(DEFAULT_ID);
-        assertThat(taskService.getTask(DEFAULT_ID).getTitle()).isEqualTo(TITLE);
-    }
+        @Test
+        @DisplayName("유효 ID를 통한 상세 조회")
+        void getTaskWithValidId() {
+            assertThat(taskService.getTask(DEFAULT_ID).getId()).isEqualTo(DEFAULT_ID);
+            assertThat(taskService.getTask(DEFAULT_ID).getTitle()).isEqualTo(TITLE);
+        }
 
-    @Test
-    void getTaskWithInvalidId() {
-        // assertThatThrownBy 메서드를 통해 바로 IsInstanceOf 체인메서드를 호출하여 비교
-        assertThatThrownBy(() -> taskService.getTask(INVALID_ID)).isInstanceOf(TaskNotFoundException.class);
-    }
+        @Test
+        @DisplayName("유효하지 않은 ID를 통한 상세 조회")
+        void getTaskWithInvalidId() {
+            // assertThatThrownBy 메서드를 통해 바로 IsInstanceOf 체인메서드를 호출하여 비교
+            assertThatThrownBy(() -> taskService.getTask(INVALID_ID)).
+                    isInstanceOf(TaskNotFoundException.class);
+        }
 
-    @Test
-    void getTaskWithInvalidId2() {
-        // 예외 상황을 발생시킨 후 Throwable 객체 획득
-        Throwable thrown = catchThrowable(() -> {taskService.getTask(INVALID_ID);});
+        @Test
+        @DisplayName("유효하지 않은 ID를 통한 상세 조회의 다른방법")
+        void getTaskWithInvalidId2() {
+            // 예외 상황을 발생시킨 후 Throwable 객체 획득
+            Throwable thrown = catchThrowable(() -> {taskService.getTask(INVALID_ID);});
 
-        // inInstanceOf 메서드를 통해 Exception 비교
-        assertThat(thrown).isInstanceOf(TaskNotFoundException.class)
-                .hasMessage("Task not found: " + INVALID_ID);
-    }
-
-    @Test
-    void createTask() {
-        Task task = new Task();
-        task.setTitle(TITLE);
-        taskService.createTask(task);
-
-        assertThat(taskService.getTasks()).hasSize(2);
-        assertThat(taskService.getTask(CREATE_ID).getTitle()).isEqualTo(TITLE);
-    }
-
-    @Test
-    void updateTaskWithValidId() {
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-        taskService.updateTask(DEFAULT_ID,updateTask);
-
-        assertThat(taskService.getTasks()).hasSize(1);
-        assertThat(taskService.getTask(DEFAULT_ID).getTitle()).isEqualTo(UPDATE_TITLE);
-    }
-
-    @Test
-    void updateTaskWithInvalidId() {
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-        assertThatThrownBy(() -> taskService.updateTask(INVALID_ID,updateTask))
-                .isInstanceOf(TaskNotFoundException.class);
+            // inInstanceOf 메서드를 통해 Exception 비교
+            assertThat(thrown).isInstanceOf(TaskNotFoundException.class)
+                    .hasMessage("Task not found: " + INVALID_ID);
+        }
 
     }
 
-    @Test
-    void deleteTaskWithValidId() {
-        Task task = new Task();
-        task.setTitle(TITLE);
-        taskService.createTask(task);
+    @Nested
+    class CreateTaskTest{
 
-        assertThat(taskService.getTasks()).hasSize(2);
-        taskService.deleteTask(DEFAULT_ID);
-        assertThat(taskService.getTasks()).hasSize(1);
-        assertThat(taskService.getTask(CREATE_ID)).isNotNull();
+        @BeforeEach
+        void setUp(){
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService = new TaskService();
+            taskService.createTask(task);
+        }
+        @Test
+        @DisplayName("Task 생성")
+        void createTask() {
+
+            assertThat(taskService.getTasks()).hasSize(1);
+            
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService.createTask(task);
+
+            assertThat(taskService.getTasks()).hasSize(2);
+            assertThat(taskService.getTask(CREATE_ID).getTitle()).isEqualTo(TITLE);
+        }
     }
 
-    @Test
-    void deleteTaskWithInvalidId() {
-        assertThatThrownBy(() -> taskService.deleteTask(INVALID_ID))
-                .isInstanceOf(TaskNotFoundException.class);
+    @Nested
+    class UpdateTaskTest{
+        @BeforeEach
+        void setUp(){
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService = new TaskService();
+            taskService.createTask(task);
+        }
+
+        @Test
+        @DisplayName("유효한 ID를 통한 수정")
+        void updateTaskWithValidId() {
+            Task updateTask = new Task();
+            updateTask.setTitle(UPDATE_TITLE);
+            taskService.updateTask(DEFAULT_ID,updateTask);
+
+            assertThat(taskService.getTasks()).hasSize(1);
+            assertThat(taskService.getTask(DEFAULT_ID).getTitle()).isEqualTo(UPDATE_TITLE);
+        }
+
+        @Test
+        @DisplayName("유효하지 않는 ID를 통한 수정")
+        void updateTaskWithInvalidId() {
+            Task updateTask = new Task();
+            updateTask.setTitle(UPDATE_TITLE);
+            assertThatThrownBy(() -> taskService.updateTask(INVALID_ID,updateTask))
+                    .isInstanceOf(TaskNotFoundException.class);
+
+        }
     }
+
+    @Nested
+    class DeleteTaskTest{
+        @BeforeEach
+        void setUp(){
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService = new TaskService();
+            taskService.createTask(task);
+        }
+        
+        @Test
+        @DisplayName("유효한 ID를 통한 삭제")
+        void deleteTaskWithValidId() {
+            Task task = new Task();
+            task.setTitle(TITLE);
+            taskService.createTask(task);
+
+            assertThat(taskService.getTasks()).hasSize(2);
+            taskService.deleteTask(DEFAULT_ID);
+            assertThat(taskService.getTasks()).hasSize(1);
+            assertThat(taskService.getTask(CREATE_ID)).isNotNull();
+        }
+
+        @Test
+        @DisplayName("유효하지 않는 ID를 통한 삭제")
+        void deleteTaskWithInvalidId() {
+            assertThatThrownBy(() -> taskService.deleteTask(INVALID_ID))
+                    .isInstanceOf(TaskNotFoundException.class);
+        }
+    }
+    
+
+    
 }
--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -6,9 +6,7 @@ import com.codesoom.assignment.models.Task;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,137 +48,200 @@ public class TaskControllerWebTest {
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
-    @BeforeEach
-    void setUp(){
-        List<Task> tasks = new ArrayList<>();
+    @Nested
+    @DisplayName("Task 생성 테스트")
+    class CreateTaskTest{
+        
+        @BeforeEach
+        void setUp(){
+            Task createTask = new Task();
+            createTask.setId(CREATE_ID);
+            createTask.setTitle(TITLE);
+            given(taskService.createTask(any(Task.class))).willReturn(createTask);
+        }
+        @Test
+        @DisplayName("Task 생성")
+        void create() throws Exception {
+            Task resource = new Task();
+            resource.setTitle(TITLE);
 
-        Task task = new Task();
-        task.setTitle(TITLE);
-        task.setId(DEFAULT_ID);
-        tasks.add(task);
+            mockMvc.perform(post("/tasks")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(objectMapper.writeValueAsString(resource)))
+                    .andExpect(status().isCreated())
+                    .andExpect(jsonPath("title",TITLE).exists());
 
-        given(taskService.getTasks()).willReturn(tasks);
-        given(taskService.getTask(DEFAULT_ID)).willReturn(task);
-        given(taskService.getTask(INVALID_ID)).willThrow(new TaskNotFoundException(INVALID_ID));
+        }
 
-        Task createTask = new Task();
-        createTask.setId(CREATE_ID);
-        createTask.setTitle(TITLE);
-        given(taskService.createTask(any(Task.class))).willReturn(createTask);
+        @Nested
+        @DisplayName("Task 리스트 테스트")
+        class TasksTest{
 
-        Task updateTask = new Task();
-        updateTask.setId(DEFAULT_ID);
-        updateTask.setTitle(UPDATE_TITLE);
-        given(taskService.updateTask(eq(DEFAULT_ID), any(Task.class))).willReturn(updateTask);
-        given(taskService.updateTask(eq(INVALID_ID), any(Task.class))).willThrow(new TaskNotFoundException(INVALID_ID));
+            @BeforeEach
+            void setUp(){
 
-        given(taskService.deleteTask(DEFAULT_ID)).willReturn(null);
-        given(taskService.deleteTask(INVALID_ID)).willThrow(new TaskNotFoundException(INVALID_ID));
+                List<Task> tasks = new ArrayList<>();
 
+                Task task = new Task();
+                task.setTitle(TITLE);
+                task.setId(DEFAULT_ID);
+                tasks.add(task);
+
+                given(taskService.getTasks()).willReturn(tasks);
+            }
+            @Test
+            @DisplayName("Task 리스트 조회")
+            void list() throws Exception {
+
+                mockMvc.perform(get("/tasks"))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("[{\"id\":1,\"title\":\"TEST_TITLE\"}]"));
+            }
+        }
+        
+        @Nested
+        @DisplayName("Task 상세조회 테스트")
+        class TaskDetailTest{
+            
+            @BeforeEach
+            void setUp(){
+                Task task = new Task();
+                task.setTitle(TITLE);
+                task.setId(DEFAULT_ID);
+                given(taskService.getTask(DEFAULT_ID)).willReturn(task);
+
+                // 주석처리를 하지 않을 경우 아래 구문에서 예외 발생
+                // given(taskService.getTask(INVALID_ID)).willThrow(new TaskNotFoundException(INVALID_ID));
+            }
+
+            @Test
+            @DisplayName("유효 ID 상세조회")
+            void detailWithValidId() throws Exception {
+                mockMvc.perform(get("/tasks/"+DEFAULT_ID))
+                        .andExpect(status().isOk())
+                        .andExpect(content().string("{\"id\":1,\"title\":\"TEST_TITLE\"}"));
+            }
+
+            @Test
+            @DisplayName("유효하지 않는 ID 상세조회")
+            void detailWithInvalidId() throws Exception {
+                given(taskService.getTask(INVALID_ID)).willThrow(new TaskNotFoundException(INVALID_ID));
+
+                mockMvc.perform(get("/tasks/"+INVALID_ID))
+                        .andExpect(status().isNotFound())
+                        .andExpect(content().string("{\"message\":\"Task not found\"}"));
+            }
+        }
+
+        @Nested
+        @DisplayName("Task 수정 테스트")
+        class UpdateTest{
+
+            @BeforeEach
+            void setUp(){
+                Task updateTask = new Task();
+                updateTask.setId(DEFAULT_ID);
+                updateTask.setTitle(UPDATE_TITLE);
+                given(taskService.updateTask(eq(DEFAULT_ID), any(Task.class))).willReturn(updateTask);
+                given(taskService.updateTask(eq(INVALID_ID), any(Task.class))).willThrow(new TaskNotFoundException(INVALID_ID));
+            }
+
+            @Test
+            @DisplayName("[PUT] 유효 ID에 대한 Task 수정")
+            void updateWithValidId() throws Exception {
+
+                Task updateTask = new Task();
+                updateTask.setTitle(UPDATE_TITLE);
+
+                mockMvc.perform(put("/tasks/"+DEFAULT_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateTask)))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("title",UPDATE_TITLE).exists())
+                        .andExpect(jsonPath("id",DEFAULT_ID).exists());
+
+            }
+
+            @Test
+            @DisplayName("[PUT] 유효하지 않는 ID에 대한 Task 수정")
+            void updateWithInvalidId() throws Exception {
+                Task updateTask = new Task();
+                updateTask.setTitle(UPDATE_TITLE);
+
+                mockMvc.perform(put("/tasks/"+INVALID_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateTask)))
+                        .andExpect(status().isNotFound())
+                        .andExpect(content().string("{\"message\":\"Task not found\"}"));
+            }
+
+            @Test
+            @DisplayName("[PATCH] 유효 ID에 대한 Task 수정")
+            void patchWithValidId() throws Exception{
+                Task updateTask = new Task();
+                updateTask.setTitle(UPDATE_TITLE);
+
+                mockMvc.perform(patch("/tasks/"+DEFAULT_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateTask)))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("title",UPDATE_TITLE).exists())
+                        .andExpect(jsonPath("id",DEFAULT_ID).exists());
+            }
+
+            @Test
+            @DisplayName("[PATCH] 유효하지 않는 ID에 대한 Task 수정")
+            void patchWithInvalidId() throws Exception {
+                Task updateTask = new Task();
+                updateTask.setTitle(UPDATE_TITLE);
+
+                mockMvc.perform(patch("/tasks/"+INVALID_ID)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(updateTask)))
+                        .andExpect(status().isNotFound())
+                        .andExpect(content().string("{\"message\":\"Task not found\"}"));
+
+
+            }
+        }
+
+        @Nested
+        @DisplayName("Task 삭제 테스트")
+        class DeleteTask{
+
+            @Test
+            @DisplayName("유효 ID에 대한 Task 삭제")
+            void deleteWithValidId() throws Exception {
+
+                given(taskService.deleteTask(DEFAULT_ID)).willReturn(null);
+
+                mockMvc.perform(delete("/tasks/"+DEFAULT_ID))
+                        .andExpect(status().isNoContent());
+
+            }
+
+            @Test
+            @DisplayName("유효하지 않는 ID에 대한 Task 삭제")
+            void deleteWithInvalidId() throws Exception {
+                given(taskService.deleteTask(INVALID_ID)).willThrow(new TaskNotFoundException(INVALID_ID));
+
+                mockMvc.perform(delete("/tasks/"+INVALID_ID))
+                        .andExpect(status().isNotFound())
+                        .andExpect(content().string("{\"message\":\"Task not found\"}"));
+            }
+
+        }
     }
-    @Test
-    void list() throws Exception {
-
-        mockMvc.perform(get("/tasks"))
-                .andExpect(status().isOk())
-                .andExpect(content().string("[{\"id\":1,\"title\":\"TEST_TITLE\"}]"));
-    }
-
-    @Test
-    void detailWithValidId() throws Exception {
-        mockMvc.perform(get("/tasks/"+DEFAULT_ID))
-                .andExpect(status().isOk())
-                .andExpect(content().string("{\"id\":1,\"title\":\"TEST_TITLE\"}"));
-    }
-
-    @Test
-    void detailWithInvalidId() throws Exception {
-        mockMvc.perform(get("/tasks/"+INVALID_ID))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("{\"message\":\"Task not found\"}"));
-   }
-
-    @Test
-    void create() throws Exception {
-
-        Task resource = new Task();
-        resource.setTitle(TITLE);
-
-        mockMvc.perform(post("/tasks")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(resource)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("title",TITLE).exists());
-
-  }
-
-    @Test
-    void updateWithValidId() throws Exception {
-
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-
-        mockMvc.perform(put("/tasks/"+DEFAULT_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(updateTask)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("title",UPDATE_TITLE).exists())
-                .andExpect(jsonPath("id",DEFAULT_ID).exists());
-
-   }
-
-    @Test
-    void updateWithInvalidId() throws Exception {
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-
-        mockMvc.perform(put("/tasks/"+INVALID_ID)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(updateTask)))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("{\"message\":\"Task not found\"}"));
-   }
-
-    @Test
-    void patchWithValidId() throws Exception{
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-
-        mockMvc.perform(patch("/tasks/"+DEFAULT_ID)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updateTask)))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("title",UPDATE_TITLE).exists())
-                .andExpect(jsonPath("id",DEFAULT_ID).exists());
-   }
-
-    @Test
-    void patchWithInvalidId() throws Exception {
-        Task updateTask = new Task();
-        updateTask.setTitle(UPDATE_TITLE);
-
-        mockMvc.perform(patch("/tasks/"+INVALID_ID)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(updateTask)))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("{\"message\":\"Task not found\"}"));
 
 
-    }
 
-    @Test
-    void deleteWithValidId() throws Exception {
+   
 
-        mockMvc.perform(delete("/tasks/"+DEFAULT_ID))
-                .andExpect(status().isNoContent());
 
-    }
 
-    @Test
-    void deleteWithInvalidId() throws Exception {
 
-        mockMvc.perform(delete("/tasks/"+INVALID_ID))
-                .andExpect(status().isNotFound())
-                .andExpect(content().string("{\"message\":\"Task not found\"}"));
-    }
+
+
+
+    
 }
