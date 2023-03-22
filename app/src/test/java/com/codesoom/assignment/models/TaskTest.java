ba3e0651359801e135d3a3c5938eb2c98cb1e4e3
yushin <kkad45@naver.com>
2023-03-22 20:40:16 +0900

    refactor 할일 테스트 코드통합

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -1,34 +1,20 @@
 package com.codesoom.assignment.models;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Task 테스트")
 class TaskTest {
     public static final String TITLE = "TITLE";
     public static final Long UPDATE_ID = 1L;
     public static final String UPDATE_TITLE = "UPDATE_TITLE";
-    Task task;
-
-
-    @BeforeEach
-    public void init() {
-        task = new Task(0L, TITLE);
-    }
 
     @Test
-    @DisplayName("getter 메소드 테스트")
-    public void getter() {
-        assertThat(task.getTitle()).isEqualTo(TITLE);
-        assertThat(task.getId()).isEqualTo(0L);
-    }
-
-    @Test
-    @DisplayName("setter 메소드 테스트")
+    @DisplayName("기본 사용법 (Getter,Setter)")
     public void setter() {
+        Task task = new Task(0L, TITLE);
         task.setTitle(UPDATE_TITLE);
         task.setId(UPDATE_ID);
 
