8ccd85bde527fa00ea104434f46da19877dc04e8
yushin <kkad45@naver.com>
2023-03-23 20:36:40 +0900

    refactor TaskServiceTest.DisplayName 형식 수정

--- app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
+++ app/src/test/java/com/codesoom/assignment/application/TaskServiceTest.java
@@ -30,13 +30,13 @@ class TaskServiceTest {
     }
 
     @Test
-    @DisplayName("존재하지 않는 특정 할일을  상세 조회하는 경우 에러 반환")
+    @DisplayName("존재하지 않는 특정 할 일을  상세 조회하는 경우 에러를 반환한다.")
     public void getThrowError() {
         assertThatThrownBy(() -> taskService.getTask(1L));
     }
 
     @Test
-    @DisplayName(" 할일 생성 요청을 하면 사이즈가 1증가한다.")
+    @DisplayName(" 할 일에 대한  생성 요청을 하면 사이즈가 1증가한다.")
     public void create() {
         assertThat(taskService.getTasks()).hasSize(0);
 
@@ -47,7 +47,7 @@ class TaskServiceTest {
     }
 
     @Test
-    @DisplayName("특정 할일이 수정되면 해당 할일의 제목은 수정된다.")
+    @DisplayName("특정 할 일이 수정되면 해당 할 일의 제목은 수정된다.")
     public void update() {
         Task task = createTestTask();
         Task createTask = taskService.createTask(task);
@@ -66,7 +66,7 @@ class TaskServiceTest {
     }
 
     @Test
-    @DisplayName("특정 할일이  존재할 경우 해당  할일을 삭제하면 리스트의 사이즈는 줄어든다.")
+    @DisplayName("특정 할 일이  존재하는 경우 해당  할 일을 삭제하면 리스트의 사이즈는 줄어든다.")
     public void delete() {
         Task testTask = createTestTask();
         Task task = taskService.createTask(testTask);
@@ -75,7 +75,7 @@ class TaskServiceTest {
     }
 
     @Test
-    @DisplayName("특정 할 일이 존재하지 않은 경우에 삭제 요청을 하면 에러를 반환한다.")
+    @DisplayName("존재하지 않은 할 일을 삭제 하면 에러를 반환한다.")
     public void deleteWithEmptyList() {
         assertThatThrownBy(() -> taskService.deleteTask(9999L));
     }
