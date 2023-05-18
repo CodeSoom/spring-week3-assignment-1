3b6b0467a7e533a2eed8c6bc4b5ecf28eaabf3c6
EunjinJwa <ejjwa0563@gmail.com>
2023-05-18 19:33:40 +0900

    code review 반영
    - 마지막 한 줄 공백 라인추가
    - TaskTest 사용을 알 수 있도록 테스트코드 변경

--- app/src/test/java/com/codesoom/assignment/models/TaskTest.java
+++ app/src/test/java/com/codesoom/assignment/models/TaskTest.java
@@ -8,33 +8,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 class TaskTest {
 
 	@Test
-	public void taskConstructorTest() {
-		String title = "NEW TASK";
-
-		Task created = Task.title(title);
-
-		assertThat(created.getTitle()).isEqualTo(title);
-	}
-
-	@Test
 	public void taskSetIdTest() {
 		long id = 100L;
-		Task task = Task.title("NEW TASK");
+		String title = "NEW TASK";
 
+		Task task = Task.title(title);
 		task.setId(id);
 
 		assertThat(task.getId()).isEqualTo(id);
-	}
-
-	@Test
-	public void updateTaskTitleTest() {
-		String beforeTitle = "NEW TASK";
-		String afterTitle = "UPDATED TASK";
-		Task task = Task.title(beforeTitle);
-
-		task.setTitle(afterTitle);
-
-		assertThat(task.getTitle()).isEqualTo(afterTitle);
+		assertThat(task.getTitle()).isEqualTo(title);
 	}
 
 }
