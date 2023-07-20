e468b6729a576ff133db9419a31d2083d8f617ca
tmxhsk99 <tmxhsk99@naver.com>
2023-07-21 04:41:47 +0900

    test : TaskControllerWebTest 수정 삭제 테스트 추가

--- app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
+++ app/src/test/java/com/codesoom/assignment/controllers/TaskControllerWebTest.java
@@ -4,6 +4,7 @@ import com.codesoom.assignment.TaskNotFoundException;
 import com.codesoom.assignment.application.TaskService;
 import com.codesoom.assignment.models.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,36 +37,85 @@ class TaskControllerWebTest {
     @MockBean
     private TaskService taskService;
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    @DisplayName("요청한 할일목록 있는 경우 해당할일 응답")
+    void list() throws Exception {
+        //given
         List<Task> tasks = new ArrayList<>();
         Task task = new Task();
         task.setTitle("test task");
         tasks.add(task);
-
         given(taskService.getTasks()).willReturn(tasks);
 
-        given(taskService.getTask(1L)).willReturn(task);
-
-        given(taskService.getTask(100L))
-                .willThrow(new TaskNotFoundException(100L));
-    }
-    @Test
-    void list() throws Exception {
+        // expect
         mockMvc.perform(get("/tasks"))
                 .andExpect((status().isOk()))
                 .andExpect(content().string(containsString("test task")));
     }
 
     @Test
+    @DisplayName("요청한 할일이 있는 경우 200 상태코드와 조회한 할일 응답")
     void detailWithValidId() throws Exception {
+        // given
+        Task task = new Task();
+        given(taskService.getTask(1L)).willReturn(task);
+
+        // expect
         mockMvc.perform(get("/tasks/1"))
                 .andExpect(status().isOk());
     }
 
     @Test
+    @DisplayName("요청한 할일이 없는 경우 404 상태코드 응답")
     void detailWithInvalidId() throws Exception {
+        // given
+        given(taskService.getTask(100L))
+                .willThrow(new TaskNotFoundException(100L));
+        // expect
         mockMvc.perform(get("/tasks/100"))
                 .andExpect(status().isNotFound());
+    }
+    
+    @Test
+    @DisplayName("요청한 할일 생성시 201 상태코드 응답 후 생성된 할일 응답")
+    void createTask() throws Exception {
+        // given
+        Task task = new Task();
+        task.setTitle("test task");
+        given(taskService.createTask(any(Task.class))).willReturn(task);
+
+        // expect
+        mockMvc.perform(post("/tasks")
+                        .contentType("application/json")
+                        .content("{\"title\":\"test task\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(content().string(containsString("test task")));
+    }
+
+    @Test
+    @DisplayName("요청한 할일 수정시 200 상태코드 응답 후 수정된 할일 응답")
+    void updateTask() throws Exception {
+        // given
+        Task task = new Task();
+        task.setTitle("test task2");
+        given(taskService.updateTask(any(Long.class), any(Task.class))).willReturn(task);
+
+        // expect
+        mockMvc.perform(put("/tasks/1")
+                        .contentType("application/json")
+                        .content("{\"title\":\"test task2\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("test task2")));
+    }
+
+    @Test
+    @DisplayName("요청한 할일 수정시 203 상태코드 응답")
+    void deleteTask() throws Exception {
+        // given
+        given(taskService.deleteTask(any(Long.class))).willReturn(null);
+
+        // expect
+        mockMvc.perform(delete("/tasks/1"))
+                .andExpect(status().isNoContent());
     }
 }
